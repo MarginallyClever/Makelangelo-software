731fbf5bcefaff4c61e45129d23c94b8468d00cc
Dan Royer <dan@marginallyclever.com>
2012-07-25 23:53:56 -0700

    fixed osx runme script

--- java/osx_runme.sh
+++ java/osx_runme.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-java -classpath RXTXcomm.jar -Djava.library.path=classes/rxtxnative/Mac_OS_X -jar DrawbotGUI.jar
+SOURCE="${BASH_SOURCE[0]}"
+DIR="$( dirname "$SOURCE" )"
+while [ -h "$SOURCE" ]
+do 
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+  DIR="$( cd -P "$( dirname "$SOURCE"  )" && pwd )"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+java -classpath "$DIR/RXTXcomm.jar" -Djava.library.path="$DIR/classes/rxtxnative/Mac_OS_X" -jar "$DIR/DrawbotGUI.jar"
