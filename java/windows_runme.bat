ae4cd15906a59359af5e12f632e16d150991814f
Dan Royer <dan@marginallyclever.com>
2012-09-29 10:57:46 -0700

    Attempt to fix 64 bit windows startup error
    native libraries for windows-x64 were added.
    windows_runme.bat updated.

--- java/windows_runme.bat
+++ java/windows_runme.bat
@@ -1,1 +1,14 @@
-java -classpath RXTXcomm.jar -Djava.library.path=classes\rxtxnative\Windows\i3680mingw32 -jar DrawbotGUI.jar
+@echo off
+ 
+Set RegQry=HKLM\Hardware\Description\System\CentralProcessor\0
+ 
+REG.exe Query %RegQry% > checkOS.txt
+ 
+Find /i "x86" < CheckOS.txt > StringCheck.txt
+ 
+If %ERRORLEVEL% == 0 (
+    java -classpath RXTXcomm.jar -Djava.library.path=classes\rxtxnative\Windows\i3680mingw32 -jar DrawbotGUI.jar
+) ELSE (
+    java -classpath RXTXcomm.jar -Djava.library.path=classes\rxtxnative\Windows\windows-x64 -jar DrawbotGUI.jar
+)
+
