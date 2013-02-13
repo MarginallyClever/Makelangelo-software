1822e6962802361534ad3f18cde40a984b11dd96
Dan Royer <dan@marginallyclever.com>
2013-02-12 19:04:59 -0800

    0.9.0
    - split start.bat into start32.bat and start64.bat

---
+++ java/Windows/start32.bat
@@ -0,0 +1,3 @@
+@echo off
+java -classpath RXTXcomm.jar -Djava.library.path=32 -jar DrawbotGUI.jar
+@pause
---
+++ java/Windows/start64.bat
@@ -0,0 +1,3 @@
+@echo off
+java -classpath RXTXcomm.jar -Djava.library.path=64 -jar DrawbotGUI.jar
+@pause
