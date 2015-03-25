641227d72d43e7a5b6ae86a4534980a45acdf40e
Peter Colapietro <petercolapietro@gmail.com>
2015-03-25 09:37:02 -0400

    Updated Javadoc for refactored method.

--- java/src/Makelangelo/Makelangelo.java
+++ java/src/Makelangelo/Makelangelo.java
@@ -82,8 +82,10 @@ public class Makelangelo
 	// Java required?
 	static final long serialVersionUID=1L;
 
-	// software version
-	static final String version="7";
+    /**
+     * software version
+     */
+	public static final String version="7";
 	
 	private static Makelangelo singletonObject;
 	
@@ -1624,7 +1626,18 @@ public class Makelangelo
 
 	/**
 	 * 
-     * @return An HTML string (supports internationalization) used for the About Message Dialog.
+     * <p>
+     * Uses {@link java.lang.StringBuilder#append(String)} to create an internationalization supported {@code String}
+     * representing the About Message Dialog's HTML.
+     * </p>
+     *
+     * <p>
+     * The summation of {@link String#length()} for each of the respective values retrieved with the
+     * {@code "AboutHTMLBeforeVersionNumber"}, and {@code "AboutHTMLAfterVersionNumber"} {@link MultilingualSupport} keys,
+     * in conjunction with {@link Makelangelo#version} is calculated for use with {@link java.lang.StringBuilder#StringBuilder(int)}.
+     * </p>
+     *
+     * @return An HTML string used for the About Message Dialog.
      */
     private String getAboutHtmlFromMultilingualString() {
         final String aboutHtmlBeforeVersionNumber = MultilingualSupport.getSingleton().get("AboutHTMLBeforeVersionNumber");
