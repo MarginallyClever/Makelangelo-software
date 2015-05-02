63e5c60b8839bdf87e63ba78b65f245bed168501
Dan Royer <dan@marginallyclever.com>
2015-05-02 14:12:06 -0700

    removed start win64.bat (now redundant)

--- last_official_release/Makelangelo/start windows.bat
+++ last_official_release/Makelangelo/start windows.bat
@@ -4,20 +4,37 @@
 cls
 setlocal ENABLEEXTENSIONS
 ::
-:: get the current version
+:: get the current java version
 ::
+
+:CheckOS
+
+IF EXIST "%PROGRAMFILES(X86)%" (GOTO 64bit) ELSE (GOTO 32bit)
+
+:64bit
+
+FOR /F "skip=2 tokens=2*" %%A IN ('REG QUERY "HKLM\Software\Wow6432Node\JavaSoft\Java Runtime Environment" /v CurrentVersion') DO set CurVer=%%B
+
+FOR /F "skip=2 tokens=2*" %%A IN ('REG QUERY "HKLM\Software\Wow6432Node\JavaSoft\Java Runtime Environment\%CurVer%" /v JavaHome') DO set JAVA_HOME=%%B
+
+GOTO endos
+
+:32bit
+
 FOR /F "skip=2 tokens=2*" %%A IN ('REG QUERY "HKLM\Software\JavaSoft\Java Runtime Environment" /v CurrentVersion') DO set CurVer=%%B
 
 FOR /F "skip=2 tokens=2*" %%A IN ('REG QUERY "HKLM\Software\JavaSoft\Java Runtime Environment\%CurVer%" /v JavaHome') DO set JAVA_HOME=%%B
+
+:endos
 
 if defined JAVA_HOME (
   @echo The current Java runtime is  %CurVer%
 ) else (
   @echo Java not found.
+  @pause
   goto end
 )
 
-"%JAVA_HOME%\bin\java.exe" -jar Makelangelo.jar
+"%JAVA_HOME%\bin\java.exe" -jar Makelangelo.jar -splash:src/main/resources/splash.png
 
 :end
-@pause
