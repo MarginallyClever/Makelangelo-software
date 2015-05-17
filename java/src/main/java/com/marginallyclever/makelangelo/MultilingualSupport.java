6f4166099914d82146d83c42d23e62e9afe14e24
Peter Colapietro <petercolapietro@gmail.com>
2015-05-17 08:58:28 -0400

    Extracted method for creating language containers from array of Files.

--- java/src/main/java/com/marginallyclever/makelangelo/MultilingualSupport.java
+++ java/src/main/java/com/marginallyclever/makelangelo/MultilingualSupport.java
@@ -41,24 +41,26 @@ public class MultilingualSupport {
 	public void loadConfig() {
 		currentLanguage = prefs.get(LANGUAGE_KEY, "English");
 	}
-	
-	public void loadLanguages() {
-		// Scan folder for language files
-        String workingDirectory=System.getProperty("user.dir")+File.separator+"languages";
-        //System.out.println(workingDirectory);
-		File f = new File(workingDirectory);
-		LanguageContainer lang;
 
-		File [] all_files = f.listFiles();
-		try {
-			if(all_files.length<=0) {
-				throw new Exception("No language files found!");
-			}
+	/**
+	 * Scan folder for language files.
+	 */
+	public void loadLanguages() throws IllegalStateException {
+        String workingDirectory=System.getProperty("user.dir")+File.separator+"languages";
+		final File f = new File(workingDirectory);
+		final File [] all_files = f.listFiles();
+		if(all_files.length<=0) {
+			throw new IllegalStateException("No language files found!");
 		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-		
+		createLanguageContainersFromLanguageFiles(all_files);
+	}
+
+	/**
+	 *
+	 * @param all_files
+	 */
+	private void createLanguageContainersFromLanguageFiles(File[] all_files) {
+		LanguageContainer lang;
 		for(int i=0;i<all_files.length;++i) {
 			if(all_files[i].isHidden()) continue;
 			if(all_files[i].isDirectory()) continue;
@@ -69,7 +71,7 @@ public class MultilingualSupport {
 			lang = new LanguageContainer();
 			lang.Load(all_files[i].getAbsolutePath());
 			languages.put(lang.getName(), lang);
-		}	
+		}
 	}
 	
 	public String get(String key) {
