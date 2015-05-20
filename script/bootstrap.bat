110490bd702f533d75d472eee301d446146aa665
Peter Colapietro <petercolapietro@gmail.com>
2015-05-20 19:59:16 -0400

    Calling maven wrapper scripts in bootstrap script.

---
+++ script/bootstrap.bat
@@ -0,0 +1,6 @@
+@REM author: Peter Carlo Colapietro
+@REM #see: http://wynnnetherland.com/linked/2013012801/bootstrapping-consistency/
+@REM #see: https://github.com/bdemers/maven-wrapper
+pushd java
+call mvnw.bat install
+popd
