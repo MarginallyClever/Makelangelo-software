ea4a5d1caf92d836a7586e67a27b8a1459cc37d3
chrsmlls333 <chrsmlls333@gmail.com>
2015-05-23 14:57:42 -0700

    make last 2 commits optional

--- java/src/main/java/com/marginallyclever/makelangelo/MainGUI.java
+++ java/src/main/java/com/marginallyclever/makelangelo/MainGUI.java
@@ -1022,7 +1022,7 @@ public class MainGUI
 		sendLineToRobot(machineConfiguration.getConfigLine());
 		sendLineToRobot(machineConfiguration.getBobbinLine());
 		sendLineToRobot("G92 X0 Y0");
-		sendLineToRobot("M17");
+		//sendLineToRobot("M17"); FIXME add to options dialog
 	}
 	
 	
@@ -1239,7 +1239,7 @@ public class MainGUI
 			return;
 		}
 		if( subject == buttonDisconnect ) {
-			sendLineToRobot("M18");
+			//sendLineToRobot("M18"); FIXME add to options dialog
 			connectionToRobot.closeConnection();
 			connectionToRobot=null;
 			clearLog();
