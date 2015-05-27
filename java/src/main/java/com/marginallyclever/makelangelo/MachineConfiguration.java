1857d85b3d8ac34075fe7014114b1242156d8934
Dan Royer <dan@marginallyclever.com>
2015-05-27 13:54:53 -0700

    removed unneeded library

--- java/src/main/java/com/marginallyclever/makelangelo/MachineConfiguration.java
+++ java/src/main/java/com/marginallyclever/makelangelo/MachineConfiguration.java
@@ -11,7 +11,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.Vector;
 import java.util.prefs.Preferences;
 
 import javax.imageio.ImageIO;
