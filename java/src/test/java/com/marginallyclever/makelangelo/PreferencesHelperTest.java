def61c161aff5378ed0a57dd7c0c286ebaad5010
Peter Colapietro <petercolapietro@gmail.com>
2015-06-17 10:21:52 -0400

    Updated testMachineConfigurationNames method to actually do what it is meant to.

--- java/src/test/java/com/marginallyclever/makelangelo/PreferencesHelperTest.java
+++ java/src/test/java/com/marginallyclever/makelangelo/PreferencesHelperTest.java
@@ -2,8 +2,10 @@ package com.marginallyclever.makelangelo;
 
 import com.marginallyclever.util.MarginallyCleverJsonFilePreferencesFactory;
 import com.marginallyclever.util.MarginallyCleverPreferences;
+import com.marginallyclever.util.UnitTestHelper;
 import org.json.JSONObject;
 import org.json.Property;
+import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -93,11 +95,17 @@ public class PreferencesHelperTest {
 
     @Test
     public void testMachineConfigurationNames() throws BackingStoreException {
-        logger.info("Node name: {}", preferenceNode.name());
-        for (String key : preferenceNode.keys()) {
-            logger.info("key name: {}", key);
+        final String thisMethodsName = Thread.currentThread().getStackTrace()[CLIENT_CODE_STACK_INDEX].getMethodName();
+        logger.info("start: {}", thisMethodsName);
+        final Preferences machinesPreferenceNode = PreferencesHelper.getPreferenceNode(PreferencesHelper.MakelangeloPreferenceKey.MACHINES);
+        logger.info("node name: {}", machinesPreferenceNode.name());
+        final String[] childrenPreferenceNodeNames = machinesPreferenceNode.childrenNames();
+        for (String childNodeName : childrenPreferenceNodeNames) {
+            logger.info("child node name: {}", childNodeName);
+            final boolean isMachineNameAnInteger = UnitTestHelper.isInteger(childNodeName);
+            Assert.assertTrue(isMachineNameAnInteger);
         }
-
+        logger.info("end: {}", thisMethodsName);
     }
 
     /**
@@ -206,5 +214,32 @@ public class PreferencesHelperTest {
         } catch (BackingStoreException e) {
             logger.error("{}", e);
         }
+    }
+
+    /**
+     * Over engineered. There are <a href="http://stackoverflow.com/a/442773">pitfalls</a> to this method of getting a
+     * {@code StackTraceElement}'s index which this method does not address. I make a best guess based upon development
+     * environment testing.
+     *
+     * @see <a href="http://stackoverflow.com/a/8592871">Getting the name of the current executing method</a>
+     */
+    private static final int CLIENT_CODE_STACK_INDEX;
+
+    static {
+        /*
+         Finds out the index of "this code" in the returned stack trace - funny but it differs in JDK 1.5, and 1.6.
+         In my tests I had to modify to get to work in 1.7 and 1.8.
+        */
+        int i = 0;
+        for (StackTraceElement ste : Thread.currentThread().getStackTrace()) {
+            /* original placement of increment via SO
+            i++;
+             */
+            if (ste.getClassName().equals(PreferencesHelperTest.class.getName())) {
+                break;
+            }
+            i++;//My placement of increment via environmental testing.
+        }
+        CLIENT_CODE_STACK_INDEX = i;
     }
 }
