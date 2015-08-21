4c2a8d9f2f84795fdbd7f8630080c39b6b07674f
Dan Royer <dan@marginallyclever.com>
2015-08-21 00:30:58 -0700

    moved glyph rendering to your message here
    it’s stored as a comment, for reference.
    soon it will be the method for rendering text, rather than relying on
    english ascii only gcode files.

--- java/src/main/java/com/marginallyclever/filters/Filter_GeneratorYourMessageHere.java
+++ java/src/main/java/com/marginallyclever/filters/Filter_GeneratorYourMessageHere.java
@@ -1,6 +1,7 @@
 package com.marginallyclever.filters;
 
 
+import com.jogamp.opengl.GL2;
 import com.marginallyclever.makelangelo.MachineConfiguration;
 import com.marginallyclever.makelangelo.MainGUI;
 import com.marginallyclever.makelangelo.MultilingualSupport;
@@ -11,6 +12,9 @@ import org.slf4j.LoggerFactory;
 import javax.swing.*;
 
 import java.awt.*;
+import java.awt.font.FontRenderContext;
+import java.awt.font.TextLayout;
+import java.awt.geom.PathIterator;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 
@@ -55,11 +59,85 @@ public class Filter_GeneratorYourMessageHere extends Filter {
 	    if (result == JOptionPane.OK_OPTION) {
 			lastMessage = text.getText();
 			createMessage(lastMessage,dest);
+
+	        //renderFont(gl2,"TimesRoman","مرحبا بالعالم",18);
+	        //renderFont(gl2,"TimesRoman","Makelangelo",36);
 			
 			// TODO Move to GUI?
 			mainGUI.log("<font color='green'>Completed.</font>\n");
 	    }
 	}
+	
+	/*
+    void renderFont(GL2 gl2, String font_name,String text,int size) {
+      gl2.glPushMatrix();
+        gl2.glScalef(0.1f, -0.1f, 1);
+        gl2.glLineWidth(3);
+        gl2.glPointSize(4);
+        
+    Font font = new Font(font_name, Font.PLAIN, size);
+    FontRenderContext frc = new FontRenderContext(null,true,true);
+    TextLayout textLayout = new TextLayout(text,font,frc);
+    Shape s = textLayout.getOutline(null);
+      PathIterator pi = s.getPathIterator(null);
+      float [] coords = new float[6];
+      float [] coords2 = new float[6];
+      float [] start = new float[6];
+      while(pi.isDone() == false ) {
+        int type = pi.currentSegment(coords);
+        switch(type) {
+        case PathIterator.SEG_CLOSE:
+            gl2.glVertex2f(start[0], start[1]);
+            gl2.glEnd();
+          break;
+        case PathIterator.SEG_LINETO:
+            gl2.glVertex2f(coords[0], coords[1]);
+          coords2[0] = coords[0];
+          coords2[1] = coords[1];
+          break;
+        case PathIterator.SEG_MOVETO:
+          // move without drawing
+          start[0] = coords2[0] = coords[0];
+          start[1] = coords2[1] = coords[1];
+            gl2.glBegin(GL2.GL_LINE_STRIP);
+            gl2.glVertex2f(start[0], start[1]);
+          break;
+        case PathIterator.SEG_CUBICTO:
+          for(int i=0;i<10;++i) {
+            float t = (float)i/10.0f;
+          // p = a0 + a1*t + a2 * tt + a3*ttt;
+          float tt=t*t;
+          float ttt=tt*t;
+          float x = coords2[0] + (coords[0]*t) + (coords[2]*tt) + (coords[4]*ttt);
+          float y = coords2[1] + (coords[1]*t) + (coords[3]*tt) + (coords[5]*ttt);
+          gl2.glVertex2f(x,y);
+          }
+        gl2.glVertex2f(coords[4],coords[5]);
+          coords2[0] = coords[4];
+          coords2[1] = coords[5];
+          break;
+        case PathIterator.SEG_QUADTO:
+          for(int i=0;i<10;++i) {
+            float t = (float)i/10.0f;
+            //(1-t)²*P0 + 2t*(1-t)*P1 + t²*P2
+            float u = (1.0f-t);
+          float tt=u*u;
+          float ttt=2.0f*t*u;
+          float tttt=t*t;
+          float x = coords2[0]*tt + (coords[0]*ttt) + (coords[2]*tttt);
+          float y = coords2[1]*tt + (coords[1]*ttt) + (coords[3]*tttt);
+          gl2.glVertex2f(x,y);
+          }
+        gl2.glVertex2f(coords[2],coords[3]);
+          coords2[0] = coords[2];
+          coords2[1] = coords[3];
+          break;
+        }
+        pi.next();
+      }
+      gl2.glPopMatrix();
+    }
+	 */
 
   protected void createMessage(String str,String dest) {
 
