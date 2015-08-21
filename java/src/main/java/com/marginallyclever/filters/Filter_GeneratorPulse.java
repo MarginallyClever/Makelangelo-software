402b03336d0e24cccd80162bdfe8d739135eccaf
Dan Royer <dan@marginallyclever.com>
2015-08-21 00:26:55 -0700

    improved pulse and crosshatch
    Now both take place in paper space instead of image space.
    The same techniqu should be applied to all filters for better image
    quality.

--- java/src/main/java/com/marginallyclever/filters/Filter_GeneratorPulse.java
+++ java/src/main/java/com/marginallyclever/filters/Filter_GeneratorPulse.java
@@ -18,7 +18,7 @@ import javax.swing.JTextField;
 
 
 public class Filter_GeneratorPulse extends Filter {
-  float blockScale=4.0f;
+  float blockScale=6.0f;
   int direction=0;
   
   public Filter_GeneratorPulse(MainGUI gui, MachineConfiguration mc,
@@ -40,6 +40,44 @@ public class Filter_GeneratorPulse extends Filter {
       lastup=up;
     }
     tool.writeMoveTo(out, TX(x), TY(y));
+  }
+  
+  /**
+   * create horizontal lines across the image.  Raise and lower the pen to darken the appropriate areas
+   * @param img the image to convert.
+   */
+  @Override
+  public void convert(BufferedImage img) throws IOException {
+    final JTextField field_size = new JTextField(Float.toString(blockScale));
+
+    JPanel panel = new JPanel(new GridLayout(0,1));
+    panel.add(new JLabel(translator.get("HilbertCurveSize")));
+    panel.add(field_size);
+    
+    String [] directions = { "horizontal", "vertical" };
+    final JComboBox<String> direction_choices = new JComboBox<String>(directions);
+    panel.add(direction_choices);
+    
+    int result = JOptionPane.showConfirmDialog(null, panel, getName(), JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+    if (result == JOptionPane.OK_OPTION) {
+      blockScale = Float.parseFloat(field_size.getText());
+      direction = direction_choices.getSelectedIndex();
+      convertNow(img);
+    }
+  }
+
+  
+  double xStart,yStart;
+  double xEnd,yEnd;
+  double paperWidth,paperHeight;
+    
+  protected int sampleScale(BufferedImage img,double x0,double y0,double x1,double y1) {
+    return sample(img,
+        (x0-xStart)/(xEnd-xStart) * (double)image_width,
+        (double)image_height - (y1-yStart)/(yEnd-yStart) * (double)image_height,
+        (x1-xStart)/(xEnd-xStart) * (double)image_width,
+        (double)image_height - (y0-yStart)/(yEnd-yStart) * (double)image_height
+        );
   }
   
   // sample the pixels from x0,y0 (top left) to x1,y1 (bottom right)
@@ -65,57 +103,174 @@ public class Filter_GeneratorPulse extends Filter {
     return value/sum;
   }
   
-  /**
-   * create horizontal lines across the image.  Raise and lower the pen to darken the appropriate areas
-   * @param img the image to convert.
-   */
-  @Override
-  public void convert(BufferedImage img) throws IOException {
-    final JTextField field_size = new JTextField(Float.toString(blockScale));
 
-    JPanel panel = new JPanel(new GridLayout(0,1));
-    panel.add(new JLabel(translator.get("HilbertCurveSize")));
-    panel.add(field_size);
-    
-    String [] directions = { "horizontal", "vertical" };
-    final JComboBox<String> direction_choices = new JComboBox<String>(directions);
-    panel.add(direction_choices);
-    
-      int result = JOptionPane.showConfirmDialog(null, panel, getName(), JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
-      if (result == JOptionPane.OK_OPTION) {
-        blockScale = Float.parseFloat(field_size.getText());
-        direction = direction_choices.getSelectedIndex();
-      convertNow(img);
-      }
+  /**
+   * Converts images into zigzags in paper space instead of image space
+   * @param img the buffered image to convert
+   * @throws IOException couldn't open output file
+   */
+  private void convertNow(BufferedImage img) throws IOException {
+	    Filter_BlackAndWhite bw = new Filter_BlackAndWhite(mainGUI,machine,translator,255); 
+	    img = bw.process(img);
+
+	    mainGUI.log("<font color='green'>Converting to gcode and saving "+dest+"</font>\n");
+	        try(
+	        final OutputStream fileOutputStream = new FileOutputStream(dest);
+	        final Writer out = new OutputStreamWriter(fileOutputStream, StandardCharsets.UTF_8);
+	        ) {
+
+	            imageStart(img, out);
+
+	            // set absolute coordinates
+	            out.write("G00 G90;\n");
+	            tool.writeChangeTo(out);
+	            liftPen(out);
+
+//	            convertImageSpace(img, out);
+	    		convertPaperSpace(img,out);
+
+	            liftPen(out);
+	            signName(out);
+	            moveTo(out, 0, 0, true);
+	        }
   }
   
-  private void convertNow(BufferedImage img) throws IOException {
-    // The picture might be in color.  Smash it to 255 shades of grey.
-    Filter_BlackAndWhite bw = new Filter_BlackAndWhite(mainGUI,machine,translator,255);
-    img = bw.process(img);
+  
+  private void convertPaperSpace(BufferedImage img,Writer out) throws IOException {
+    // if the image were projected on the paper, where would the top left corner of the image be in paper space?
+    // image(0,0) is (-paperWidth/2,-paperHeight/2)*paperMargin
+    
+    paperWidth = machine.getPaperWidth();
+    paperHeight = machine.getPaperHeight();
+    
+    xStart = -paperWidth/2.0;
+    yStart = xStart * (double)image_height/(double)image_width;
 
-    // Open the destination file
-        try(
-        final OutputStream fileOutputStream = new FileOutputStream(dest);
-        final Writer out = new OutputStreamWriter(fileOutputStream, StandardCharsets.UTF_8);
-        ) {
-            // Set up the conversion from image space to paper space, select the current tool, etc.
-            imageStart(img, out);
-            // "please change to tool X and press any key to continue"
-            tool.writeChangeTo(out);
-            // Make sure the pen is up for the first move
-            liftPen(out);
+    if(yStart < -(paperHeight/2.0)) {
+      xStart *= (-(paperHeight/2.0)) / yStart;      
+      yStart = -(paperHeight/2.0);
+    }
 
+    xStart *= 10.0* machine.paperMargin;
+    yStart *= 10.0* machine.paperMargin;
+    xEnd = -xStart;
+    yEnd = -yStart;
+    
+    double PULSE_MINIMUM=0.5;
 
+    // figure out how many lines we're going to have on this image.
+	double stepSize = tool.getDiameter() * blockScale;
+    double halfStep = stepSize/2.0;
+    double zigZagSpacing = tool.getDiameter();
+
+    // from top to bottom of the image...
+    double x, y, z, scale_z,pulse_size, i = 0;
+    double n=1;
+
+    if (direction == 0) {
+        // horizontal
+        for (y = yStart; y < yEnd; y += stepSize) {
+            ++i;
+
+            if ((i % 2) == 0) {
+                // every even line move left to right
+                //moveToPaper(file,x,y,pen up?)]
+                moveToPaper(out,  xStart,  y + halfStep, true);
+
+                for (x = xStart; x < xEnd; x += zigZagSpacing) {
+                    // read a block of the image and find the average intensity in this block
+                    z = sampleScale( img, x-zigZagSpacing, y - halfStep,x+zigZagSpacing,y + halfStep );
+                    // scale the intensity value
+                    assert(z>=0);
+                    assert(z<=255.0);
+                    scale_z = (255.0 -  z) / 255.0;
+                    //scale_z *= scale_z;  // quadratic curve
+                    pulse_size = halfStep * scale_z;
+
+                    moveToPaper(out, x,  (y + halfStep + pulse_size * n), pulse_size < PULSE_MINIMUM);
+                    n = n>0? -1:1;
+                }
+                moveToPaper(out, xEnd,  y + halfStep, true);
+            } else {
+                // every odd line move right to left
+                //moveToPaper(file,x,y,pen up?)]
+                moveToPaper(out,  xEnd,  y + halfStep, true);
+
+                for (x = xEnd; x >= xStart; x -= zigZagSpacing) {
+                    // read a block of the image and find the average intensity in this block
+                    z = sampleScale( img, x - zigZagSpacing,y - halfStep, x+zigZagSpacing, y + halfStep );
+                    // scale the intensity value
+                    scale_z = (255.0 -  z) / 255.0;
+                    //scale_z *= scale_z;  // quadratic curve
+                    assert(scale_z<=1.0);
+                    pulse_size = halfStep * scale_z;
+                    moveToPaper(out, x, (y + halfStep + pulse_size * n), pulse_size < PULSE_MINIMUM);
+                    n = n>0? -1:1;
+                }
+                moveToPaper(out,  xStart,  y + halfStep, true);
+            }
+        }
+    } else {
+        // vertical
+        for (x = xStart; x < xEnd; x += stepSize) {
+            ++i;
+
+            if ((i % 2) == 0) {
+                // every even line move top to bottom
+                //moveToPaper(file,x,y,pen up?)]
+                moveToPaper(out,  x + halfStep,  yStart, true);
+
+                for (y = yStart; y < yEnd; y += zigZagSpacing) {
+                    // read a block of the image and find the average intensity in this block
+                    z = sampleScale(img, x - halfStep, y - zigZagSpacing, x + halfStep, y + zigZagSpacing );
+                    // scale the intensity value
+                    scale_z = (255.0f -  z) / 255.0f;
+                    //scale_z *= scale_z;  // quadratic curve
+                    pulse_size = halfStep * scale_z;
+                    moveToPaper(out,  (x + halfStep + pulse_size * n),  y, pulse_size < PULSE_MINIMUM);
+                	n *= -1;
+                }
+                moveToPaper(out,  x + halfStep,  yEnd, true);
+            } else {
+                // every odd line move bottom to top
+                //moveToPaper(file,x,y,pen up?)]
+                moveToPaper(out,  x + halfStep,  yEnd, true);
+
+                for (y = yEnd; y >= yStart; y -= zigZagSpacing) {
+                    // read a block of the image and find the average intensity in this block
+                    z = sampleScale(img, x - halfStep, y - zigZagSpacing, x + halfStep, y + zigZagSpacing );
+                    // scale the intensity value
+                    scale_z = (255.0f -  z) / 255.0f;
+                    //scale_z *= scale_z;  // quadratic curve
+                    pulse_size = halfStep * scale_z;
+                    moveToPaper(out,  (x + halfStep + pulse_size * n),  y, pulse_size < PULSE_MINIMUM);
+                	n *= -1;
+                }
+                moveToPaper(out,  x + halfStep,  yStart, true);
+            }
+        }
+    }
+  }
+  
+  
+  /**
+   * TODO he detail level on the zigzags cannot be increased beyond the resolution of the bufferedimage because this entire \
+   * method was written in image space.  It should be upgraded to paper space to fix the issue.
+   * @param img the buffered image to convert
+   * @throws IOException couldn't open output file
+   */
+  private void convertImageSpace(BufferedImage img,Writer out) throws IOException {
             // figure out how many lines we're going to have on this image.
-            int steps = (int) Math.ceil(tool.getDiameter() / (1.75 * scale));
+            int steps = (int) Math.ceil(tool.getDiameter() / (2.0f*scale));
             if (steps < 1) steps = 1;
 
             int blockSize = (int) (steps * blockScale);
             float halfstep = (float) blockSize / 2.0f;
+            float zigZagSpacing = steps;
 
             // from top to bottom of the image...
-            int x, y, z, i = 0, k = 0;
+            int x, y, z, i = 0;
+            float n=1;
 
             if (direction == 0) {
                 // horizontal
@@ -143,11 +298,10 @@ public class Filter_GeneratorPulse extends Filter {
                                 // fill the same block in the output image with a heartbeat monitor zigzag.
                                 // the height of the pulse is relative to the intensity.
                                 moveTo(out, (float) (x), (float) (y + halfstep), false);
-                                ++k;
-                                for (int block_x = x; block_x <= finalx; block_x += steps) {
-                                    float n = 1 + (k % 2) * -2;
-                                    ++k;
+                                n *= -1;
+                                for (int block_x = x; block_x <= finalx; block_x += zigZagSpacing) {
                                     moveTo(out, (float) (block_x), (float) (y + halfstep + pulse_size * n), false);
+                                    n *= -1;
                                 }
                                 moveTo(out, (float) (finalx), (float) (y + halfstep), false);
                             }
@@ -174,11 +328,10 @@ public class Filter_GeneratorPulse extends Filter {
                                 // fill the same block in the output image with a heartbeat monitor zigzag.
                                 // the height of the pulse is relative to the intensity.
                                 moveTo(out, (float) (x), (float) (y + halfstep), false);
-                                ++k;
-                                for (int block_x = x; block_x >= finalx; block_x -= steps) {
-                                    float n = 1 + (k % 2) * -2;
-                                    ++k;
+                                n *= -1;
+                                for (int block_x = x; block_x >= finalx; block_x -= zigZagSpacing) {
                                     moveTo(out, (float) (block_x), (float) (y + halfstep + pulse_size * n), false);
+                                	n *= -1;
                                 }
                                 moveTo(out, (float) (finalx), (float) (y + halfstep), false);
                             }
@@ -213,11 +366,10 @@ public class Filter_GeneratorPulse extends Filter {
                                 // fill the same block in the output image with a heartbeat monitor zigzag.
                                 // the height of the pulse is relative to the intensity.
                                 moveTo(out, (float) (x + halfstep), (float) (y), false);
-                                ++k;
-                                for (int block_y = y; block_y <= finaly; block_y += steps) {
-                                    float n = 1 + (k % 2) * -2;
-                                    ++k;
+                            	n *= -1;
+                                for (int block_y = y; block_y <= finaly; block_y += zigZagSpacing) {
                                     moveTo(out, (float) (x + halfstep + pulse_size * n), (float) (block_y), false);
+                                	n *= -1;
                                 }
                                 moveTo(out, (float) (x + halfstep), (float) (finaly), false);
                             }
@@ -244,11 +396,10 @@ public class Filter_GeneratorPulse extends Filter {
                                 // fill the same block in the output image with a heartbeat monitor zigzag.
                                 // the height of the pulse is relative to the intensity.
                                 moveTo(out, (float) (x + halfstep), (float) (y), false);
-                                ++k;
-                                for (int block_y = y; block_y >= finaly; block_y -= steps) {
-                                    float n = 1 + (k % 2) * -2;
-                                    ++k;
+                            	n *= -1;
+                                for (int block_y = y; block_y >= finaly; block_y -= zigZagSpacing) {
                                     moveTo(out, (float) (x + halfstep + pulse_size * n), (float) (block_y), false);
+                                	n *= -1;
                                 }
                                 moveTo(out, (float) (x + halfstep), (float) (finaly), false);
                             }
@@ -257,11 +408,6 @@ public class Filter_GeneratorPulse extends Filter {
                     }
                 }
             }
-
-            liftPen(out);
-            signName(out);
-            tool.writeMoveTo(out, 0, 0);
-        }
   }
 }
 
