6240d90b5a87b819301aeea5dd38c18ea618b279
Dan Royer <dan@marginallyclever.com>
2016-03-17 16:47:20 -0700

    added dutch, piglatin.
    I thought I'd added these already?

--- java/src/main/java/com/marginallyclever/makelangelo/settings/PanelAdjustPaper.java
+++ java/src/main/java/com/marginallyclever/makelangelo/settings/PanelAdjustPaper.java
@@ -5,8 +5,6 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.text.NumberFormat;
