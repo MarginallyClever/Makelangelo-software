ec8107a857a9ff80bd7d8168474242d88eb03775
Dan Royer <dan@marginallyclever.com>
2016-09-07 12:59:54 -0700

    Improved settings dialog (padding, hardware version selection)

--- java/src/main/java/com/marginallyclever/makelangeloRobot/settings/MakelangeloSettingsDialog.java
+++ java/src/main/java/com/marginallyclever/makelangeloRobot/settings/MakelangeloSettingsDialog.java
@@ -9,9 +9,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.Serializable;
 
+import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
@@ -38,10 +41,17 @@ implements ActionListener {
 
   protected JTabbedPane panes;
   protected JButton save, cancel;
+
+  // TODO pull these from the class listings in the package?
+  private int [] availableHardwareVersions={2,3,5};
+  private JComboBox<String> hardwareVersionChoices;
+  private String[] hardwareVersionNames={"2+","3+","5+"};
   
+  private JPanel modelPanel;
   protected PanelAdjustMachine panelAdjustMachine;
   protected PanelAdjustPaper panelAdjustPaper;
   protected DrawingTool panelAdjustPen;
+  private JPanel saveAndCancelPanel;
   
   protected int dialogWidth = 450;
   protected int dialogHeight = 500;
@@ -53,69 +63,134 @@ implements ActionListener {
   }
 
   
-  // settings menu
+  // display settings menu
   public void run() {
-    panes = new JTabbedPane();
-    
-    panelAdjustMachine = new PanelAdjustMachine(robot);
-    panelAdjustPaper = new PanelAdjustPaper(robot);
-    panelAdjustPen = robot.getSettings().getTool(0);
-    //panelAdjustTools = new PanelAdjustTools(translator,robot);
-    //panelSelectTool = new PanelSelectTool(translator,robot);
-    
-   	panes.addTab(Translator.get("MenuSettingsMachine"),panelAdjustMachine);
-    panes.addTab(Translator.get("MenuAdjustPaper"),panelAdjustPaper);
-    panes.addTab(Translator.get("MenuAdjustTool"),panelAdjustPen.getPanel());
-    //panes.addTab(translator.get("MenuAdjustTool"),panelAdjustTools);
-    //panes.addTab(translator.get("MenuSelectTool"),panelSelectTool);
-    
-	this.setLayout(new GridBagLayout());
-    GridBagConstraints d = new GridBagConstraints();
+	  this.setLayout(new GridBagLayout());
+	  GridBagConstraints d = new GridBagConstraints();
 
-    	// TODO: add choice of machine configuration, save config, save config as, delete config.
+	  buildModelPanel();
 
-    	// the panes for the selected machine configuration
-    	d.fill=GridBagConstraints.BOTH;
-    	d.gridx=0;
-    	d.gridy=0;
-    	d.weightx=1;
-    	d.weighty=1;
-    	//panes.setPreferredSize(new Dimension(dialogWidth,dialogHeight));
-    	this.add(panes,d);
-	
-        // save and cancel buttons
-    	cancel = new JButton(Translator.get("Cancel"));
-        save = new JButton(Translator.get("Save"));
+	  // hardware model settings
+	  panes = new JTabbedPane(JTabbedPane.TOP);
+	  panes.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+	  //panes.setPreferredSize(new Dimension(dialogWidth,dialogHeight));
 
-	    JPanel p = new JPanel(new GridBagLayout());
-	    GridBagConstraints c = new GridBagConstraints();
-		    c.anchor=GridBagConstraints.EAST;
-		    c.gridy=0;
-		    c.weightx=0;
-		    c.weighty=1;
-		    c.gridx=1; c.gridwidth=1; p.add(save,c);
-		    c.gridx=2; c.gridwidth=1; p.add(cancel,c);
-		    c.weightx=1;
-		    c.gridx=0; c.gridwidth=1; p.add(Box.createGlue(),c);
-	        this.getRootPane().setDefaultButton(save);
-	        cancel.addActionListener(this);
-	        save.addActionListener(this);
+	  rebuildTabbedPanes();
 
-    	d.weightx=1;
-    	d.weighty=0;
-	    d.gridy=1;
-	    d.fill=GridBagConstraints.HORIZONTAL;
-	    this.add(p,d);
+	  buildSaveAndCancel();
 
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		this.setLocation((screenSize.width - dialogWidth) / 2, (screenSize.height - dialogHeight) / 2);
-        this.pack();
-        this.setVisible(true);
+	  // now assemble the dialog
+	  d.fill=GridBagConstraints.HORIZONTAL;
+	  d.gridx=0;
+	  d.gridy=0;
+	  d.weightx=0;
+	  d.weighty=0;
+	  d.gridwidth=1;
+	  this.add(modelPanel, d);
+	  d.fill=GridBagConstraints.BOTH;
+	  d.gridy=1;
+	  d.weightx=1;
+	  d.weighty=1;
+	  this.add(panes,d);
+	  d.fill=GridBagConstraints.HORIZONTAL;
+	  d.gridy=2;
+	  d.weighty=0;
+	  this.add(saveAndCancelPanel,d);
+	  this.getRootPane().setDefaultButton(save);
+
+	  Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	  this.setLocation((screenSize.width - dialogWidth) / 2, (screenSize.height - dialogHeight) / 2);
+	  this.pack();
+	  this.setVisible(true);
+  }
+
+  // hardware model choice
+  private void buildModelPanel() {
+	  modelPanel = new JPanel(new GridBagLayout());
+	  modelPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 10));
+
+	  GridBagConstraints d = new GridBagConstraints();
+	  // the panes for the selected machine configuration
+	  d.fill=GridBagConstraints.BOTH;
+	  d.gridx=0;
+	  d.gridy=0;
+	  d.weightx=0;
+	  d.weighty=0;
+	  
+	  JLabel modelLabel = new JLabel(Translator.get("HardwareVersion")); 
+	  modelLabel.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 10));
+	  modelPanel.add(modelLabel, d);
+	  
+	  d.gridx=1;
+	  d.gridwidth=2;
+	  hardwareVersionNames = new String[availableHardwareVersions.length];
+	  for(int i=0;i<hardwareVersionNames.length;++i) {
+		  hardwareVersionNames[i] = "Makelangelo "+Integer.toString(availableHardwareVersions[i])+"+";
+	  }	  
+	  hardwareVersionChoices = new JComboBox<>(hardwareVersionNames);
+	  // set the default
+	  int hv = robot.getSettings().getHardwareVersion();
+	  String hvn = Integer.toString(hv);
+	  for(int i=0;i<hardwareVersionNames.length;++i) {
+		  if(hardwareVersionNames[i].startsWith("Makelangelo "+hvn)) {
+			  hardwareVersionChoices.setSelectedIndex(i);
+			  break;
+		  }
+	  }
+	  modelPanel.add(hardwareVersionChoices, d);
+	  hardwareVersionChoices.addActionListener(this);
+  }
+
+  // save and cancel buttons
+  private void buildSaveAndCancel() {
+	  cancel = new JButton(Translator.get("Cancel"));
+	  save = new JButton(Translator.get("Save"));
+
+	  saveAndCancelPanel = new JPanel(new GridBagLayout());
+	  GridBagConstraints c = new GridBagConstraints();
+	  c.anchor=GridBagConstraints.EAST;
+	  c.gridx=0;
+	  c.gridy=0;
+	  c.weightx=0;
+	  c.weighty=1;
+	  c.gridx=1; c.gridwidth=1; saveAndCancelPanel.add(save,c);
+	  c.gridx=2; c.gridwidth=1; saveAndCancelPanel.add(cancel,c);
+	  c.weightx=1;
+	  c.gridx=0; c.gridwidth=1; saveAndCancelPanel.add(Box.createGlue(),c);
+	  cancel.addActionListener(this);
+	  save.addActionListener(this);
+
+	  saveAndCancelPanel.setBorder(BorderFactory.createEmptyBorder(0, 10, 10, 10));
+  }
+  
+  
+  private void rebuildTabbedPanes() {
+	  panes.removeAll();
+	  
+	  panelAdjustMachine = new PanelAdjustMachine(robot);
+	  panes.addTab(Translator.get("MenuSettingsMachine"),panelAdjustMachine);
+
+	  panelAdjustPaper = new PanelAdjustPaper(robot);
+	  panes.addTab(Translator.get("MenuAdjustPaper"),panelAdjustPaper);
+
+	  panelAdjustPen = robot.getSettings().getTool(0);
+	  panes.addTab(Translator.get("MenuAdjustTool"),panelAdjustPen.getPanel());
+
+	  //panelAdjustTools = new PanelAdjustTools(translator,robot);
+	  //panes.addTab(translator.get("MenuAdjustTool"),panelAdjustTools);
+
+	  //panelSelectTool = new PanelSelectTool(translator,robot);
+	  //panes.addTab(translator.get("MenuSelectTool"),panelSelectTool);
   }
   
   public void actionPerformed(ActionEvent e) {
 	  Object src = e.getSource();
 	  
+	  if(src == hardwareVersionChoices) {
+		  int newChoice=availableHardwareVersions[hardwareVersionChoices.getSelectedIndex()];
+		  robot.getSettings().setHardwareVersion(newChoice);
+		  rebuildTabbedPanes();
+	  }
 	  if(src == save) {
 		  panelAdjustMachine.save();
 		  panelAdjustPaper.save();
