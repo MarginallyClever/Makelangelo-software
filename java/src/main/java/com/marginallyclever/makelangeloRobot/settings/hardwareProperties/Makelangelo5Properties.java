8a001800d102d3fce1287d3ad6372eefa56c8d45
Dan Royer <dan@marginallyclever.com>
2016-11-24 09:39:48 -0800

    returned m5 machine width to it's original size

--- java/src/main/java/com/marginallyclever/makelangeloRobot/settings/hardwareProperties/Makelangelo5Properties.java
+++ java/src/main/java/com/marginallyclever/makelangeloRobot/settings/hardwareProperties/Makelangelo5Properties.java
@@ -35,7 +35,6 @@ public class Makelangelo5Properties extends Makelangelo3Properties {
 		return false;
 	}
 	
-	public float getWidth() { return 648; }
 	public float getHeight() { return 1000; }
 
 	@Override
