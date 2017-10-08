fde57edf20441c2712e7f206df3e30a28001957e
Dan Royer <dan@marginallyclever.com>
2017-10-07 21:17:02 -0700

    improved rendering of m2 and m3

--- java/src/main/java/com/marginallyclever/makelangeloRobot/settings/hardwareProperties/Makelangelo2Properties.java
+++ java/src/main/java/com/marginallyclever/makelangeloRobot/settings/hardwareProperties/Makelangelo2Properties.java
@@ -5,10 +5,10 @@ import com.marginallyclever.makelangeloRobot.MakelangeloRobot;
 import com.marginallyclever.makelangeloRobot.settings.MakelangeloRobotSettings;
 
 public class Makelangelo2Properties implements MakelangeloHardwareProperties {
-	public final static float PEN_HOLDER_RADIUS_2= 6.0f; // cm
-	public final static float MOTOR_SIZE= 2.1f; // cm
-	public final static float PLOTTER_SIZE= 2.1f; // cm
-	public final static float FRAME_SIZE= 5.0f; // cm
+	public final static float PEN_HOLDER_RADIUS_2= 60f; // cm
+	public final static float MOTOR_SIZE= 21f; // cm
+	public final static float PLOTTER_SIZE= 21f; // cm
+	public final static float FRAME_SIZE= 50f; // cm
 
 	@Override
 	public int getVersion() {
@@ -116,15 +116,15 @@ public class Makelangelo2Properties implements MakelangeloHardwareProperties {
 		// mounting plate for PCB
 		gl2.glColor3f(1, 0.8f, 0.5f);
 		gl2.glBegin(GL2.GL_QUADS);
-		gl2.glVertex2d(-8, 5);
-		gl2.glVertex2d(+8, 5);
-		gl2.glVertex2d(+8, -5);
-		gl2.glVertex2d(-8, -5);
+		gl2.glVertex2d(-80, 50);
+		gl2.glVertex2d(+80, 50);
+		gl2.glVertex2d(+80, -50);
+		gl2.glVertex2d(-80, -50);
 		gl2.glEnd();
 
 		// wires to each motor
 		gl2.glBegin(GL2.GL_LINES);
-		float SPACING=0.2f;
+		float SPACING=2f;
 		float y=SPACING*-1.5f;
 		gl2.glColor3f(1, 0, 0);		gl2.glVertex2d(0, y);	gl2.glVertex2d(left, y);  y+=SPACING;
 		gl2.glColor3f(0, 1, 0);		gl2.glVertex2d(0, y);	gl2.glVertex2d(left, y);  y+=SPACING;
@@ -153,8 +153,8 @@ public class Makelangelo2Properties implements MakelangeloHardwareProperties {
 	protected void paintPenHolderToCounterweights(GL2 gl2, MakelangeloRobot robot) {
 		MakelangeloRobotSettings settings = robot.getSettings();
 		double dx, dy;
-		double gx = robot.getGondolaX() / 10;
-		double gy = robot.getGondolaY() / 10;
+		double gx = robot.getGondolaX();
+		double gy = robot.getGondolaY();
 
 		double top = settings.getLimitTop();
 		double bottom = settings.getLimitBottom();
@@ -163,7 +163,7 @@ public class Makelangelo2Properties implements MakelangeloHardwareProperties {
 
 		double mw = right - left;
 		double mh = top - settings.getLimitBottom();
-		double suggestedLength = Math.sqrt(mw * mw + mh * mh) + 5;
+		double suggestedLength = Math.sqrt(mw * mw + mh * mh) + 50;
 
 		dx = gx - left;
 		dy = gy - top;
@@ -192,18 +192,18 @@ public class Makelangelo2Properties implements MakelangeloHardwareProperties {
 		gl2.glVertex2d(right, top);
 		gl2.glVertex2d(gx, gy);
 
-		float bottleCenter = 0.8f + 0.75f;
+		float bottleCenter = 8f + 7.5f;
 
 		// belt from motor to counterweight left
-		gl2.glVertex2d(left - bottleCenter - 0.2, top);
-		gl2.glVertex2d(left - bottleCenter - 0.2, top - left_b);
-		gl2.glVertex2d(left - bottleCenter + 0.2, top);
-		gl2.glVertex2d(left - bottleCenter + 0.2, top - left_b);
+		gl2.glVertex2d(left - bottleCenter - 2, top);
+		gl2.glVertex2d(left - bottleCenter - 2, top - left_b);
+		gl2.glVertex2d(left - bottleCenter + 2, top);
+		gl2.glVertex2d(left - bottleCenter + 2, top - left_b);
 		// belt from motor to counterweight right
-		gl2.glVertex2d(right + bottleCenter - 0.2, top);
-		gl2.glVertex2d(right + bottleCenter - 0.2, top - right_b);
-		gl2.glVertex2d(right + bottleCenter + 0.2, top);
-		gl2.glVertex2d(right + bottleCenter + 0.2, top - right_b);
+		gl2.glVertex2d(right + bottleCenter - 2, top);
+		gl2.glVertex2d(right + bottleCenter - 2, top - right_b);
+		gl2.glVertex2d(right + bottleCenter + 2, top);
+		gl2.glVertex2d(right + bottleCenter + 2, top - right_b);
 		gl2.glEnd();
 
 		// gondola
@@ -218,19 +218,19 @@ public class Makelangelo2Properties implements MakelangeloHardwareProperties {
 		// counterweight left
 		gl2.glBegin(GL2.GL_LINE_LOOP);
 		gl2.glColor3f(0, 0, 1);
-		gl2.glVertex2d(left - bottleCenter - 1.5, top - left_b);
-		gl2.glVertex2d(left - bottleCenter + 1.5, top - left_b);
-		gl2.glVertex2d(left - bottleCenter + 1.5, top - left_b - 15);
-		gl2.glVertex2d(left - bottleCenter - 1.5, top - left_b - 15);
+		gl2.glVertex2d(left - bottleCenter - 15, top - left_b);
+		gl2.glVertex2d(left - bottleCenter + 15, top - left_b);
+		gl2.glVertex2d(left - bottleCenter + 15, top - left_b - 150);
+		gl2.glVertex2d(left - bottleCenter - 15, top - left_b - 150);
 		gl2.glEnd();
 
 		// counterweight right
 		gl2.glBegin(GL2.GL_LINE_LOOP);
 		gl2.glColor3f(0, 0, 1);
-		gl2.glVertex2d(right + bottleCenter - 1.5, top - right_b);
-		gl2.glVertex2d(right + bottleCenter + 1.5, top - right_b);
-		gl2.glVertex2d(right + bottleCenter + 1.5, top - right_b - 15);
-		gl2.glVertex2d(right + bottleCenter - 1.5, top - right_b - 15);
+		gl2.glVertex2d(right + bottleCenter - 15, top - right_b);
+		gl2.glVertex2d(right + bottleCenter + 15, top - right_b);
+		gl2.glVertex2d(right + bottleCenter + 15, top - right_b - 150);
+		gl2.glVertex2d(right + bottleCenter - 15, top - right_b - 150);
 		gl2.glEnd();
 
 		/*
--- java/src/main/java/com/marginallyclever/makelangeloRobot/settings/hardwareProperties/Makelangelo3Properties.java
+++ java/src/main/java/com/marginallyclever/makelangeloRobot/settings/hardwareProperties/Makelangelo3Properties.java
@@ -51,8 +51,8 @@ public class Makelangelo3Properties extends Makelangelo2Properties {
 		
 		// mounting plate for PCB
 		gl2.glColor3f(1,0.8f,0.5f);
-		float w =8;
-		float h = 5;
+		float w =80;
+		float h = 50;
 		gl2.glBegin(GL2.GL_QUADS);
 		gl2.glVertex2d(-w, h);
 		gl2.glVertex2d(+w, h);
@@ -62,7 +62,7 @@ public class Makelangelo3Properties extends Makelangelo2Properties {
 
 		// wires to each motor
 		gl2.glBegin(GL2.GL_LINES);
-		float SPACING=0.2f;
+		float SPACING=2f;
 		float y=SPACING*-1.5f;
 		gl2.glColor3f(1, 0, 0);		gl2.glVertex2d(0, y);	gl2.glVertex2d(left, y);  y+=SPACING;
 		gl2.glColor3f(0, 1, 0);		gl2.glVertex2d(0, y);	gl2.glVertex2d(left, y);  y+=SPACING;
@@ -77,8 +77,8 @@ public class Makelangelo3Properties extends Makelangelo2Properties {
 		gl2.glEnd();
 		
 		// RUMBA in v3 (135mm*75mm)
-		h = 7.5f/2;
-		w = 13.5f/2;
+		h = 75f/2;
+		w = 135f/2;
 		gl2.glColor3d(0.9,0.9,0.9);
 		gl2.glBegin(GL2.GL_QUADS);
 		gl2.glVertex2d(-w, h);
@@ -95,11 +95,11 @@ public class Makelangelo3Properties extends Makelangelo2Properties {
 	protected void renderLCD(GL2 gl2) {
 		// position
 		gl2.glPushMatrix();
-		gl2.glTranslated(-18, 0, 0);
+		gl2.glTranslated(-180, 0, 0);
 		
 		// mounting plate for LCD
-		float w = 8.0f;
-		float h = 5.0f;
+		float w = 80f;
+		float h = 50f;
 		gl2.glColor3f(1,0.8f,0.5f);
 		gl2.glBegin(GL2.GL_QUADS);
 		gl2.glVertex2d(-w, h);
@@ -109,8 +109,8 @@ public class Makelangelo3Properties extends Makelangelo2Properties {
 		gl2.glEnd();
 
 		// LCD red
-		w = 15.0f/2;
-		h = 5.6f/2;
+		w = 150f/2;
+		h = 56f/2;
 		gl2.glColor3f(0.8f,0.0f,0.0f);
 		gl2.glBegin(GL2.GL_QUADS);
 		gl2.glVertex2d(-w, h);
@@ -123,8 +123,8 @@ public class Makelangelo3Properties extends Makelangelo2Properties {
 		gl2.glPushMatrix();
 		gl2.glTranslated(-(2.6)/2, -0.771, 0);
 		
-		w = 9.8f/2;
-		h = 6.0f/2;
+		w = 98f/2;
+		h = 60f/2;
 		gl2.glColor3f(0,0.6f,0.0f);
 		gl2.glBegin(GL2.GL_QUADS);
 		gl2.glVertex2d(-w, h);
@@ -134,7 +134,7 @@ public class Makelangelo3Properties extends Makelangelo2Properties {
 		gl2.glEnd();
 
 		// LCD black
-		h = 4.0f/2;
+		h = 40f/2;
 		gl2.glColor3f(0,0,0);
 		gl2.glBegin(GL2.GL_QUADS);
 		gl2.glVertex2d(-w, h);
@@ -144,8 +144,8 @@ public class Makelangelo3Properties extends Makelangelo2Properties {
 		gl2.glEnd();
 
 		// LCD blue
-		h = 2.5f/2;
-		w = 7.5f/2;
+		h = 25f/2;
+		w = 75f/2;
 		gl2.glColor3f(0,0,0.7f);
 		gl2.glBegin(GL2.GL_QUADS);
 		gl2.glVertex2d(-w, h);
