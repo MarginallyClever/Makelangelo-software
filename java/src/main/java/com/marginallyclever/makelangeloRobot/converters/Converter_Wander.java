f338067392ea1156256e6981479b6e7e807aa246
Dan Royer <dan@marginallyclever.com>
2017-11-14 12:35:38 -0800

    Moved converter Wander to RandomLines, created new Wander
    based on older TSP solver, generates N random points into buckets, then
    sorts each bucket and draws out the Pretty Good (tm) line.

--- java/src/main/java/com/marginallyclever/makelangeloRobot/converters/Converter_Wander.java
+++ java/src/main/java/com/marginallyclever/makelangeloRobot/converters/Converter_Wander.java
@@ -1,17 +1,37 @@
 package com.marginallyclever.makelangeloRobot.converters;
 
 
+import java.awt.geom.Point2D;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.LinkedList;
+
 import javax.swing.JPanel;
 import com.marginallyclever.makelangeloRobot.TransformedImage;
+import com.marginallyclever.makelangelo.Log;
 import com.marginallyclever.makelangelo.Translator;
 import com.marginallyclever.makelangeloRobot.imageFilters.Filter_BlackAndWhite;
 
 
 // create random lines across the image.  Raise and lower the pen to darken the appropriate areas
 public class Converter_Wander extends ImageConverter {
-	static protected int numLines = 2500;
+	static protected int numLines = 9000;
+	
+	class Bucket {
+		public Point2D a,b;
+		public LinkedList<Point2D> unsortedPoints;
+		public LinkedList<Point2D> sortedPoints;
+		
+		public Bucket() {
+			a = new Point2D.Double();
+			b = new Point2D.Double();
+			unsortedPoints = new LinkedList<Point2D>();
+			sortedPoints = new LinkedList<Point2D>();
+		}
+	};
+
+	private LinkedList<Bucket> buckets;
+	
 	
 	@Override
 	public String getName() {
@@ -28,7 +48,6 @@ public class Converter_Wander extends ImageConverter {
 		Filter_BlackAndWhite bw = new Filter_BlackAndWhite(255);
 		TransformedImage img = bw.filter(sourceImage);
 
-
 		// Set up the conversion from image space to paper space, select the current tool, etc.
 		imageStart(out);
 		liftPen(out);
@@ -36,6 +55,7 @@ public class Converter_Wander extends ImageConverter {
 
 		float stepSize = machine.getPenDiameter()*5;
 		if (stepSize < 1) stepSize = 1;
+		float halfStep = stepSize/2;
 
 		// Color values are from 0...255 inclusive.  255 is white, 0 is black.
 		// Lift the pen any time the color value is > level (128 or more).
@@ -46,25 +66,100 @@ public class Converter_Wander extends ImageConverter {
 		float yTop    = (float)machine.getPaperTop()    * (float)machine.getPaperMargin();
 		float xLeft   = (float)machine.getPaperLeft()   * (float)machine.getPaperMargin();
 		float xRight  = (float)machine.getPaperRight()  * (float)machine.getPaperMargin();
-		double dy = yTop - yBottom-1;
-		double dx = xRight - xLeft-1;
 
-		liftPen(out);
-		moveTo(out,0,yTop,true);
+		// find numLines number of random points darker than the cutoff value
+		double height = yTop - yBottom-1;
+		double width = xRight - xLeft-1;
+		Point2D a = null;
+		
+		Log.info("Creating buckets in a Z pattern...");
+		buckets = new LinkedList<Bucket>();
+		int actualPoints=0;
+		double wMod = width/5.0;
+		double hMod = height/10.0;
+		double by,bx;
+		for(by=0;by<height;by+=hMod) {
+			for(bx=0;bx<width;bx+=wMod) {
+				Bucket b = new Bucket();
+				b.a.setLocation(xLeft+bx     , yBottom+by     );
+				b.b.setLocation(xLeft+bx+wMod, yBottom+by+hMod);
+				buckets.push(b);
+			}
+			by+=hMod;
+			for(bx=width-wMod;bx>=-1;bx-=wMod) {
+				Bucket b = new Bucket();
+				b.a.setLocation(xLeft+bx     , yBottom+by     );
+				b.b.setLocation(xLeft+bx+wMod, yBottom+by+hMod);
+				buckets.push(b);
+			}
+		}
 
-		double startPX = 0; 
-		double startPY = yTop;
+		Log.info("Finding points...");
+		for(int i=0;i<numLines;++i) {
+			level = 55 + 50.0 * (double)i / (double)numLines;  // set the cutoff value
+			int v, tries=0;
+			double endPX,endPY; 
+			do {
+				endPX = xLeft   + (Math.random() * width)+0.5; 
+				endPY = yBottom + (Math.random() * height)+0.5; 
+				v = img.sample(
+						endPX - halfStep, endPY - halfStep, 
+						endPX + halfStep, endPY + halfStep);
+				++tries;
+			} while(v>level && tries<1000);
+			if(tries==1000) break;  // ran out of points to try?
 
-		int i;
-		for(i=0;i<numLines;++i) {
-			level = 200.0 * (double)i / (double)numLines;
-			double endPX = xLeft   + (Math.random() * dx)+0.5; 
-			double endPY = yBottom + (Math.random() * dy)+0.5; 
+			int j;
+			for(j=0;j<buckets.size();++j) {
+				Bucket b = buckets.get(j);
+				if( b.a.getX()<=endPX && b.b.getX()>endPX && 
+				    b.a.getY()<=endPY && b.b.getY()>endPY ) {
+					b.unsortedPoints.addLast(new Point2D.Double(endPX,endPY));
+					++actualPoints;
+					break;
+				}
+			}
+		}
 
-			convertAlongLine(startPX,startPY,endPX,endPY,stepSize,level,img,out);
+		// sort the points by nearest neighbor first.
+		Log.info("Sorting "+actualPoints+" points...");
+		for(int j=0;j<buckets.size();++j) {
+			//Log.info(j+" of "+buckets.size()+ " has "+buckets.get(j).unsortedPoints.size()+" points");
+
+			// assume we start at the center of the image, for those machines with no pen up option.
+			a = new Point2D.Double(0,0);
 			
-			startPX = endPX;
-			startPY = endPY;
+			Bucket b = buckets.get(j);
+			if(!b.unsortedPoints.isEmpty()) {
+				while(!b.unsortedPoints.isEmpty()) {
+					double bestLen = Double.MAX_VALUE;
+					int bestI=0;
+					for(int i=0;i<b.unsortedPoints.size();++i) {
+						double len = a.distanceSq(b.unsortedPoints.get(i));
+						if(bestLen > len) {
+							bestLen = len;
+							bestI = i;
+						}
+					}
+					a = b.unsortedPoints.remove(bestI);
+					b.sortedPoints.addLast(a);
+				}
+			}
+		}
+		
+		
+		// draw the sorted list of points.
+		Log.info("Drawing points...");
+		liftPen(out);
+		boolean isFirst=true;
+
+		for(int j=0;j<buckets.size();++j) {
+			Bucket b = buckets.get(j);
+			while(!b.sortedPoints.isEmpty()) {
+				a = b.sortedPoints.pop();
+				moveTo(out,a.getX(),a.getY(),isFirst);
+				isFirst=false;
+			}
 		}
 
 		liftPen(out);
