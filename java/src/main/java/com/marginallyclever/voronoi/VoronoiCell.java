1768d10fad918b5424f0e8cbc8401032885f0d52
Dan Royer <dan@marginallyclever.com>
2017-12-21 14:57:53 -0800

    restored voronoicell sorting (broke saving)

--- java/src/main/java/com/marginallyclever/voronoi/VoronoiCell.java
+++ java/src/main/java/com/marginallyclever/voronoi/VoronoiCell.java
@@ -1,18 +1,23 @@
 package com.marginallyclever.voronoi;
 
-import java.awt.Point;
+import java.awt.geom.Point2D;
+
+import org.apache.batik.ext.awt.geom.Polygon2D;
 
 public class VoronoiCell implements Comparable<VoronoiCell> {
-	public Point centroid = new Point();
-	public Point oldCentroid = new Point();
+	public Point2D centroid = new Point2D.Double();
+	public Point2D oldCentroid = new Point2D.Double();
+	public Polygon2D region = new Polygon2D();
 	public double weight;
-	
+
 	@Override
 	public int compareTo(VoronoiCell arg0) {
-		int y = centroid.y - arg0.centroid.y;
-		if(y!=0) return y;
-		
-		return centroid.x - arg0.centroid.x;
+		int y1 = (int)Math.floor(arg0.centroid.getY()/10);
+		int y0 = (int)Math.floor(centroid.getY()/10);
+		if(y1 != y0) return y0-y1;
+		int x1 = (int)Math.floor(arg0.centroid.getX()/10);
+		int x0 = (int)Math.floor(centroid.getX()/10);
+		return x1-x0;
 	}
 	
 	public String toString() {
