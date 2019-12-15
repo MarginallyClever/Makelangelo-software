4b2ca2d2269573b3d6e608bc77bc191a792c7e70
Dan Royer <dan@marginallyclever.com>
2019-12-15 11:54:07 -0800

    Update Point2D.java

--- src/main/com/marginallyclever/convenience/Point2D.java
+++ src/main/com/marginallyclever/convenience/Point2D.java
@@ -3,6 +3,16 @@ package com.marginallyclever.convenience;
 public class Point2D {
 	public double x,y;
 	
+	public Point2D() {}
+	public Point2D(double x0,double y0) {
+		x=x0;
+		y=y0;
+	}
+	
+	public void set(double x0,double y0) {
+		x=x0;
+		y=y0;
+	}
 	public double lengthSquared() {
 		return x*x+y*y;
 	}
