5cf36f82d789395d135ccd3f1b07c74db89b571d
Dan Royer <dan@marginallyclever.com>
2019-12-25 11:35:23 -0800

    Create ArtPipeline.java

---
+++ src/main/com/marginallyclever/artPipeline/ArtPipeline.java
@@ -0,0 +1,6 @@
+package com.marginallyclever.artPipeline;
+
+// See https://www.marginallyclever.com/2019/12/lets-talk-about-where-makelangelo-software-is-going-in-2020/
+public class ArtPipeline {
+
+}
