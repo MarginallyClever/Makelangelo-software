d41333cef50f3d80f73816a3af9e4d6fd82d366c
Dan Royer <dan@marginallyclever.com>
2020-01-17 14:34:42 -0800

    fix for #262

--- src/main/com/marginallyclever/convenience/StringHelper.java
+++ src/main/com/marginallyclever/convenience/StringHelper.java
@@ -1,15 +1,27 @@
 package com.marginallyclever.convenience;
 
+import java.util.Locale;
+
 public class StringHelper {
 	
+	/**
+	 * format floats with 3 decimal places in US locale ONLY
+	 * @param arg0
+	 * @return
+	 */
 	static public String formatFloat(float arg0) {
 		//return Float.toString(roundOff(arg0));
-		return String.format("%.3f", arg0);
+		return String.format(Locale.US,"%.3f", arg0);
 	}
-	
+
+	/**
+	 * format doubles with 3 decimal places in US locale ONLY
+	 * @param arg0
+	 * @return
+	 */
 	static public String formatDouble(double arg0) {
 		//return Float.toString(roundOff(arg0));
-		return String.format("%.3f", arg0);
+		return String.format(Locale.US,"%.3f", arg0);
 	}
 
 	static public double parseNumber(String str) {
