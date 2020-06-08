f6b64db4462098b6fc6f38585852f8d5721f5f03
Dan Royer <dan@marginallyclever.com>
2020-06-08 11:37:07 -0700

    Update TransportLayer.java

--- src/main/com/marginallyclever/communications/TransportLayer.java
+++ src/main/com/marginallyclever/communications/TransportLayer.java
@@ -2,7 +2,7 @@ package com.marginallyclever.communications;
 
 /**
  * Lists available connections of a type (serial, ethernet, smoke signals, etc) and opens a connection of that type
- *
+ * See also "OSI Layers" (Google it)
  * @author Dan
  * @since v7.1.0.0
  */
