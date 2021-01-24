c365d03484075bb32e5031defea261e802d0cb17
Dan Royer <dan@marginallyclever.com>
2021-01-23 22:44:20 -0800

    fixed zoom

--- src/main/com/marginallyclever/makelangelo/preview/Camera.java
+++ src/main/com/marginallyclever/makelangelo/preview/Camera.java
@@ -10,12 +10,12 @@ public class Camera {
 	public static final float CAMERA_ZNEAR = 10.0f;
 	public static final double ZOOM_STEP_SIZE = 3.5d / 4.0d;
 
-
 	// scale + position
 	private double offsetX = 0.0;
 	private double offsetY = 0.0;
 	private double zoom = 1.0;
-	
+
+	// window size (for aspect ratio?)
 	private double width, height;
 
 
@@ -48,20 +48,12 @@ public class Camera {
 
 	
 	// scale the picture of the robot to fake a zoom.
-	public void zoomToFitPaper() {
-		/*
-		if (robot != null) {
-			double widthOfPaper = robot.getSettings().getPaperWidth();
-			double heightOfPaper = robot.getSettings().getPaperHeight();
-			double drawPanelWidthZoom = widthOfPaper;
-			double drawPanelHeightZoom = heightOfPaper;
-
-			if (windowWidth < windowHeight) {
-				zoom = (drawPanelWidthZoom > drawPanelHeightZoom ? drawPanelWidthZoom : drawPanelHeightZoom);
-			} else {
-				zoom = (drawPanelWidthZoom < drawPanelHeightZoom ? drawPanelWidthZoom : drawPanelHeightZoom);
-			}
-		}*/
+	public void zoomToFit(double w,double h) {
+		if (w < h) {
+			zoom = (w > h ? w : h);
+		} else {
+			zoom = (w < h ? w : h);
+		}
 		
 		offsetX = 0;
 		offsetY = 0;
