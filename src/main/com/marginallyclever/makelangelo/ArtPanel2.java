81a1c9ccd3a6019e4a424f051498bc80f6f4f1d3
Dan Royer <dan@marginallyclever.com>
2021-01-27 14:23:49 -0800

    7.24.2
    fix minimum java version

---
+++ src/main/com/marginallyclever/makelangelo/ArtPanel2.java
@@ -0,0 +1,57 @@
+package com.marginallyclever.makelangelo;
+
+import java.awt.Dimension;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import com.marginallyclever.convenience.log.Log;
+import com.marginallyclever.makelangelo.select.SelectPanel;
+
+public class ArtPanel2 {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1L;
+	protected JFrame parentFrame;
+	
+	public ArtPanel2() {
+		super();
+	}
+	
+	public JPanel buildPanel(JFrame frame) {
+		parentFrame = frame;
+		
+		JPanel rootPanel = new JPanel();
+		rootPanel.setBorder(BorderFactory.createLoweredBevelBorder());
+		
+		SelectPanel firstLayer = new SelectPanel();
+		rootPanel.add(firstLayer);
+		
+		// add image manipulators
+		
+		return rootPanel;
+	}
+	
+	/**
+	 * Run this to visually examine every panel element and how they look in next to each other.
+	 * @param args ignored
+	 */
+	public static void main(String[] args) {
+		Log.start();
+		Translator.start();
+		JFrame frame = new JFrame("Art Pipeline Panel 2");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		ArtPanel2 ap = new ArtPanel2();
+		
+		JPanel p = ap.buildPanel(frame);
+		
+		frame.setPreferredSize(new Dimension(800,600));
+		frame.add(p);
+		
+		frame.pack();
+		frame.setVisible(true);
+	} 
+}
