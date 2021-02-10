89ea9bcf0da25bc98139d4436ace07ce715cbffc
Dan Royer <dan@marginallyclever.com>
2021-02-10 13:07:38 -0800

    Update ListAllTranslations.java

--- src/main/com/marginallyclever/util/ListAllTranslations.java
+++ src/main/com/marginallyclever/util/ListAllTranslations.java
@@ -5,11 +5,14 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 /**
- * Scan all java files in the project, find instances of Translator.get("AAAAA") and return the AAAAA part
+ * Scan all java files in the project, find instances of Translator.get("AAAAA") and return the AAAAA part,
+ * XML formatted, to the System.out.  You can copy/paste the results to an XML file and start filling in the values.
+ * Don't forget to change the meta-language and meta-author values!
  * @author Dan Royer
  * @since 7.25.0
  *
@@ -38,10 +41,8 @@ public class ListAllTranslations {
 	        	String fullPath = path + File.separator + filename;
 	            if(filename.endsWith(".java")) {
 	            	boolean found=false;
-	                Scanner kb = new Scanner(System.in);
-	                List<String> lines;
 					try {
-						lines = Files.readAllLines(Paths.get(fullPath));
+						List<String> lines = Files.readAllLines(Paths.get(fullPath));
 		                for (String line : lines) {
 		                    while(line.contains(TRANSLATOR_HEAD)) {
 		                    	if(!found) {
@@ -52,7 +53,7 @@ public class ListAllTranslations {
 		                    	int last = line.indexOf(TRANSLATOR_TAIL, first);
 		                    	String result = line.substring(first+TRANSLATOR_HEAD.length(),last);
 		                    	if(!keys.contains(result)) {
-		                    		System.out.println("\t"+result);
+		                    		System.out.println("\t<string><key>"+result+"</key><value></value></string>");
 		                    		keys.add(result);
 		                    	}
 		                        line = line.substring(last+TRANSLATOR_TAIL.length());
@@ -69,9 +70,22 @@ public class ListAllTranslations {
 	public static void main(String[] args) {
 		String cwd = System.getProperty("user.dir") + File.separator + "src"+ File.separator +"main";
 		
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+		LocalDateTime now = LocalDateTime.now();
+		
+		System.out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+		System.out.println("<!DOCTYPE language>");
+		System.out.println("<language>");
+		System.out.println("\t<meta>");
+		System.out.println("\t\t<name>English</name>");
+		System.out.println("\t\t<author>Dan Royer (dan@marginallyclever.com)</author>");
+		System.out.println("\t\t<when>"+dtf.format(now)+"</when>");
+		System.out.println("\t</meta>");
 		ListAllTranslations lat = new ListAllTranslations(cwd);
 		File f = new File(cwd);
 		lat.listFilesForFolder(f);
+
+		System.out.println("</language>");
 	}
 	
 }
