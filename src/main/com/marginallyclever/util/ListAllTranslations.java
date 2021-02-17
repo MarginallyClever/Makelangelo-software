405415923fe6c24612863f5eb520bd29f42a4e2a
Dan Royer <dan@marginallyclever.com>
2021-02-17 10:42:15 -0800

    more format options

--- src/main/com/marginallyclever/util/ListAllTranslations.java
+++ src/main/com/marginallyclever/util/ListAllTranslations.java
@@ -10,7 +10,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 /**
- * Scan all java files in the project, find instances of Translator.get("AAAAA") and return the AAAAA part,
+ * Scan all java files in the project, find instances of "Translator.get"+"(\"AAAAA\")" and return the AAAAA part,
  * XML formatted, to the System.out.  You can copy/paste the results to an XML file and start filling in the values.
  * Don't forget to change the meta-language and meta-author values!
  * @author Dan Royer
@@ -21,8 +21,19 @@ public class ListAllTranslations {
 	private final static String TRANSLATOR_HEAD = "Translator.get(\"";
 	private final static String TRANSLATOR_TAIL = "\")";
 
+	private class TranslationKey {
+		public String myKey;
+		public String mySrcFile;
+		//public String value;
+		
+		public TranslationKey(String key,String srcFile) {
+			myKey=key;
+			mySrcFile=srcFile;
+		}
+	};
+	private ArrayList<TranslationKey> keys = new ArrayList<TranslationKey>();
+	
 	private String rootPath;
-	private ArrayList<String> keys = new ArrayList<String>();
 		
 	
 	ListAllTranslations(String root) {
@@ -30,7 +41,46 @@ public class ListAllTranslations {
 		rootPath = root;
 	}
 	
-	public void listFilesForFolder(File folder) {
+	private void emitXML() {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+		LocalDateTime now = LocalDateTime.now();
+		
+		System.out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+		System.out.println("<!DOCTYPE language>");
+		System.out.println("<language>");
+		System.out.println("\t<meta>");
+		System.out.println("\t\t<name>English</name>");
+		System.out.println("\t\t<author>Dan Royer (dan@marginallyclever.com)</author>");
+		System.out.println("\t\t<when>"+dtf.format(now)+"</when>");
+		System.out.println("\t</meta>");
+
+		for(TranslationKey tk : keys) {
+			System.out.println("\t<string>");
+			System.out.println("\t\t<key>"+tk.myKey+"</key>");
+			System.out.println("\t\t<value></value>");
+			System.out.println("\t\t<src>"+tk.mySrcFile+"</src>");
+			System.out.println("\t</string>");
+		}
+
+		System.out.println("</language>");
+		
+	}
+	
+	private void emitCSV() {
+		System.out.println("File\tKey\tValue");
+		for(TranslationKey tk : keys) {
+			System.out.println(tk.mySrcFile+"\t"+tk.myKey+"\t ");
+		}
+	}
+	
+	private boolean keyIsUnique(String k) {
+		for(TranslationKey tk : keys) {
+			if(tk.myKey.contentEquals(k)) return false;
+		}
+		return true;
+	}
+	
+	private void listFilesForFolder(File folder) {
 		String path = folder.getAbsolutePath();
 		
 	    for (final File fileEntry : folder.listFiles()) {
@@ -39,6 +89,7 @@ public class ListAllTranslations {
 	        } else {
 	        	String filename = fileEntry.getName();
 	        	String fullPath = path + File.separator + filename;
+	        	String srcFile="";
 	            if(filename.endsWith(".java")) {
 	            	boolean found=false;
 					try {
@@ -46,15 +97,15 @@ public class ListAllTranslations {
 		                for (String line : lines) {
 		                    while(line.contains(TRANSLATOR_HEAD)) {
 		                    	if(!found) {
-		            	            System.out.println("<!-- "+fullPath.substring(rootPath.length())+" -->");
+		                    		srcFile = fullPath.substring(rootPath.length());
 		            	            found=true;
 		                    	}
 		                    	int first = line.indexOf(TRANSLATOR_HEAD);
 		                    	int last = line.indexOf(TRANSLATOR_TAIL, first);
 		                    	String result = line.substring(first+TRANSLATOR_HEAD.length(),last);
-		                    	if(!keys.contains(result)) {
-		                    		System.out.println("\t<string><key>"+result+"</key><value></value></string>");
-		                    		keys.add(result);
+		                    	if(keyIsUnique(result)) {
+		                    		TranslationKey tk = new TranslationKey(result,srcFile);
+		            	            keys.add(tk);
 		                    	}
 		                        line = line.substring(last+TRANSLATOR_TAIL.length());
 		                    }
@@ -70,22 +121,19 @@ public class ListAllTranslations {
 	public static void main(String[] args) {
 		String cwd = System.getProperty("user.dir") + File.separator + "src"+ File.separator +"main";
 		
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
-		LocalDateTime now = LocalDateTime.now();
-		
-		System.out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-		System.out.println("<!DOCTYPE language>");
-		System.out.println("<language>");
-		System.out.println("\t<meta>");
-		System.out.println("\t\t<name>English</name>");
-		System.out.println("\t\t<author>Dan Royer (dan@marginallyclever.com)</author>");
-		System.out.println("\t\t<when>"+dtf.format(now)+"</when>");
-		System.out.println("\t</meta>");
 		ListAllTranslations lat = new ListAllTranslations(cwd);
 		File f = new File(cwd);
 		lat.listFilesForFolder(f);
-
-		System.out.println("</language>");
+		
+		int choice=0;  // default csv
+		for( String a : args ) {
+			if(a.contentEquals("csv")) choice=0;
+			if(a.contentEquals("xml")) choice=1;
+		}
+		switch(choice) {
+		case 0:  lat.emitCSV();  break;
+		case 1:  lat.emitXML();  break;
+		}
 	}
 	
 }
