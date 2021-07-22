f438106ad3bd50bd07e37cff4c35212e0b4e0b64
Dan Royer <dan@marginallyclever.com>
2021-07-22 13:30:43 -0700

    Create MakelangeloRobotEvent.java

---
+++ src/main/com/marginallyclever/makelangeloRobot/MakelangeloRobotEvent.java
@@ -0,0 +1,23 @@
+package com.marginallyclever.makelangeloRobot;
+
+public class MakelangeloRobotEvent {
+	public MakelangeloRobot subject;
+	public int type;
+	public Object extra;
+	
+	public MakelangeloRobotEvent(int eventType, MakelangeloRobot robot) {
+		type=eventType;
+		subject = robot;
+		extra=null;
+	}
+
+	public MakelangeloRobotEvent(int eventType, MakelangeloRobot robot, Object extraData) {
+		this(eventType,robot);
+		extra=extraData;
+	}
+
+	public static final int DISCONNECT = 0;
+	public static final int CONNECTION_READY = 1;
+	public static final int BAD_FIRMWARE = 2;
+	public static final int BAD_HARDWARE = 3;
+}
