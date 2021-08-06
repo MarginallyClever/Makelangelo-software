9e57900ac5701a84bcf3a9c02da476e91f83e7c4
Dan Royer <dan@marginallyclever.com>
2021-08-06 10:59:45 -0700

    Create ImageConverterThread.java
    moving swingworker stuff into a dedicated class for clarity

---
+++ src/main/com/marginallyclever/artPipeline/loadAndSave/ImageConverterThread.java
@@ -0,0 +1,56 @@
+package com.marginallyclever.artPipeline.loadAndSave;
+
+import javax.swing.SwingWorker;
+
+import com.marginallyclever.artPipeline.converters.ImageConverter;
+import com.marginallyclever.convenience.log.Log;
+
+public class ImageConverterThread extends SwingWorker<Void, Void> {
+	private ImageConverter chosenConverter;
+	private String name;
+	private LoadAndSaveImage listener;
+
+	public ImageConverterThread(LoadAndSaveImage createdBy,ImageConverter converter,String name) {
+		listener = createdBy;
+		chosenConverter = converter;
+		this.name = name;
+		
+		chosenConverter.setSwingWorker(this);
+	}
+	
+	@Override
+	protected Void doInBackground() throws Exception {
+		Log.message("Starting "+name);
+
+		int loopCount=0;
+		while(!isCancelled() && chosenConverter.iterate()) {
+			loopCount++;
+			try {
+				Thread.sleep(5);
+			} catch (InterruptedException e) {
+				Log.message("Thread interrupted.");
+				break;
+			}
+		}
+
+		Log.message("Ending "+name+" after "+loopCount+" loop(s).");
+		
+		return null;
+	}
+	
+	@Override
+	public void done() {
+		String state;
+
+		if(isCancelled()) {
+			state = "Cancelled";
+		} else {
+			chosenConverter.finish();
+			state = "Finished";
+		}
+		
+		Log.message(state+" thread "+name);
+
+		listener.done(this);
+	}
+}
