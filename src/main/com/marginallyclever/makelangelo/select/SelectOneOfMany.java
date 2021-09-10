df3fdd6de96e1bb077affc3c2fc4ac6728252169
Dan Royer <dan@marginallyclever.com>
2021-09-10 13:48:46 -0700

    fix for #342

--- src/main/com/marginallyclever/makelangelo/select/SelectOneOfMany.java
+++ src/main/com/marginallyclever/makelangelo/select/SelectOneOfMany.java
@@ -1,10 +1,10 @@
 package com.marginallyclever.makelangelo.select;
 
 import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
+import java.util.Arrays;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.border.EmptyBorder;
@@ -15,26 +15,42 @@ import javax.swing.border.EmptyBorder;
  */
 public class SelectOneOfMany extends Select {
 	private JLabel label;
-	private JComboBox<String> field;
-
-	public SelectOneOfMany(String labelKey,String[] options,int defaultValue) {
+	private JComboBox<String> field = new JComboBox<String>();
+	private DefaultComboBoxModel<String> model = (DefaultComboBoxModel<String>)field.getModel();
+	
+	public SelectOneOfMany(String labelKey) {
 		super();
 		
 		label = new JLabel(labelKey,JLabel.LEADING);
-		field = new JComboBox<String>(options); 
-		field.setSelectedIndex(defaultValue);
 
 		final Select parent = this;
-		field.addActionListener(new ActionListener() {
-        	@Override
-			public void actionPerformed(ActionEvent e) {
-				notifyPropertyChangeListeners(new PropertyChangeEvent(parent, "value", null, field.getSelectedIndex()));
-			}
+		field.addActionListener((e)->{
+			notifyPropertyChangeListeners(new PropertyChangeEvent(parent, "value", null, field.getSelectedIndex()));
 		});
 
+		panel.removeAll();
 		panel.setBorder(new EmptyBorder(0,0,0,1));
 		panel.add(label,BorderLayout.LINE_START);
 		panel.add(field,BorderLayout.LINE_END);
+	}
+	
+	public SelectOneOfMany(String labelKey,String[] options,int defaultValue) {
+		this(labelKey);
+		
+		setNewList(options);
+		field.setSelectedIndex(defaultValue);
+	}
+	
+	public void removeAll() {
+		model.removeAllElements();
+	}
+	
+	public void addItem(String s) {
+		model.addElement(s);
+	}
+	
+	public void removeItem(String s) {
+		model.removeElement(s);
 	}
 	
 	public void setReadOnly() {
@@ -51,5 +67,10 @@ public class SelectOneOfMany extends Select {
 
 	public void setSelectedIndex(int index) {
 		field.setSelectedIndex(index);
+	}
+
+	public void setNewList(String[] list) {
+		model.removeAllElements();
+		model.addAll(Arrays.asList(list));
 	}
 }
