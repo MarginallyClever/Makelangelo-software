893f0564739d1636cb0ee047d0309c8b61dd2024
Dan Royer <dan@marginallyclever.com>
2021-09-21 10:15:15 -0700

    Create ConversationLog.java

---
+++ src/main/com/marginallyclever/communications/ConversationLog.java
@@ -0,0 +1,11 @@
+package com.marginallyclever.communications;
+
+import javax.swing.DefaultListModel;
+
+public class ConversationLog extends DefaultListModel<ConversationEvent> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6864256418767539292L;
+}
