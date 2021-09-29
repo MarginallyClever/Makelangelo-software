8e34888f207dbf50ff637b9260eab83a9fc015be
Dan Royer <dan@marginallyclever.com>
2021-09-29 12:25:12 -0700

    Update CartesianButtons.java

--- src/main/com/marginallyclever/makelangeloRobot/CartesianButtons.java
+++ src/main/com/marginallyclever/makelangeloRobot/CartesianButtons.java
@@ -19,7 +19,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionListener;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 
