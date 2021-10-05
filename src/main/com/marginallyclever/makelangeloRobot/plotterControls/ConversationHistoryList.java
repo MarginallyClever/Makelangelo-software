680762ba54cdde6e93163832f20289d3eebbed16
Dan Royer <dan@marginallyclever.com>
2021-10-05 13:15:15 -0700

    Create ConversationHistoryList.java

---
+++ src/main/com/marginallyclever/makelangeloRobot/plotterControls/ConversationHistoryList.java
@@ -0,0 +1,151 @@
+package com.marginallyclever.makelangeloRobot.plotterControls;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JToolBar;
+import javax.swing.ListCellRenderer;
+import javax.swing.ListSelectionModel;
+import javax.swing.UIManager;
+import javax.swing.border.EtchedBorder;
+import javax.swing.event.ListSelectionListener;
+
+import com.marginallyclever.convenience.log.Log;
+
+public class ConversationHistoryList extends JPanel {
+	private static final long serialVersionUID = 6287436679006933618L;
+	private DefaultListModel<ConversationEvent> listModel = new DefaultListModel<ConversationEvent>();
+	private JList<ConversationEvent> listView = new JList<ConversationEvent>(listModel);
+	private JFileChooser chooser = new JFileChooser();
+
+	private JButton bClear = new JButton("Clear");
+	private JButton bSave = new JButton("Save");
+
+	
+	public ConversationHistoryList() {
+		super();
+		createCellRenderingSystem();
+
+		JScrollPane scrollPane = new JScrollPane(listView);
+		scrollPane.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
+		
+		listView.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		listView.setPreferredSize(new Dimension(500,500));
+
+		this.setBorder(BorderFactory.createTitledBorder(ConversationHistoryList.class.getSimpleName()));
+		this.setLayout(new BorderLayout());
+		this.add(getToolBar(), BorderLayout.PAGE_START);
+		this.add(scrollPane, BorderLayout.CENTER);
+	}
+	
+	private JToolBar getToolBar() {
+		JToolBar bar = new JToolBar();
+		bar.setRollover(true);
+
+		bar.add(bClear);
+		bar.add(bSave);
+		
+		bClear.addActionListener( (e) -> runNewAction() );
+		bSave.addActionListener( (e) -> runSaveAction() );
+		
+		return bar;
+	}
+
+	private void createCellRenderingSystem() {
+		listView.setCellRenderer(new ListCellRenderer<ConversationEvent>() {
+			private DefaultListCellRenderer defaultRenderer = new DefaultListCellRenderer(); 
+			
+			@Override
+			public Component getListCellRendererComponent(JList<? extends ConversationEvent> list,
+					ConversationEvent value, int index, boolean isSelected, boolean cellHasFocus) {
+				Component c = defaultRenderer.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+				
+				if(c instanceof JLabel) {
+					JLabel jc = (JLabel)c;
+					jc.setText(value.toString());
+					if(!value.whoSpoke.contentEquals("You")) {
+						jc.setForeground(Color.BLUE);
+					}
+				}
+				return c;
+			}
+			
+		});
+	}
+
+	private void runNewAction() {
+		listModel.clear();
+	}
+	
+	private void runSaveAction() {
+		if(chooser.showSaveDialog(this) == JFileChooser.APPROVE_OPTION) {
+			try {
+				saveFile(chooser.getSelectedFile());
+			} catch (IOException e1) {
+				JOptionPane.showMessageDialog(this, e1.getLocalizedMessage(),"runSaveAction error",JOptionPane.ERROR_MESSAGE);
+				e1.printStackTrace();
+			}
+		}
+	}
+	
+	private void saveFile(File file) throws IOException {
+		BufferedWriter fileWriter = new BufferedWriter(new FileWriter(file));
+		int size=listModel.getSize();
+		for(int i=0;i<size;++i) {
+			String str = listModel.get(i).toString();
+			if(!str.endsWith("\n")) str+="\n";
+			fileWriter.write(str);
+		}
+		fileWriter.close();
+	}
+	
+	public void clear() {
+		runNewAction();
+	}
+	
+	public void addListSelectionListener(ListSelectionListener listener) {
+		listView.addListSelectionListener(listener);
+	}
+
+	public int getSelectedIndex() {
+		return listView.getSelectedIndex();
+	}
+
+	public String getSelectedValue() {
+		return listView.getSelectedValue().toString();
+	}
+
+	public void addElement(String src,String str) {
+		listModel.addElement(new ConversationEvent(src, str));
+		listView.ensureIndexIsVisible(listModel.getSize()-1);
+	}
+	
+	public static void main(String[] args) {
+		Log.start();
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch(Exception e) {}
+		JFrame frame = new JFrame(ConversationHistoryList.class.getSimpleName());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(new ConversationHistoryList());
+		frame.pack();
+		frame.setVisible(true);
+	}
+}
