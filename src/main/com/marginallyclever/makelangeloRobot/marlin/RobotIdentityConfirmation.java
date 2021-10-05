4d9ddb9b145508ec352d29c3402713b737b377fe
Dan Royer <dan@marginallyclever.com>
2021-10-05 09:52:27 -0700

    reduced

--- src/main/com/marginallyclever/makelangeloRobot/marlin/RobotIdentityConfirmation.java
+++ src/main/com/marginallyclever/makelangeloRobot/marlin/RobotIdentityConfirmation.java
@@ -2,16 +2,99 @@ package com.marginallyclever.makelangeloRobot.marlin;
 
 import java.util.ArrayList;
 
+import com.marginallyclever.communications.NetworkSessionEvent;
 import com.marginallyclever.communications.NetworkSessionListener;
+import com.marginallyclever.convenience.log.Log;
 
-public abstract class RobotIdentityConfirmation implements NetworkSessionListener {
+public class RobotIdentityConfirmation implements NetworkSessionListener {
+	private static final String HELLO = "Marlin";
+	private static final String EXPECTED_FIRMWARE_VERSION = "2.0.9"; // must match the version in the the firmware EEPROM
+	private static final String AUTHOR = "Author: (Marginally Clever,";
+	
+	private boolean firmwareVersionChecked = false;
+	private boolean hardwareVersionChecked = false;
+	private String versionFound;
 	private boolean identityConfirmed;
 	private boolean portConfirmed;
 	
 	
-	abstract public void reset();
+	public RobotIdentityConfirmation() {
+		super();
+		reset();
+	} 
 	
-	abstract public void start();
+	public void reset() {
+		setIdentityConfirmed(false);
+		setPortConfirmed(false);
+		firmwareVersionChecked = false;
+		hardwareVersionChecked = false;
+	}
+	
+	public void start() {}
+
+	public String getVersion() {
+		return versionFound;
+	}
+	
+	@Override
+	public void networkSessionEvent(NetworkSessionEvent evt) {	
+		if(evt.flag == NetworkSessionEvent.DATA_RECEIVED) {	
+			if(!getIdentityConfirmed()) {
+				if(whenTheIntroductionsFinishOK((String)evt.data)) {
+					setIdentityConfirmed(true);
+					notifyListeners(new RobotIdentityEvent(this,RobotIdentityEvent.IDENTITY_CONFIRMED,null));
+				}
+			}
+		}
+	}
+
+	private boolean whenTheIntroductionsFinishOK(String data) {
+		if(!getPortConfirmed()) {
+			if(doISeeAHello(data)) {
+				setPortConfirmed(true);
+			}
+		}
+		
+		if(!hardwareVersionChecked) {
+			String version=doISeeAHardwareVersion(data);
+			if(version!=null) {
+				hardwareVersionChecked = true;
+				versionFound = version;
+			}
+		}
+		
+		return getPortConfirmed() && firmwareVersionChecked && hardwareVersionChecked;
+	}
+
+	private String doISeeAHardwareVersion(String data) {
+		int a = data.lastIndexOf(AUTHOR); 
+		if(a >= 0) {
+			int b = data.lastIndexOf(")");
+			String machineType = data.substring(a+AUTHOR.length(),b).trim();
+			Log.message("Hardware version check found "+machineType);
+			return machineType;
+		}
+
+		return null;
+	}
+
+	private boolean doISeeAHello(String data) {
+		if (data.lastIndexOf(HELLO) >= 0) {
+			Log.message("Heard "+HELLO+".  Hello found.");
+			// which firmware is this?
+			String afterHello = data.substring(data.lastIndexOf(HELLO) + HELLO.length()).trim();
+			if(afterHello.compareTo(EXPECTED_FIRMWARE_VERSION)>=0) {
+				Log.message("OK");
+				firmwareVersionChecked=true;
+				return true;
+			} else {
+				Log.message("BAD");
+				notifyListeners(new RobotIdentityEvent(this,RobotIdentityEvent.BAD_FIRMWARE,afterHello));
+			}
+		}
+		
+		return false;
+	}
 
 	protected void setIdentityConfirmed(boolean state) {
 		identityConfirmed=state;
