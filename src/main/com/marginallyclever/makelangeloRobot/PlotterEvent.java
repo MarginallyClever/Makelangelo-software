a4398ef7bcd7e768440fb44054375a4383716abd
Dan Royer <dan@marginallyclever.com>
2021-10-05 16:26:33 -0700

    strip out meaningless events

--- src/main/com/marginallyclever/makelangeloRobot/PlotterEvent.java
+++ src/main/com/marginallyclever/makelangeloRobot/PlotterEvent.java
@@ -1,6 +1,7 @@
 package com.marginallyclever.makelangeloRobot;
 
 public class PlotterEvent {
+	/*
 	// robot has disconnected
 	public static final int DISCONNECT = 0;
 	// robot is connected and identity confirmed.
@@ -13,16 +14,21 @@ public class PlotterEvent {
 	public static final int START = 4;
 	// stopping
 	public static final int STOP = 5;
-	// requests tool change. extra is a 24 bit RGB value.
-	public static final int TOOL_CHANGE = 6;
 	// extra is the line number last sent to the robot.
-	public static final int PROGRESS_SOFAR = 7;
+	public static final int PROGRESS_SOFAR = 6;
+	*/
+	// requests tool change. extra is a 24 bit RGB value.
+	public static final int TOOL_CHANGE = 7;
 	// new GCode commands have been given to the robot.
 	public static final int NEW_GCODE = 8;
 	// motor engaged status has changed.  extra is a boolean with the new state.
 	public static final int MOTORS_ENGAGED = 9;
 	// robot has homed and is safe to drive.
 	public static final int HOME_FOUND = 10;
+	// robot has moved.
+	public static final int POSITION = 11;
+	// robot has lifted pen.  extra is true if pen is up, false if pen is down.
+	public static final int PEN_UP = 12;
 	
 	public Plotter subject;
 	public int type;
