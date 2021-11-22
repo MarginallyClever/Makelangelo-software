b413f560d6afb06df2395c424fd7c0b8629618d9
Dan Royer <dan@marginallyclever.com>
2021-11-22 10:38:19 -0800

    added a main() to run the dialog as a test

--- src/main/com/marginallyclever/makelangelo/makelangeloSettingsPanel/LanguagePreferences.java
+++ src/main/com/marginallyclever/makelangelo/makelangeloSettingsPanel/LanguagePreferences.java
@@ -1,10 +1,17 @@
 package com.marginallyclever.makelangelo.makelangeloSettingsPanel;
 
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.UIManager;
 
+import com.marginallyclever.convenience.CommandLineOptions;
+import com.marginallyclever.convenience.log.Log;
+import com.marginallyclever.makelangelo.ScaleTurtlePanel;
 import com.marginallyclever.makelangelo.Translator;
 import com.marginallyclever.makelangelo.select.SelectOneOfMany;
 import com.marginallyclever.makelangelo.select.SelectPanel;
+import com.marginallyclever.makelangelo.turtle.Turtle;
+import com.marginallyclever.util.PreferencesHelper;
 
 public class LanguagePreferences {
 	static private SelectPanel panel;
@@ -29,7 +36,6 @@ public class LanguagePreferences {
 	
 	static public void cancel() {}
 
-
 	/**
 	 * Display a dialog box of available languages and let the user select their preference.
 	 * TODO when language changes, restart app OR replace all strings with new language.
@@ -44,5 +50,23 @@ public class LanguagePreferences {
 		
 		Translator.setCurrentLanguage(languageList[languageOptions.getSelectedIndex()]);
 		Translator.saveConfig();
+	}
+	
+	// TEST
+	
+	public static void main(String[] args) {
+		Log.start();
+		PreferencesHelper.start();
+		CommandLineOptions.setFromMain(args);
+		Translator.start();
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {}
+
+		try {
+			chooseLanguage();
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null,e.getMessage(),"Error",JOptionPane.ERROR_MESSAGE);
+		}
 	}
 }
