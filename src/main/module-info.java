a3100c2661be6c89a35e95143c4e74ce32ddf02c
Dan Royer <dan@marginallyclever.com>
2021-12-08 13:54:57 -0800

    Update module-info.java

--- src/main/module-info.java
+++ src/main/module-info.java
@@ -1,5 +1,5 @@
 module com.marginallyclever.makelangelo {
-	requires java.desktop;
+	requires transitive java.desktop;
 	requires java.prefs;
 	requires java.logging;
 	requires org.apache.commons.io;
@@ -7,11 +7,16 @@ module com.marginallyclever.makelangelo {
 	requires org.jetbrains.annotations;
 	requires org.slf4j;
 
-	requires jrpicam;
-	requires jogamp.fat;
-	requires kabeja;
-	requires jssc;
-	requires vecmath;
-	requires batik.all;
-	requires xml.apis.ext;
+	requires transitive jrpicam;
+	requires transitive jogamp.fat;
+	requires transitive kabeja;
+	requires transitive jssc;
+	requires transitive vecmath;
+	requires transitive batik.all;
+	requires transitive xml.apis.ext;
+	
+	exports com.marginallyclever.makelangelo;
+	exports com.marginallyclever.makelangelo.select;
+	exports com.marginallyclever.makelangelo.turtle;
+	exports com.marginallyclever.convenience;
 }
