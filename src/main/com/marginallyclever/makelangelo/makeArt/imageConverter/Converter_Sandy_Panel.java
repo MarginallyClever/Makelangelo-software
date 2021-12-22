b2ad5bb70fe023e3f656b9778a496bec106032f7
Dan Royer <dan@marginallyclever.com>
2021-12-22 09:45:17 -0800

    added strings for #386

--- src/main/com/marginallyclever/makelangelo/makeArt/imageConverter/Converter_Sandy_Panel.java
+++ src/main/com/marginallyclever/makelangelo/makeArt/imageConverter/Converter_Sandy_Panel.java
@@ -9,8 +9,8 @@ public class Converter_Sandy_Panel extends ImageConverterPanel {
 	
 	public Converter_Sandy_Panel(Converter_Sandy converter) {
 		super(converter);
-		add(new SelectSlider("rings",Translator.get("SandyRings"),300,10,converter.getScale()));
-		add(new SelectOneOfMany("direction",Translator.get("Direction"),converter.getDirections(),converter.getDirectionIndex()));
+		add(new SelectSlider("rings",Translator.get("SandyNoble.rings"),300,10,converter.getScale()));
+		add(new SelectOneOfMany("direction",Translator.get("SandyNoble.center"),converter.getDirections(),converter.getDirectionIndex()));
 		finish();
 	}
 }
