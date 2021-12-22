MarginallyClever/Makelangelo-software

commit e9f859b16e980cd56374a1609d1d441ae2142e10

added "PlotterControls." to the start of every translatable PlotterControls string

src/main/com/marginallyclever/makelangelo/plotter/plotterControls/PlotterControls.java
@@ -26,48 +26,48 @@ public class PlotterControls extends JPanel {
 	private MarlinInterface marlinInterface;
 	private ProgramInterface programInterface;
 
-	private JButton bSaveGCode = new JButton(Translator.get("SaveGCode"));
-	private JButton bFindHome= new JButton(Translator.get("FindHome"));
-	private JButton bRewind = new JButton(Translator.get("Rewind"));
-	private JButton bStart = new JButton(Translator.get("Play"));
-	private JButton bStep = new JButton(Translator.get("Step"));
-	private JButton bPause = new JButton(Translator.get("Pause"));
-	private JProgressBar progress = new JProgressBar(0,100); 
-	
-	private boolean isRunning=false;
-	private boolean penIsUpBeforePause=false;
-		
-	public PlotterControls(Plotter plotter,Turtle turtle) {
+	private JButton bSaveGCode = new JButton(Translator.get("PlotterControls.SaveGCode"));
+	private JButton bFindHome = new JButton(Translator.get("JogInterface.FindHome"));
+	private JButton bRewind = new JButton(Translator.get("PlotterControls.Rewind"));
+	private JButton bStart = new JButton(Translator.get("PlotterControls.Play"));
+	private JButton bStep = new JButton(Translator.get("PlotterControls.Step"));
+	private JButton bPause = new JButton(Translator.get("PlotterControls.Pause"));
+	private JProgressBar progress = new JProgressBar(0, 100);
+
+	private boolean isRunning = false;
+	private boolean penIsUpBeforePause = false;
+
+	public PlotterControls(Plotter plotter, Turtle turtle) {
 		super();
-		myPlotter=plotter;
-		myTurtle=turtle;
-		
+		myPlotter = plotter;
+		myTurtle = turtle;
+
 		jogInterface = new JogInterface(plotter);
 		marlinInterface = new MarlinPlotterInterface(plotter);
-		programInterface = new ProgramInterface(plotter,turtle);
-		
+		programInterface = new ProgramInterface(plotter, turtle);
+
 		JTabbedPane pane = new JTabbedPane();
 		pane.addTab(JogInterface.class.getSimpleName(), jogInterface);
 		pane.addTab(MarlinInterface.class.getSimpleName(), marlinInterface);
 		pane.addTab(ProgramInterface.class.getSimpleName(), programInterface);
-		
+
 		this.setLayout(new BorderLayout());
-		this.add(pane,BorderLayout.CENTER);
-		this.add(getToolBar(),BorderLayout.NORTH);
-		this.add(progress,BorderLayout.SOUTH);
-		
-		marlinInterface.addListener((e)->{
-			if(e.getActionCommand().contentEquals(MarlinInterface.IDLE) ) {
-				//Log.message("PlotterControls heard idle");
-				if(isRunning) {
-					//Log.message("PlotterControls is running");
+		this.add(pane, BorderLayout.CENTER);
+		this.add(getToolBar(), BorderLayout.NORTH);
+		this.add(progress, BorderLayout.SOUTH);
+
+		marlinInterface.addListener((e) -> {
+			if (e.getActionCommand().contentEquals(MarlinInterface.IDLE)) {
+				// Log.message("PlotterControls heard idle");
+				if (isRunning) {
+					// Log.message("PlotterControls is running");
 					step();
 				}
 			}
 			updateProgressBar();
 		});
 	}
-	
+
 	private JToolBar getToolBar() {
 		JToolBar bar = new JToolBar();
 		bar.setFloatable(false);
@@ -77,18 +77,18 @@ public class PlotterControls extends JPanel {
 		bar.add(bStart);
 		bar.add(bPause);
 		bar.add(bStep);
-		
-		bFindHome.addActionListener((e)-> findHome());
-		bRewind.addActionListener((e)-> rewind());
-		bStart.addActionListener((e)-> play());
-		bPause.addActionListener((e)-> pause());
-		bStep.addActionListener((e)-> step());
-		
+
+		bFindHome.addActionListener((e) -> jogInterface.findHome());
+		bRewind.addActionListener((e) -> rewind());
+		bStart.addActionListener((e) -> play());
+		bPause.addActionListener((e) -> pause());
+		bStep.addActionListener((e) -> step());
+
 		updateButtonStatus();
-		
+
 		return bar;
 	}
-	
+
 	private void findHome() {
 		myPlotter.findHome();
 		updateButtonStatus();
@@ -96,7 +96,7 @@ public class PlotterControls extends JPanel {
 
 	private void step() {
 		programInterface.step();
-		if(programInterface.getLineNumber()==-1) {
+		if (programInterface.getLineNumber() == -1) {
 			// done
 			pause();
 		}
@@ -104,15 +104,17 @@ public class PlotterControls extends JPanel {
 
 	public void startAt(int lineNumber) {
 		int count = programInterface.getMoveCount();
-		if(lineNumber>=count) lineNumber = count;
-		if(lineNumber<0) lineNumber=0;
+		if (lineNumber >= count)
+			lineNumber = count;
+		if (lineNumber < 0)
+			lineNumber = 0;
 
 		programInterface.setLineNumber(lineNumber);
 		play();
 	}
-	
+
 	private void updateProgressBar() {
-		progress.setValue((int)(100.0*programInterface.getLineNumber()/programInterface.getMoveCount()));
+		progress.setValue((int) (100.0 * programInterface.getLineNumber() / programInterface.getMoveCount()));
 	}
 
 	private void rewind() {
@@ -123,13 +125,14 @@ public class PlotterControls extends JPanel {
 	private void play() {
 		isRunning = true;
 		updateButtonStatus();
-		if(!penIsUpBeforePause) myPlotter.lowerPen();
+		if (!penIsUpBeforePause)
+			myPlotter.lowerPen();
 		rewindIfNoProgramLineSelected();
 		step();
 	}
 
 	private void rewindIfNoProgramLineSelected() {
-		if(programInterface.getLineNumber()==-1) {
+		if (programInterface.getLineNumber() == -1) {
 			programInterface.rewind();
 		}
 	}
@@ -138,7 +141,8 @@ public class PlotterControls extends JPanel {
 		isRunning = false;
 		updateButtonStatus();
 		penIsUpBeforePause = myPlotter.getPenIsUp();
-		if(!penIsUpBeforePause) myPlotter.raisePen();
+		if (!penIsUpBeforePause)
+			myPlotter.raisePen();
 	}
 
 	public boolean isRunning() {
@@ -146,24 +150,27 @@ public class PlotterControls extends JPanel {
 	}
 
 	private void updateButtonStatus() {
-		boolean isHomed=myPlotter.getDidFindHome();
+		boolean isHomed = myPlotter.getDidFindHome();
 		bRewind.setEnabled(isHomed && !isRunning);
 		bStart.setEnabled(isHomed && !isRunning);
 		bPause.setEnabled(isHomed && isRunning);
 		bStep.setEnabled(isHomed && !isRunning);
 	}
-	
+
 	@SuppressWarnings("unused")
 	private int findLastPenUpBefore(int startAtLine) {
 		ArrayList<TurtleMove> history = myTurtle.history;
 		int total = history.size();
 		int x = startAtLine;
-		if(x >= total) x = total-1;
-		if(x<0) return 0;
+		if (x >= total)
+			x = total - 1;
+		if (x < 0)
+			return 0;
 
 		while (x > 1) {
 			TurtleMove m = history.get(x);
-			if(m.type == TurtleMove.TRAVEL) return x;
+			if (m.type == TurtleMove.TRAVEL)
+				return x;
 			--x;
 		}
 
@@ -171,16 +178,16 @@ public class PlotterControls extends JPanel {
 	}
 
 	// TEST
-	
+
 	public static void main(String[] args) {
 		Log.start();
 		PreferencesHelper.start();
 		CommandLineOptions.setFromMain(args);
 		Translator.start();
-		
+
 		JFrame frame = new JFrame(PlotterControls.class.getSimpleName());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(new PlotterControls(new Plotter(),new Turtle()));
+		frame.add(new PlotterControls(new Plotter(), new Turtle()));
 		frame.pack();
 		frame.setVisible(true);
 	}

src/main/resources/languages/chinese.xml
@@ -580,13 +580,8 @@
 	</string>
 
 	<string>
-		<key>Pause</key>
+		<key>PlotterControls.Pause</key>
 		<value>暂停</value>
-		<hint>menu or dialog title</hint>
-	</string>
-	<string>
-		<key>Unpause</key>
-		<value>取消暂停</value>
 		<hint>menu or dialog title</hint>
 	</string>
 
@@ -606,12 +601,12 @@
 		<hint>menu or dialog title</hint>
 	</string>
 	<string>
-		<key>Rewind</key>
+		<key>PlotterControls.Rewind</key>
 		<value>Rewind</value>
 		<hint>menu or dialog title</hint>
 	</string>
 	<string>
-		<key>Play</key>
+		<key>PlotterControls.Play</key>
 		<value>Draw</value>
 		<hint>menu or dialog title</hint>
 	</string>
@@ -658,12 +653,12 @@
 		<hint>manual driving dialog</hint>
 	</string>
 	<string>
-		<key>PenUp</key>
+		<key>JogInterface.PenUp</key>
 		<value>抬笔</value>
 		<hint>manual driving dialog</hint>
 	</string>
 	<string>
-		<key>PenDown</key>
+		<key>JogInterface.PenDown</key>
 		<value>落笔</value>
 		<hint>manual driving dialog</hint>
 	</string>
@@ -1176,12 +1171,12 @@
 	</string>
 
 	<string>
-		<key>DisengageMotors</key>
+		<key>JogInterface.DisengageMotors</key>
 		<value>关闭</value>
 		<hint>Manual driving dialog</hint>
 	</string>
 	<string>
-		<key>EngageMotors</key>
+		<key>JogInterface.EngageMotors</key>
 		<value>启用</value>
 		<hint>Manual driving dialog</hint>
 	</string>

src/main/resources/languages/dutch.xml
@@ -318,13 +318,8 @@
 	</string>
 
 	<string>
-		<key>Pause</key>
+		<key>PlotterControls.Pause</key>
 		<value>Pause</value>
-		<hint>menu or dialog title</hint>
-	</string>
-	<string>
-		<key>Unpause</key>
-		<value>Hervatten</value>
 		<hint>menu or dialog title</hint>
 	</string>
 
@@ -344,12 +339,12 @@
 		<hint>menu or dialog title</hint>
 	</string>
 	<string>
-		<key>Rewind</key>
+		<key>PlotterControls.Rewind</key>
 		<value>Rewind</value>
 		<hint>menu or dialog title</hint>
 	</string>
 	<string>
-		<key>Play</key>
+		<key>PlotterControls.Play</key>
 		<value>Draw</value>
 		<hint>menu or dialog title</hint>
 	</string>
@@ -386,12 +381,12 @@
 		<hint>manual driving dialog</hint>
 	</string>
 	<string>
-		<key>PenUp</key>
+		<key>JogInterface.PenUp</key>
 		<value>Pen omhoog</value>
 		<hint>manual driving dialog</hint>
 	</string>
 	<string>
-		<key>PenDown</key>
+		<key>JogInterface.PenDown</key>
 		<value>Pen omlaag</value>
 		<hint>manual driving dialog</hint>
 	</string>
@@ -812,7 +807,7 @@
 	</string>
 
 	<string>
-		<key>DisengageMotors</key>
+		<key>JogInterface.DisengageMotors</key>
 		<value>Motoren uitzetten</value>
 		<hint>Disengage Button on manual driving dialog</hint>
 	</string>
@@ -946,10 +941,5 @@
 		<key>PaperSize</key>
 		<value>Papier Grootte</value>
 		<hint>machine size control panel</hint>
-	</string>
-	<string>
-		<key>DisengageMotors</key>
-		<value>Stop motoren</value>
-		<hint>Disengage Button on manual driving dialog</hint>
 	</string>
 </language>

src/main/resources/languages/english.xml
@@ -557,13 +557,8 @@
 	</string>
 
 	<string>
-		<key>Pause</key>
+		<key>PlotterControls.Pause</key>
 		<value>Pause</value>
-		<hint>menu or dialog title</hint>
-	</string>
-	<string>
-		<key>Unpause</key>
-		<value>Unpause</value>
 		<hint>menu or dialog title</hint>
 	</string>
 
@@ -578,17 +573,17 @@
 		<hint>menu or dialog title</hint>
 	</string>
 	<string>
-		<key>SaveGCode</key>
+		<key>PlotterControls.SaveGCode</key>
 		<value>Save Gcode to file/SD</value>
 		<hint>menu or dialog title</hint>
 	</string>
 	<string>
-		<key>Rewind</key>
+		<key>PlotterControls.Rewind</key>
 		<value>Rewind</value>
 		<hint>menu or dialog title</hint>
 	</string>
 	<string>
-		<key>Play</key>
+		<key>PlotterControls.Play</key>
 		<value>Draw</value>
 		<hint>menu or dialog title</hint>
 	</string>
@@ -642,11 +637,6 @@
 	</string>
 	<string>
 		<key>JogInterface.FindHome</key>
-		<value>Find home</value>
-		<hint>manual driving dialog</hint>
-	</string>
-	<string>
-		<key>FindHome</key>
 		<value>Find home</value>
 		<hint>manual driving dialog</hint>
 	</string>
@@ -870,32 +860,19 @@
 		<hint></hint>
 	</string>
 	<string>
-		<key>MenuSaveGCODEAs</key>
-		<value>Save to file/SD card</value>
-		<hint>menu or dialog title</hint>
-	</string>
-	<string>
 		<key>MenuSaveFile</key>
 		<value>Save</value>
 		<hint>menu or dialog title</hint>
 	</string>
 	<string>
-		<key>Step</key>
+		<key>PlotterControls.Step</key>
 		<value>Step</value>
 		<hint>menu or dialog title</hint>
 	</string>
-
-
 
 	<string>
 		<key>MenuDraw</key>
 		<value>Draw</value>
-		<hint>menu or dialog title</hint>
-	</string>
-
-	<string>
-		<key>Step</key>
-		<value>Step</value>
 		<hint>menu or dialog title</hint>
 	</string>
 

src/main/resources/languages/german.xml
@@ -557,13 +557,8 @@
 	</string>
 
 	<string>
-		<key>Pause</key>
+		<key>PlotterControls.Pause</key>
 		<value>Pause</value>
-		<hint>menu or dialog title</hint>
-	</string>
-	<string>
-		<key>Unpause</key>
-		<value>Weiter</value>
 		<hint>menu or dialog title</hint>
 	</string>
 
@@ -583,12 +578,12 @@
 		<hint>menu or dialog title</hint>
 	</string>
 	<string>
-		<key>Rewind</key>
+		<key>PlotterControls.Rewind</key>
 		<value>Zurückspulen</value>
 		<hint>menu or dialog title</hint>
 	</string>
 	<string>
-		<key>Play</key>
+		<key>PlotterControls.Play</key>
 		<value>Zeichnen</value>
 		<hint>menu or dialog title</hint>
 	</string>
@@ -642,11 +637,6 @@
 	</string>
 	<string>
 		<key>JogInterface.FindHome</key>
-		<value>Zuhause finden</value>
-		<hint>manual driving dialog</hint>
-	</string>
-	<string>
-		<key>FindHome</key>
 		<value>Zuhause finden</value>
 		<hint>manual driving dialog</hint>
 	</string>
@@ -879,7 +869,7 @@
 		<hint>menu or dialog title</hint>
 	</string>
 	<string>
-		<key>Step</key>
+		<key>PlotterControls.Step</key>
 		<value>Schritt</value>
 		<hint>menu or dialog title</hint>
 	</string>
@@ -889,12 +879,6 @@
 	<string>
 		<key>MenuDraw</key>
 		<value>Zeichnen</value>
-		<hint>menu or dialog title</hint>
-	</string>
-
-	<string>
-		<key>Step</key>
-		<value>Schritt</value>
 		<hint>menu or dialog title</hint>
 	</string>
 

src/main/resources/languages/piglatin.xml
@@ -329,13 +329,8 @@
 	</string>
 
 	<string>
-		<key>Pause</key>
+		<key>PlotterControls.Pause</key>
 		<value>ausePay</value>
-		<hint>menu or dialog title</hint>
-	</string>
-	<string>
-		<key>Unpause</key>
-		<value>Unpauseyay</value>
 		<hint>menu or dialog title</hint>
 	</string>
 
@@ -355,12 +350,12 @@
 		<hint>menu or dialog title</hint>
 	</string>
 	<string>
-		<key>Rewind</key>
+		<key>PlotterControls.Rewind</key>
 		<value>Rewind</value>
 		<hint>menu or dialog title</hint>
 	</string>
 	<string>
-		<key>Play</key>
+		<key>PlotterControls.Play</key>
 		<value>Draw</value>
 		<hint>menu or dialog title</hint>
 	</string>
@@ -397,12 +392,12 @@
 		<hint>manual driving dialog</hint>
 	</string>
 	<string>
-		<key>PenUp</key>
+		<key>JogInterface.PenUp</key>
 		<value>enPay upyay</value>
 		<hint>manual driving dialog</hint>
 	</string>
 	<string>
-		<key>PenDown</key>
+		<key>JogInterface.PenDown</key>
 		<value>enPay ownday</value>
 		<hint>manual driving dialog</hint>
 	</string>

commit f234c4b117191b777ca7de2b7653c30c95156088

Fix compilation issue introduced in e9f859b

src/main/com/marginallyclever/makelangelo/plotter/plotterControls/PlotterControls.java
@@ -78,7 +78,7 @@ public class PlotterControls extends JPanel {
 		bar.add(bPause);
 		bar.add(bStep);
 
-		bFindHome.addActionListener((e) -> jogInterface.findHome());
+		bFindHome.addActionListener((e) -> findHome());
 		bRewind.addActionListener((e) -> rewind());
 		bStart.addActionListener((e) -> play());
 		bPause.addActionListener((e) -> pause());