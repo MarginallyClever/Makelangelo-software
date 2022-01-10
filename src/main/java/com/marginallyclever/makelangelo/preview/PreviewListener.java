f6e0b7bc5e4f596ab2f311b1369b330bf5c3a7a7
Dan Royer <dan@marginallyclever.com>
2022-01-09 18:30:51 -0800

    Update PreviewListener.java

--- src/main/java/com/marginallyclever/makelangelo/preview/PreviewListener.java
+++ src/main/java/com/marginallyclever/makelangelo/preview/PreviewListener.java
@@ -13,5 +13,5 @@ public interface PreviewListener {
 	 * Callback from {@link PreviewPanel} that it is time to render to the WYSIWYG display.
 	 * @param gl2 the render context
 	 */
-	void render(GL2 gl2);
+	public void render(GL2 gl2);
 }
