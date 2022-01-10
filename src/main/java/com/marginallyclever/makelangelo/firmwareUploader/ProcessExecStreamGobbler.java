6aed19e410ca60ac0c4ddb3de1ef6501e2f4d602
PPAC37 <pierre.pacaud.37@gmail.com>
2022-01-10 22:23:27 +0100

    cleanning

--- src/main/java/com/marginallyclever/makelangelo/firmwareUploader/ProcessExecStreamGobbler.java
+++ src/main/java/com/marginallyclever/makelangelo/firmwareUploader/ProcessExecStreamGobbler.java
@@ -1,14 +1,15 @@
 /*
  * Based on StreamGobbler.java from http://www.javaworld.com/javaworld/jw-12-2000/jw-1229-traps.html (year 2012 ?).
- * Altered by PPAC37 ( lecture char/char et divers fonctions pour événements via surcharges de méthodes )
+ * Altered by PPAC37 ( reading char / char and various functions for events via method override )
  */
 package com.marginallyclever.makelangelo.firmwareUploader;
 
 import java.io.*;
 import java.util.Date;
 
-/** StreamGobbler (Threaded), to take care of an "output" (for the command point of view) stream of a process running.
- * 
+/**
+ * StreamGobbler (Threaded), to take care of an "output" (for the command point
+ * of view) stream of a process running.
  */
 public class ProcessExecStreamGobbler extends Thread {
 
@@ -23,78 +24,77 @@ public class ProcessExecStreamGobbler extends Thread {
     int lineCount = 0;
     StringBuilder stringBuilder = null;
     //
-    // ?? usable to identify frozen commands ?
+    // TODO : to implement : to identify frozen commands. (no output timer ...)
     private long startedAt;
     private long lastEventAt;
 
     public String getInBuffer() {
-        return stringBuilder.toString();
+	return stringBuilder.toString();
     }
 
     public ProcessExecStreamGobbler(InputStream is, String type) {
-        this(is, type, null);
+	this(is, type, null);
     }
 
     public ProcessExecStreamGobbler(InputStream is, String type, OutputStream redirect) {
-        this.is = is;
-        this.type = type;
-        this.os = redirect;
-        stringBuilder = new StringBuilder();
+	this.is = is;
+	this.type = type;
+	this.os = redirect;
+	stringBuilder = new StringBuilder();
     }
 
     @Override
     public void run() {
 	startedAt = System.currentTimeMillis();
-        fireUpdateRunBegin();
-        try {
-            PrintWriter pw = null;
-            if (os != null) {
-                pw = new PrintWriter(os);
-            }
+	fireUpdateRunBegin();
+	try {
+	    PrintWriter pw = null;
+	    if (os != null) {
+		pw = new PrintWriter(os);
+	    }
 
-            InputStreamReader isr = new InputStreamReader(is);
-	    
-            int intRead;
-            while ((intRead = isr.read()) != -1) {
+	    InputStreamReader isr = new InputStreamReader(is);
+
+	    int intRead;
+	    while ((intRead = isr.read()) != -1) {
 		lastEventAt = System.currentTimeMillis();
-                readCount++;		
-                // Pour le cas d'une redirection
-                if (pw != null) {
-                    pw.write(intRead);
-                }
-                //
-                Date dNow = new Date();
-                // for line by lines events
-                if (intRead != '\n') {
-                    stringBuilder.appendCodePoint(intRead);
-                } else {
-                    eventAndFlushSB(dNow);
-                }
-                // for char by char events
-                readEvent(dNow, intRead);
-            }
-	    
-            if (pw != null) {
-                pw.flush();
-            }
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        }
-        // For the case of a final line with no line return.
-        if (stringBuilder.length() != 0) {
-            eventAndFlushSB(new Date());
-        }
-        fireUpdateRunEnd();
-        haveFinish = true;
-        doFinish();
+		readCount++;
+		// For the case of a redirection
+		if (pw != null) {
+		    pw.write(intRead);
+		}
+		//
+		Date dNow = new Date();
+		// for line by lines events
+		if (intRead != '\n') {
+		    stringBuilder.appendCodePoint(intRead);
+		} else {
+		    eventAndFlushSB(dNow);
+		}
+		// for char by char events
+		readEvent(dNow, intRead);
+	    }
+
+	    if (pw != null) {
+		pw.flush();
+	    }
+	} catch (IOException ioe) {
+	    ioe.printStackTrace();
+	}
+	// For the case of a final line with no line return.
+	if (stringBuilder.length() != 0) {
+	    eventAndFlushSB(new Date());
+	}
+	fireUpdateRunEnd();
+	haveFinish = true;
+	doFinish();
     }
 
     /**
-     * Pour forcer un retour a la ligne ( dans le cas d'un flux de sortie
-     * entrelasser avec des entrées ...
+     * To force a carriage return (in the case of an output flow interlaced with inputs ...
      */
     public void inputCarryReturn() {
-        eventAndFlushSB(new Date());
+	eventAndFlushSB(new Date());
     }
 
     /**
@@ -102,60 +102,64 @@ public class ProcessExecStreamGobbler extends Thread {
      * @param dNow
      */
     private void eventAndFlushSB(Date dNow) {
-        String lineTmp = stringBuilder.toString();
-        //
-        lineCount++;
-        readLineEventWithCounter(dNow, lineCount, lineTmp);
-        //
-        readLineEvent(dNow, lineTmp);
-        // on vide le buffer de la ligne
-        stringBuilder.setLength(0);
+	String lineTmp = stringBuilder.toString();
+	//
+	lineCount++;
+	readLineEventWithCounter(dNow, lineCount, lineTmp);
+	//
+	readLineEvent(dNow, lineTmp);
+	// on vide le buffer de la ligne
+	stringBuilder.setLength(0);
     }
 
     /**
-     * To Override to have read byte events.
-     * Be prepared to take care of non printable chars (used for exemple for some progress status to refresh the progress)
-     * @param d the time of the event 
+     * To Override to have read byte events. Be prepared to take care of non
+     * printable chars (used for exemple for some progress status to refresh the
+     * progress)
+     *
+     * @param d the time of the event
      * @param intread the int value read from the stream.
      */
     public void readEvent(Date d, int intread) {
-        System.out.println("@todo StreamGobbler[" + type + "].readEvent( '" + (char) intread + "' = " + intread + " )");
+	System.out.println("@todo StreamGobbler[" + type + "].readEvent( '" + (char) intread + "' = " + intread + " )");
     }
 
     /**
      * To Override to have read lines events.
+     *
      * @param d the time of the event.
      * @param s the line readed from the stream.
      */
     public void readLineEvent(Date d, String s) {
-        System.out.println("@todo StreamGobbler[" + type + "].readLineEvent( " + s + " )");
+	System.out.println("@todo StreamGobbler[" + type + "].readLineEvent( " + s + " )");
     }
 
     /**
      * To Override to have read lines events (with a line number).
+     *
      * @param d the time of the event.
      * @param lineNum the line number in the stream.
      * @param s the line readed from the stream.
      */
     public void readLineEventWithCounter(Date d, int lineNum, String s) {
-        System.out.println("@todo StreamGobbler[" + type + "].readLineEventWithCounter( " + s + " )");
+	System.out.println("@todo StreamGobbler[" + type + "].readLineEventWithCounter( " + s + " )");
     }
 
     public void doFinish() {
-        haveFinish = true;
+	haveFinish = true;
     }
 
     //
     //
     //
     /**
-     * To Override to have a posible start event.
+     * To Override to have a possible start event.
      */
     public void fireUpdateRunBegin() {
     }
 
     /**
-     * To Override to have a posible end event.
+     * To Override to have a possible end event.
      */
     public void fireUpdateRunEnd() {
     }
