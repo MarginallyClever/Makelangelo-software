a5facb059a641a7ef52810584ed2dfad45999893
PPAC37 <pierre.pacaud.37@gmail.com>
2022-02-01 10:41:53 +0100

    Test the test language Xml validation no dup or empty key xsd (restriction key better than unique ) english.xml proposal without dup.

--- src/test/java/com/marginallyclever/makelangelo/LanguagesXmlValidationForNoDupKeyTest.java
+++ src/test/java/com/marginallyclever/makelangelo/LanguagesXmlValidationForNoDupKeyTest.java
@@ -23,39 +23,37 @@ import org.xml.sax.SAXParseException;
 public class LanguagesXmlValidationForNoDupKeyTest {
 
     private static final Logger logger = LoggerFactory.getLogger(LanguagesXmlValidationForNoDupKeyTest.class);
-    
+
     public final static String ressourceStringForXmlShemaFile = "/translator/language_no_dup_key.xsd";
-	    
+
     @Test
     public void validateLaguagesXmlFiles() throws IOException {
 
 	List<String> xmlFilesWithValidationDefect = new ArrayList<>();
 	File srcDir = new File("src" + File.separator + "main" + File.separator + "resources" + File.separator + "languages");
-	
+
 	// Pre requi	
 //	//URL schemaFile = new File("src/test/resources/translator/language_no_dup_key.xsd").toURI().toURL();
 	URL schemaFile = LanguagesXmlValidationForNoDupKeyTest.class.getResource(ressourceStringForXmlShemaFile);
-	
+
 	//  Pre requi schema xsd file
-	assertNotNull(schemaFile,"The test need a redable schema xsd file ("+ressourceStringForXmlShemaFile+") to validate the language files");
-	
-	
+	assertNotNull(schemaFile, "The test need a redable schema xsd file (" + ressourceStringForXmlShemaFile + ") to validate the language files");
+
 	//  Pre requi some language xml file to test
 	List<File> files = listFiles(srcDir.toPath(), ".xml");
-	
+
 	// assert some files
 	// ?TODO 
-	
 	// valide the files found
 	files.forEach(file -> {
-		boolean isValide = xmlValidationWithExternalXsdShema(file, schemaFile);
-		if (!isValide) {
-		    xmlFilesWithValidationDefect.add(file.toString());
-		}
+	    boolean isValide = xmlValidationWithExternalXsdShema(file, schemaFile);
+	    if (!isValide) {
+		xmlFilesWithValidationDefect.add(file.toString());
+	    }
 	});
 
 	if (!xmlFilesWithValidationDefect.isEmpty()) {
-	    logger.info("invalide with "+ressourceStringForXmlShemaFile+" :");
+	    logger.info("invalide with " + ressourceStringForXmlShemaFile + " :");
 	    for (String result : xmlFilesWithValidationDefect) {
 		logger.info("  {}", result);
 	    }
@@ -63,16 +61,50 @@ public class LanguagesXmlValidationForNoDupKeyTest {
 	assertEquals(0, xmlFilesWithValidationDefect.size(), "Some language xml file do not validate with language.xsd, see previous logs for details");
     }
 
-   /**
-    * Validate an xml file with an external xsd Schema.
-    * src inspiration : https://stackoverflow.com/questions/15732/whats-the-best-way-to-validate-an-xml-file-against-an-xsd-file
-    * 
-    * @param xmlFileToValidate the xml file to validate.
-    * @param schemaFile the xsd schema file to use to validate the xml file.
-    * @return true if the validation is a succes ( no warm, error or fatal error ) false otherwise and if other exception ( cant found file xml, or xsd, or xsd not valid) occure.
-    */
-    public static boolean xmlValidationWithExternalXsdShema(File xmlFileToValidate, URL schemaFile)  {
-	Source xmlStreamSource = new StreamSource(xmlFileToValidate);	
+    @Test
+    public void verifyThatValidationWorks_notvalide() throws IOException {
+
+	URL schemaFile = LanguagesXmlValidationForNoDupKeyTest.class.getResource(ressourceStringForXmlShemaFile);
+	assertNotNull(schemaFile, "The test need a redable schema xsd file (" + ressourceStringForXmlShemaFile + ") to validate the language files");
+
+	String myTestFileWithWithDupKeyInRessources = "/translator/english_with_dup_key.xml";
+	URL resource = LanguagesXmlValidationForNoDupKeyTest.class.getResource(myTestFileWithWithDupKeyInRessources);
+	assertNotNull(resource, "The test need a redable xml file (" + myTestFileWithWithDupKeyInRessources + ") to test itself");
+
+	// english_with_dup_key.xml contains some duplicated an en empty key //language/string/key
+	boolean isValide = xmlValidationWithExternalXsdShema(new File(resource.getFile()), schemaFile);
+
+	assertTrue(!isValide);
+    }
+
+    @Test
+    public void verifyThatValidationWorks_valide() throws IOException {
+
+	URL schemaFile = LanguagesXmlValidationForNoDupKeyTest.class.getResource(ressourceStringForXmlShemaFile);
+	assertNotNull(schemaFile, "The test need a redable schema xsd file (" + ressourceStringForXmlShemaFile + ") to validate the language files");
+
+	String myTestFileWithNoDupKeyInRessources = "/translator/english_with_no_dup_key.xml";
+	URL resource = LanguagesXmlValidationForNoDupKeyTest.class.getResource(myTestFileWithNoDupKeyInRessources);
+	assertNotNull(resource, "The test need a redable xml file (" + myTestFileWithNoDupKeyInRessources + ") to test itself");
+
+	// english_with_no_dup_key.xml do not containe duplicated or empty //language/string/key        
+	boolean isValide = xmlValidationWithExternalXsdShema(new File(resource.getFile()), schemaFile);
+
+	assertTrue(isValide);
+    }
+
+    /**
+     * Validate an xml file with an external xsd Schema. src inspiration :
+     * https://stackoverflow.com/questions/15732/whats-the-best-way-to-validate-an-xml-file-against-an-xsd-file
+     *
+     * @param xmlFileToValidate the xml file to validate.
+     * @param schemaFile the xsd schema file to use to validate the xml file.
+     * @return true if the validation is a succes ( no warm, error or fatal
+     * error ) false otherwise and if other exception ( cant found file xml, or
+     * xsd, or xsd not valid) occure.
+     */
+    public static boolean xmlValidationWithExternalXsdShema(File xmlFileToValidate, URL schemaFile) {
+	Source xmlStreamSource = new StreamSource(xmlFileToValidate);
 	SchemaFactory schemaFactory = SchemaFactory
 		.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
 	try {
@@ -83,36 +115,36 @@ public class LanguagesXmlValidationForNoDupKeyTest {
 	    // to get full error list, we set a ErrorEandler
 	    validator.setErrorHandler(new ErrorHandler() {
 		@Override
-		public void warning(SAXParseException e) throws SAXException {		    
-		     logger.info(String.format("[ %7s ] %s:%-4d %s", "warning", e.getSystemId(), e.getLineNumber(), e.getMessage()));
+		public void warning(SAXParseException e) throws SAXException {
+		    logger.info(String.format("[ %7s ] %s:%-4d %s", "warning", e.getSystemId(), e.getLineNumber(), e.getMessage()));
 		    errorHandlerException.add(e.toString());
 		}
 
 		@Override
 		public void error(SAXParseException e) throws SAXException {
-		     logger.info(String.format("[ %7s ] %s:%-4d %s", "error", e.getSystemId(), e.getLineNumber(), e.getMessage()));
-		     errorHandlerException.add(e.toString());
+		    logger.info(String.format("[ %7s ] %s:%-4d %s", "error", e.getSystemId(), e.getLineNumber(), e.getMessage()));
+		    errorHandlerException.add(e.toString());
 		}
 
 		@Override
 		public void fatalError(SAXParseException e) throws SAXException {
 		    logger.info(String.format("[ %7s ] %s:%-4d %s", "fatalError", e.getSystemId(), e.getLineNumber(), e.getMessage()));
-		     errorHandlerException.add(e.toString());
+		    errorHandlerException.add(e.toString());
 		}
-	    });	    
+	    });
 	    // the validation.
-	    validator.validate(xmlStreamSource);	    
+	    validator.validate(xmlStreamSource);
 	    return errorHandlerException.isEmpty();
 	} catch (SAXException e) {
 	    // avec le setErrorHandler on ne devrait plus passer ici.
 	    logger.info(String.format("[ %7s ] %s\n%s", "invalid", xmlStreamSource.getSystemId(), e));
 	} catch (IOException ex) {
 	    logger.info(String.format("[ %7s ] %s\n%s", "error", xmlStreamSource.getSystemId(), ex));
-	}catch (Exception ex) {
+	} catch (Exception ex) {
 	    // le shema n'existe pas ?
 	    logger.info(String.format("[ %7s ] %s\n%s", "error", xmlStreamSource.getSystemId(), ex));
 	}
 	return false;
     }
-    
+
 }
