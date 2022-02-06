aabbc15b1fb8b4667b7d1d861209c3549810f42d
PPAC37 <pierre.pacaud.37@gmail.com>
2022-02-06 05:26:04 +0100

    add key "TurtleGenerators.LearnMore.Link.Text" and Translator.get to all "Learn More" occurrences.

--- src/main/java/com/marginallyclever/makelangelo/makeArt/turtleGenerator/Generator_HilbertCurve_Panel.java
+++ src/main/java/com/marginallyclever/makelangelo/makeArt/turtleGenerator/Generator_HilbertCurve_Panel.java
@@ -25,7 +25,7 @@ public class Generator_HilbertCurve_Panel extends TurtleGeneratorPanel {
 		this.generator = generator;
 
 		add(fieldOrder = new SelectSlider("order",Translator.get("HilbertCurveOrder"),8,1,Generator_HilbertCurve.getOrder()));
-		add(new SelectReadOnlyText("url","<a href='https://en.wikipedia.org/wiki/Hilbert_curve'>Learn more</a>"));
+		add(new SelectReadOnlyText("url","<a href='https://en.wikipedia.org/wiki/Hilbert_curve'>"+Translator.get("TurtleGenerators.LearnMore.Link.Text")+"</a>"));
 	}
 
 	@Override
