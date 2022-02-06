b4aecda79b057de3bc777a5903eda1ea39642873
PPAC37 <pierre.pacaud.37@gmail.com>
2022-02-06 03:48:12 +0100

    refactoring utility function in SelectReadOnlyText to create JeditoPane whith HyperLinkListenerAndToolTips

--- src/main/java/com/marginallyclever/makelangelo/select/SelectReadOnlyText.java
+++ src/main/java/com/marginallyclever/makelangelo/select/SelectReadOnlyText.java
@@ -2,56 +2,83 @@ package com.marginallyclever.makelangelo.select;
 
 import java.awt.BorderLayout;
 import java.awt.Desktop;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import javax.swing.JEditorPane;
 
-import javax.swing.JLabel;
+import javax.swing.ToolTipManager;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * Read only HTML-rich text that can display AT MOST one clickable link.
- * @author Dan Royer
+ * Read only JEditorPane with an HyperLinckListener for text/html contening or not multiple html link.
+ * With clickable link (Desktop.BROWSE) and ToolTips when hovered over .
+ * 
+ * @author Dan Royer (and modified by PPAC37)
  * @since 7.24.0
  */
 public class SelectReadOnlyText extends Select {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = -8918068053490064344L;
-	private static final String A_HREF = "<a href=";
-	private static final String HREF_CLOSED = ">";
+	private static final long serialVersionUID = -8918068053490064344L;// TODO to regenerat ?
 	
-	private JLabel label;
+	private static final Logger logger = LoggerFactory.getLogger(SelectReadOnlyText.class);
+		
+	private JEditorPane jEdPane;
 	
 	public SelectReadOnlyText(String internalName,String labelKey) {
-		super(internalName);
-		
-		label = new JLabel("<html>"+ labelKey+"</html>",JLabel.LEADING);
-		label.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent evt) {
-		        String text = label.getText();
-		        try {
-		        	String link = getPlainLink(text);
-		        	if(link==null) return;
-		            URI uri = new java.net.URI(link);
-		            Desktop.getDesktop().browse(uri);
-		        } catch (URISyntaxException | IOException e) {
-		            throw new AssertionError(e.getMessage() + ": " + text); //NOI18N
-		        }
-			}
-		});
-		this.add(label,BorderLayout.CENTER);
+		super(internalName);		
+		jEdPane = createJEditorPaneWithHyperlinkListenerAndToolTipsForDesktopBrowse("<html>"+ labelKey+"</html>");
+		this.add(jEdPane,BorderLayout.CENTER);
 	}
 	
-
-	private String getPlainLink(String s) {
-		int first = s.indexOf(A_HREF);
-		if(first<0) return null;
-		int last = s.indexOf(HREF_CLOSED,first);
-		if(last<0) return null;
-	    return s.substring(first + A_HREF.length()+1, last-1);
+	/**
+	 * Create a JEditorPane not editable for text/html contente, with an HyperLinkListener to Desktop Browse (when clicked) and show a ToolTips with the URL hovered.
+	 * @param sToSetAsTextToTheHtmlEditorPane
+	 * @return 
+	 */
+	public static JEditorPane createJEditorPaneWithHyperlinkListenerAndToolTipsForDesktopBrowse(String sToSetAsTextToTheHtmlEditorPane) {
+		JEditorPane createdJEditorPane =  new JEditorPane();
+		createdJEditorPane.setEditable(false);
+		createdJEditorPane.setOpaque(false);
+		createdJEditorPane.setContentType("text/html");
+		createdJEditorPane.setText(sToSetAsTextToTheHtmlEditorPane);
+		createdJEditorPane.addHyperlinkListener(new HyperlinkListener() {
+			@Override
+			public void hyperlinkUpdate(HyperlinkEvent hyperlinkEvent) {
+				HyperlinkEvent.EventType eventType = hyperlinkEvent.getEventType();
+				if (eventType == HyperlinkEvent.EventType.ACTIVATED) {
+					if (Desktop.isDesktopSupported()) {
+						try {
+							URI u = hyperlinkEvent.getURL().toURI();
+							Desktop desktop = Desktop.getDesktop();
+							if ( desktop.isSupported(Desktop.Action.BROWSE)){
+								logger.debug("Desktop.Action.BROWSE {}", u);
+								desktop.browse(u);
+							}else{
+								logger.error("Desktop.Action.BROWSE not supported. Cant browse {}", u);
+							}
+						} catch (IOException | URISyntaxException e) {
+							logger.error("Failed to open the browser to the url", e);
+						}
+					}else{
+						logger.error("Desktop not supported. Cant browse {}", hyperlinkEvent.getURL());
+					}
+				}
+				else if ( eventType == HyperlinkEvent.EventType.ENTERED ){
+					createdJEditorPane.setToolTipText(hyperlinkEvent.getURL().toExternalForm());
+					ToolTipManager.sharedInstance().setInitialDelay(0);// TODO sould be set at the starting of the application
+					ToolTipManager.sharedInstance().setDismissDelay(5000);// TODO sould be set at the starting of the application
+				}
+				else if ( eventType == HyperlinkEvent.EventType.EXITED ){
+					createdJEditorPane.setToolTipText(null);// null to turn off the tooltips.
+				}
+			}
+		});
+		return createdJEditorPane;
 	}
 }
