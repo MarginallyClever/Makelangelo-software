2a01b2fca296742f5f07e73d450c2637d78ee097
PPAC37 <pierre.pacaud.37@gmail.com>
2022-02-07 01:25:24 +0100

    lambda expression, correcting/removing some comments.

--- src/main/java/com/marginallyclever/makelangelo/select/SelectReadOnlyText.java
+++ src/main/java/com/marginallyclever/makelangelo/select/SelectReadOnlyText.java
@@ -14,17 +14,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Read only JEditorPane with an HyperLinckListener for text/html contening or not multiple html link.
- * With clickable link (Desktop.BROWSE) and ToolTips when hovered over .
+ * Read only JEditorPane with an HyperlinkListener for text/html, contening or not, multiple html link.
+ * With clickable link (Desktop.BROWSE) and ToolTips when hovered over.
  * 
- * @author Dan Royer (and modified by PPAC37)
+ * @author Dan Royer
  * @since 7.24.0
  */
 public class SelectReadOnlyText extends Select {
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = -8918068053490064344L;// TODO to regenerat ?
+	private static final long serialVersionUID = -8918068053490064344L;
 	
 	private static final Logger logger = LoggerFactory.getLogger(SelectReadOnlyText.class);
 		
@@ -47,36 +47,33 @@ public class SelectReadOnlyText extends Select {
 		createdJEditorPane.setOpaque(false);
 		createdJEditorPane.setContentType("text/html");
 		createdJEditorPane.setText(sToSetAsTextToTheHtmlEditorPane);
-		createdJEditorPane.addHyperlinkListener(new HyperlinkListener() {
-			@Override
-			public void hyperlinkUpdate(HyperlinkEvent hyperlinkEvent) {
-				HyperlinkEvent.EventType eventType = hyperlinkEvent.getEventType();
-				if (eventType == HyperlinkEvent.EventType.ACTIVATED) {
-					if (Desktop.isDesktopSupported()) {
-						try {
-							URI u = hyperlinkEvent.getURL().toURI();
-							Desktop desktop = Desktop.getDesktop();
-							if ( desktop.isSupported(Desktop.Action.BROWSE)){
-								logger.debug("Desktop.Action.BROWSE {}", u);
-								desktop.browse(u);
-							}else{
-								logger.error("Desktop.Action.BROWSE not supported. Cant browse {}", u);
-							}
-						} catch (IOException | URISyntaxException e) {
-							logger.error("Failed to open the browser to the url", e);
+		createdJEditorPane.addHyperlinkListener((HyperlinkEvent hyperlinkEvent) -> {
+			HyperlinkEvent.EventType eventType = hyperlinkEvent.getEventType();
+			if (eventType == HyperlinkEvent.EventType.ACTIVATED) {
+				if (Desktop.isDesktopSupported()) {
+					try {
+						URI u = hyperlinkEvent.getURL().toURI();
+						Desktop desktop = Desktop.getDesktop();
+						if ( desktop.isSupported(Desktop.Action.BROWSE)){
+							logger.debug("Desktop.Action.BROWSE {}", u);
+							desktop.browse(u);
+						}else{
+							logger.error("Desktop.Action.BROWSE not supported. Cant browse {}", u);
 						}
-					}else{
-						logger.error("Desktop not supported. Cant browse {}", hyperlinkEvent.getURL());
+					} catch (IOException | URISyntaxException e) {
+						logger.error("Failed to open the browser to the url", e);
 					}
+				}else{
+					logger.error("Desktop not supported. Cant browse {}", hyperlinkEvent.getURL());
 				}
-				else if ( eventType == HyperlinkEvent.EventType.ENTERED ){
-					createdJEditorPane.setToolTipText(hyperlinkEvent.getURL().toExternalForm());
-					ToolTipManager.sharedInstance().setInitialDelay(0);// TODO sould be set at the starting of the application
-					ToolTipManager.sharedInstance().setDismissDelay(5000);// TODO sould be set at the starting of the application
-				}
-				else if ( eventType == HyperlinkEvent.EventType.EXITED ){
-					createdJEditorPane.setToolTipText(null);// null to turn off the tooltips.
-				}
+			}
+			else if ( eventType == HyperlinkEvent.EventType.ENTERED ){
+				createdJEditorPane.setToolTipText(hyperlinkEvent.getURL().toExternalForm());
+				ToolTipManager.sharedInstance().setInitialDelay(0);// TODO sould be set at the starting of the application
+				ToolTipManager.sharedInstance().setDismissDelay(5000);// TODO sould be set at the starting of the application
+			}
+			else if ( eventType == HyperlinkEvent.EventType.EXITED ){
+				createdJEditorPane.setToolTipText(null);// null to turn off the tooltips.
 			}
 		});
 		return createdJEditorPane;
