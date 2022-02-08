8d8aeb643010b2679a8f8952bb69fa5eb62b0fab
PPAC37 <94939582+PPAC37@users.noreply.github.com>
2022-02-08 19:00:05 +0100

    refactoring utility function in SelectReadOnlyText to create JeditoPane whith HyperLinkListenerAndToolTips (#522)
    * refactoring utility function in SelectReadOnlyText to create JeditoPane whith HyperLinkListenerAndToolTips
    
    * lambda expression, correcting/removing some comments.
    
    * removing unused import.

--- src/main/java/com/marginallyclever/makelangelo/DialogBadFirmwareVersion.java
+++ src/main/java/com/marginallyclever/makelangelo/DialogBadFirmwareVersion.java
@@ -1,17 +1,11 @@
 package com.marginallyclever.makelangelo;
 
+import com.marginallyclever.makelangelo.select.SelectReadOnlyText;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.awt.Component;
-import java.awt.Desktop;
-import java.io.IOException;
-import java.net.URISyntaxException;
-
-import javax.swing.JEditorPane;
 import javax.swing.JOptionPane;
-import javax.swing.event.HyperlinkEvent;
-import javax.swing.event.HyperlinkListener;
 import javax.swing.text.JTextComponent;
 
 @Deprecated
@@ -35,38 +29,9 @@ public class DialogBadFirmwareVersion {
 		return Translator.get("firmwareVersionBadMessage", new String[]{version});
 	}
 
-	/**
-	 * @param html String of valid HTML.
-	 * @return a text component with clickable links.
-	 */
-	private JTextComponent createHyperlinkListenableJEditorPane(String html) {
-		final JEditorPane bottomText = new JEditorPane();
-		bottomText.setContentType("text/html");
-		bottomText.setEditable(false);
-		bottomText.setText(html);
-		bottomText.setOpaque(false);
-		final HyperlinkListener hyperlinkListener = new HyperlinkListener() {
-			public void hyperlinkUpdate(HyperlinkEvent hyperlinkEvent) {
-				if (hyperlinkEvent.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
-					if (Desktop.isDesktopSupported()) {
-						try {
-							Desktop.getDesktop().browse(hyperlinkEvent.getURL().toURI());
-						} catch (IOException | URISyntaxException e) {
-							logger.error("Failed to open the browser to the firmware url", e);
-						}
-					}
-
-				}
-			}
-		};
-		bottomText.addHyperlinkListener(hyperlinkListener);
-		return bottomText;
-	}
-
-
 	public void display(Component parent,String version) {
 		final String aboutHtml = getAboutHtmlFromMultilingualString(version);
-		final JTextComponent bottomText = createHyperlinkListenableJEditorPane(aboutHtml);
+		final JTextComponent bottomText = SelectReadOnlyText.createJEditorPaneWithHyperlinkListenerAndToolTipsForDesktopBrowse(aboutHtml);
 		JOptionPane.showMessageDialog(parent, bottomText, Translator.get("firmwareVersionBadTitle"), JOptionPane.ERROR_MESSAGE);
 	}
 }
