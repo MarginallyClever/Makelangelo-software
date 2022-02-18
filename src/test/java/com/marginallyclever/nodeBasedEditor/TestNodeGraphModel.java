8351a4445a7914fea8c8501a017ceadfc4c7efee
Dan Royer <dan@marginallyclever.com>
2022-02-18 15:51:54 -0800

    added more tests

--- src/test/java/com/marginallyclever/nodeBasedEditor/TestNodeGraphModel.java
+++ src/test/java/com/marginallyclever/nodeBasedEditor/TestNodeGraphModel.java
@@ -6,30 +6,43 @@ import com.marginallyclever.nodeBasedEditor.model.NodeGraphModel;
 import com.marginallyclever.nodeBasedEditor.model.builtInNodes.Add;
 import com.marginallyclever.nodeBasedEditor.model.builtInNodes.Constant;
 import com.marginallyclever.nodeBasedEditor.model.builtInNodes.ReportToStdOut;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestNodeGraphModel {
+    private static NodeGraphModel model;
+
+    @BeforeAll
+    static void beforeAll() {
+        model = new NodeGraphModel();
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        model.clear();
+    }
+
     @Test
     public void testAdd() {
         Node add = new Add();
-        add.getVariable(0).setValue(Integer.valueOf(5));
-        add.getVariable(1).setValue(Integer.valueOf(10));
+        add.getVariable(0).setValue(5);
+        add.getVariable(1).setValue(10);
         add.update();
         assertEquals( 15.0, add.getVariable(2).getValue() );
     }
 
     @Test
     public void testAddAndReport() throws Exception {
-        NodeGraphModel model = new NodeGraphModel();
         Node constant0 = model.addNode(new Constant(1));
         Node constant1 = model.addNode(new Constant(2));
         Node add = model.addNode(new Add());
         Node report = model.addNode(new ReportToStdOut());
-        NodeConnection c0 = model.addConnection(new NodeConnection(constant0,0,add,0));
-        NodeConnection c1 = model.addConnection(new NodeConnection(constant1,0,add,1));
-        NodeConnection c2 = model.addConnection(new NodeConnection(add,2,report,0));
+        model.addConnection(new NodeConnection(constant0,0,add,0));
+        model.addConnection(new NodeConnection(constant1,0,add,1));
+        model.addConnection(new NodeConnection(add,2,report,0));
 
         for(int i=0;i<3;++i) {
             model.update();
@@ -37,5 +50,26 @@ public class TestNodeGraphModel {
 
         System.out.println("Done");
         System.out.println(model);
+    }
+
+    @Test
+    public void testFactoryFailsOnBadRequests() {
+        assertThrows(IllegalArgumentException.class, ()->{
+            NodeFactory.createNode("");
+        });
+
+        assertThrows(IllegalArgumentException.class, ()->{
+            NodeFactory.createNode(null);
+        });
+    }
+
+    @Test
+    public void testFactoryCreatesAllDefaultTypes() {
+        int count=0;
+        for(String s : NodeFactory.getNames()) {
+            assertNotNull(NodeFactory.createNode(s));
+            ++count;
+        }
+        assertNotEquals(0,count);
     }
 }
