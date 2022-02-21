ed050bee0692dce6f03d3a40c8cb531377881ca2
Dan Royer <dan@marginallyclever.com>
2022-02-21 11:10:11 -0800

    Create Min.java

---
+++ src/main/java/com/marginallyclever/nodeBasedEditor/model/builtInNodes/math/Min.java
@@ -0,0 +1,31 @@
+package com.marginallyclever.nodeBasedEditor.model.builtInNodes.math;
+
+import com.marginallyclever.nodeBasedEditor.model.Node;
+import com.marginallyclever.nodeBasedEditor.model.NodeVariable;
+
+public class Min extends Node {
+    private final NodeVariable<Number> a = NodeVariable.newInstance("A",Number.class,0,true,false);
+    private final NodeVariable<Number> b = NodeVariable.newInstance("B",Number.class,0,true,false);
+    private final NodeVariable<Number> c = NodeVariable.newInstance("output",Number.class,0,false,true);
+
+    public Min() {
+        super("Min");
+        addVariable(a);
+        addVariable(b);
+        addVariable(c);
+    }
+
+    @Override
+    public Node create() {
+        return new Min();
+    }
+
+    @Override
+    public void update() {
+        if(!isDirty()) return;
+        double av = a.getValue().doubleValue();
+        double bv = b.getValue().doubleValue();
+        c.setValue(Math.min(av,bv));
+        cleanAllInputs();
+    }
+}
