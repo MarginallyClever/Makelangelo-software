ad5f0f38d99fffed207e763089abf1f2a3b9f3e3
Dan Royer <dan@marginallyclever.com>
2022-02-21 10:46:04 -0800

    Create SupergraphOutput.java

---
+++ src/main/java/com/marginallyclever/nodeBasedEditor/SupergraphOutput.java
@@ -0,0 +1,7 @@
+package com.marginallyclever.nodeBasedEditor;
+
+/**
+ * Nodes with this interface are advertising their outputs to the supergraph.
+ */
+public abstract interface SupergraphOutput {
+}
