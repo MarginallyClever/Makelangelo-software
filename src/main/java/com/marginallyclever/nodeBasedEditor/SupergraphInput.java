eee84072e482d2ef6c18aa630487f9a253bc297a
Dan Royer <dan@marginallyclever.com>
2022-02-21 10:46:01 -0800

    Create SupergraphInput.java

---
+++ src/main/java/com/marginallyclever/nodeBasedEditor/SupergraphInput.java
@@ -0,0 +1,7 @@
+package com.marginallyclever.nodeBasedEditor;
+
+/**
+ * Nodes with this interface are advertising their inputs to the supergraph.
+ */
+public abstract interface SupergraphInput {
+}
