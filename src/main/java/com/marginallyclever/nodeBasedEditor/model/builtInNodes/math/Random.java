2026c4f0eba78029d1e48666eab92de6609c65d3
Dan Royer <dan@marginallyclever.com>
2022-02-21 10:46:17 -0800

    move Random

--- src/main/java/com/marginallyclever/nodeBasedEditor/model/builtInNodes/math/Random.java
+++ src/main/java/com/marginallyclever/nodeBasedEditor/model/builtInNodes/math/Random.java
@@ -1,4 +1,4 @@
-package com.marginallyclever.nodeBasedEditor.model.builtInNodes;
+package com.marginallyclever.nodeBasedEditor.model.builtInNodes.math;
 
 import com.marginallyclever.nodeBasedEditor.model.Node;
 import com.marginallyclever.nodeBasedEditor.model.NodeVariable;
