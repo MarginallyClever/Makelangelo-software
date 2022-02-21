8dbf59192fc00de0186ccb3ffa4b3950dd66fb89
Dan Royer <dan@marginallyclever.com>
2022-02-21 10:50:49 -0800

    Update PrintWithGraphics.java

--- src/main/java/com/marginallyclever/nodeBasedEditor/PrintWithGraphics.java
+++ src/main/java/com/marginallyclever/nodeBasedEditor/PrintWithGraphics.java
@@ -1,7 +1,10 @@
 package com.marginallyclever.nodeBasedEditor;
 
-import java.awt.*;
-
-public abstract interface PrintWithGraphics {
-    public abstract void print(Graphics g);
+/**
+ * {@link com.marginallyclever.nodeBasedEditor.model.Node}s with this interface can draw to anything that extends
+ * Swing's {@link java.awt.Graphics} class.
+ * TODO don't drag Swing into the model!
+ */
+public interface PrintWithGraphics {
+    void print(java.awt.Graphics g);
 }
