MarginallyClever/Makelangelo-software

commit d359e642db62c2b4c207d8d18ed20edf42ef1356

Turtle.equals() should now work correctly.

src/main/java/com/marginallyclever/makelangelo/turtle/Turtle.java
@@ -10,6 +10,7 @@ import java.awt.*;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.locks.ReentrantLock;
 
 
@@ -24,7 +25,7 @@ public class Turtle implements Cloneable {
 	
 	public List<TurtleMove> history;
 
-	private ReentrantLock lock = new ReentrantLock();
+	private final transient ReentrantLock lock = new ReentrantLock();
 
 	// current state
 	private double px, py;
@@ -452,5 +453,76 @@ public class Turtle implements Cloneable {
 		}
 		
 		return new ColorRGB(0,0,0);
+	}
+
+	/**
+	 * Returns the total distance of all pen-down moves within this {@link Turtle}.
+	 * @return the total distance of all pen-down moves within this {@link Turtle}.
+	 */
+    public double getDrawDistance() {
+		double d=0;
+		TurtleMove prev = new TurtleMove(0,0,TurtleMove.TRAVEL);
+		for( TurtleMove m : history) {
+			if(m.type == TurtleMove.DRAW_LINE) {
+				double dx = m.x-prev.x;
+				double dy = m.y-prev.y;
+				d += Math.sqrt(dx*dx+dy*dy);
+				prev = m;
+			} else if(m.type == TurtleMove.TRAVEL) {
+				prev = m;
+			}
+		}
+		return d;
+    }
+
+	/**
+	 * Returns a point along the drawn lines of this {@link Turtle}
+	 * @param t a value from 0...{@link Turtle#getDrawDistance()}, inclusive.
+	 * @return a point along the drawn lines of this {@link Turtle}
+	 */
+	public Point2D interpolate(double t) {
+		double d=0;
+		TurtleMove prev = new TurtleMove(0,0,TurtleMove.TRAVEL);
+		for( TurtleMove m : history) {
+			if(m.type == TurtleMove.DRAW_LINE) {
+				double dx = m.x-prev.x;
+				double dy = m.y-prev.y;
+				double change = Math.sqrt(dx*dx+dy*dy);
+				if(d+change>=t) {
+					double v = (t-d)/change;
+					v = Math.max(Math.min(v,1),0);
+					return new Point2D(
+							prev.x + dx * v,
+							prev.y + dy * v);
+				}
+				d += change;
+				prev = m;
+			} else if(m.type == TurtleMove.TRAVEL) {
+				prev = m;
+			}
+		}
+		return new Point2D(prev.x,prev.y);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Turtle turtle = (Turtle) o;
+		return Double.compare(turtle.px, px) == 0 &&
+				Double.compare(turtle.py, py) == 0 &&
+				Double.compare(turtle.nx, nx) == 0 &&
+				Double.compare(turtle.ny, ny) == 0 &&
+				Double.compare(turtle.angle, angle) == 0 &&
+				isUp == turtle.isUp &&
+				Double.compare(turtle.diameter, diameter) == 0 &&
+				history.equals(turtle.history) &&
+				Objects.equals(color, turtle.color);
+	}
+
+	@Override
+	public int hashCode() {
+		history.hashCode();
+		return Objects.hash(history, px, py, nx, ny, angle, isUp, color, diameter);
 	}
 }

src/main/java/com/marginallyclever/makelangelo/turtle/TurtleMove.java
@@ -1,9 +1,10 @@
 package com.marginallyclever.makelangelo.turtle;
 
-import java.awt.Color;
-
 import com.marginallyclever.convenience.ColorRGB;
 import com.marginallyclever.convenience.StringHelper;
+
+import java.awt.*;
+import java.util.Objects;
 
 public class TurtleMove {
 
@@ -39,5 +40,18 @@ public class TurtleMove {
 		default:
 			return "DRAW_LINE X"+StringHelper.formatDouble(x)+" Y"+StringHelper.formatDouble(y);
 		}
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		TurtleMove that = (TurtleMove) o;
+		return type == that.type && Double.compare(that.x, x) == 0 && Double.compare(that.y, y) == 0;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(type, x, y);
 	}
 }

commit ce8b033c7ed1c2c975e031d8dd978b937d861d01

🐛 Fix compilation issue introduced in d359e64

.github/workflows/maven.yml
@@ -8,6 +8,8 @@ env:
 
 on:
   push:
+    branches:
+      - master
   pull_request:
 
 jobs:
@@ -20,6 +22,7 @@ jobs:
       with:
         distribution: 'temurin'
         java-version: '17'
+        cache: 'maven'
 
     # from https://help.github.com/actions/language-and-framework-guides/building-and-testing-java-with-maven
     - name: Build and Test with Maven

src/main/java/com/marginallyclever/makelangelo/turtle/Turtle.java
@@ -461,14 +461,14 @@ public class Turtle implements Cloneable {
 	 */
     public double getDrawDistance() {
 		double d=0;
-		TurtleMove prev = new TurtleMove(0,0,TurtleMove.TRAVEL);
+		TurtleMove prev = new TurtleMove(0,0,MovementType.TRAVEL);
 		for( TurtleMove m : history) {
-			if(m.type == TurtleMove.DRAW_LINE) {
+			if(m.type == MovementType.DRAW_LINE) {
 				double dx = m.x-prev.x;
 				double dy = m.y-prev.y;
 				d += Math.sqrt(dx*dx+dy*dy);
 				prev = m;
-			} else if(m.type == TurtleMove.TRAVEL) {
+			} else if(m.type == MovementType.TRAVEL) {
 				prev = m;
 			}
 		}
@@ -482,9 +482,9 @@ public class Turtle implements Cloneable {
 	 */
 	public Point2D interpolate(double t) {
 		double d=0;
-		TurtleMove prev = new TurtleMove(0,0,TurtleMove.TRAVEL);
+		TurtleMove prev = new TurtleMove(0,0,MovementType.TRAVEL);
 		for( TurtleMove m : history) {
-			if(m.type == TurtleMove.DRAW_LINE) {
+			if(m.type == MovementType.DRAW_LINE) {
 				double dx = m.x-prev.x;
 				double dy = m.y-prev.y;
 				double change = Math.sqrt(dx*dx+dy*dy);
@@ -497,7 +497,7 @@ public class Turtle implements Cloneable {
 				}
 				d += change;
 				prev = m;
-			} else if(m.type == TurtleMove.TRAVEL) {
+			} else if(m.type == MovementType.TRAVEL) {
 				prev = m;
 			}
 		}
@@ -522,7 +522,6 @@ public class Turtle implements Cloneable {
 
 	@Override
 	public int hashCode() {
-		history.hashCode();
 		return Objects.hash(history, px, py, nx, ny, angle, isUp, color, diameter);
 	}
 }