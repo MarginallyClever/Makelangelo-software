7d5d486ee938f243d65a37084e14fe43fedecd28
PPAC37 <pierre.pacaud.37@gmail.com>
2022-03-02 18:57:37 +0100

    FunctionSolverTest expected exception test.

--- src/test/java/com/marginallyclever/util/FunctionSolverTest.java
+++ src/test/java/com/marginallyclever/util/FunctionSolverTest.java
@@ -12,7 +12,7 @@ public class FunctionSolverTest {
 	
 	@Test
 	public void testSolveNumericExpression_Minus() throws Exception {
-		System.out.println("solveNumericExpression");
+		System.out.println("solveNumericExpression_Minus");
 		String expression = "10 - 10.0";
 		double expResult = 0.0;
 		double result = FunctionSolver.solveNumericExpression(expression);
@@ -21,11 +21,23 @@ public class FunctionSolverTest {
 	
 	@Test
 	public void testSolveNumericExpression_Add() throws Exception {
-		System.out.println("solveNumericExpression");
+		System.out.println("solveNumericExpression_Add");
 		String expression = "-10 + 10.0";
 		double expResult = 0.0;
 		double result = FunctionSolver.solveNumericExpression(expression);
 		assertEquals(expResult, result, 0.0);
 	}
-
+	
+	@Test
+	public void testSolveNumericExpression_ExceptionIfNotAnExpression()  {
+		System.out.println("solveNumericExpression_ExceptionIfNotAnExpression");
+		String expression = "A-10 + 10.0";
+		try{
+			FunctionSolver.solveNumericExpression(expression);
+			fail("shoud throw an exception with this expression "+expression);
+		}
+		catch (Exception e ){
+			System.out.println("OK : exception : "+e.getMessage());
+		}
+	}
 }
