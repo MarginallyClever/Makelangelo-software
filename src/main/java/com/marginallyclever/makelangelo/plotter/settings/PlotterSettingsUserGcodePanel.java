fe0ba9b5403abffd5962caacebd8977466383d19
PPAC37 <pierre.pacaud.37@gmail.com>
2022-03-09 16:25:25 +0100

    Plotter Settings User GCode SplitPane

--- src/main/java/com/marginallyclever/makelangelo/plotter/settings/PlotterSettingsUserGcodePanel.java
+++ src/main/java/com/marginallyclever/makelangelo/plotter/settings/PlotterSettingsUserGcodePanel.java
@@ -5,6 +5,7 @@ package com.marginallyclever.makelangelo.plotter.settings;
 import com.marginallyclever.makelangelo.Translator;
 import com.marginallyclever.makelangelo.plotter.Plotter;
 import com.marginallyclever.makelangelo.select.SelectTextArea;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -16,104 +17,96 @@ public class PlotterSettingsUserGcodePanel extends JPanel {
 
 	private final Plotter myPlotter;
 
-	private JButton buttonSave;
-
+	private javax.swing.JSplitPane jSplitPane1;
+	
+	private SelectTextArea replacementPatternList;
+	
+	private javax.swing.JTabbedPane jTabbedPane1;
+	
 	private SelectTextArea userGeneralStartGcode;
 	private SelectTextArea userGeneralEndGcode;
 	private SelectTextArea userToolChangeStartGcode;
 	private SelectTextArea userToolChangeEndGcode;
 
-	private SelectTextArea replacementPatternList;
-
-	private javax.swing.JTabbedPane jTabbedPane1;
+	private JButton buttonSave;
 
 	public PlotterSettingsUserGcodePanel(Plotter robot) {
 		super();
-		
-		
 		this.myPlotter = robot;
 		initComponents();
-
 	}
 
 	private void initComponents() {
-
 		PlotterSettings settings = myPlotter.getSettings();
 		setLayout(new java.awt.BorderLayout());
-		jTabbedPane1 = new javax.swing.JTabbedPane();
-
+		//
+		jTabbedPane1 = new javax.swing.JTabbedPane();		
+        jSplitPane1 = new javax.swing.JSplitPane();
+		//
 		userGeneralStartGcode = new SelectTextArea(
 				"userGeneralStartGcode",
-				Translator.get("PlotterSettings.userGeneralStartGcode"),
+				null,
 				settings.getUserGcode().getUserGeneralStartGcode()
 		);
 		userGeneralStartGcode.setLineWrap(false);
-		//add(userGeneralStartGcode);
 		jTabbedPane1.addTab(Translator.get("PlotterSettings.userGeneralStartGcode"), userGeneralStartGcode);
-		
-		userGeneralEndGcode
-				= new SelectTextArea(
-						"userGeneralEndGcode",
-						Translator.get("PlotterSettings.userGeneralEndGcode"),
-						settings.getUserGcode().getUserGeneralEndGcode()
-				);
+		//
+		userGeneralEndGcode = new SelectTextArea(
+				"userGeneralEndGcode",
+				null,
+				settings.getUserGcode().getUserGeneralEndGcode()
+		);
 		userGeneralEndGcode.setLineWrap(false);
-		//add(userGeneralEndGcode);
 		jTabbedPane1.addTab(Translator.get("PlotterSettings.userGeneralEndGcode"), userGeneralEndGcode);
-
-		userToolChangeStartGcode
-				= new SelectTextArea(
-						"userToolChangeStartGcode",
-						Translator.get("PlotterSettings.userToolChangeStartGcode"),
-						settings.getUserGcode().getUserToolChangeStartGcode()
-				);
+		//
+		userToolChangeStartGcode = new SelectTextArea(
+				"userToolChangeStartGcode",
+				null,
+				settings.getUserGcode().getUserToolChangeStartGcode()
+		);
 		userToolChangeStartGcode.setLineWrap(false);
-		//add(userToolChangeStartGcode);
 		jTabbedPane1.addTab(Translator.get("PlotterSettings.userToolChangeStartGcode"), userToolChangeStartGcode);
-		
-		userToolChangeEndGcode
-				= new SelectTextArea(
-						"userToolChangeEndGcode",
-						Translator.get("PlotterSettings.userToolChangeEndGcode"),
-						settings.getUserGcode().getUserToolChangeEndGcode()
-				);
+		//
+		userToolChangeEndGcode = new SelectTextArea(
+				"userToolChangeEndGcode",
+				null,
+				settings.getUserGcode().getUserToolChangeEndGcode()
+		);
 		userToolChangeEndGcode.setLineWrap(false);
-		//add(userToolChangeEndGcode);
 		jTabbedPane1.addTab(Translator.get("PlotterSettings.userToolChangeEndGcode"), userToolChangeEndGcode);
-		//
-		add(jTabbedPane1, java.awt.BorderLayout.CENTER);
-		//
-
-		replacementPatternList
-				= new SelectTextArea(
-						"replacementPattern",
-						Translator.get("PlotterSettings.replacementPattern"),
-						settings.getUserGcode().getAllPattern(settings)
-				);
+		//		
+		replacementPatternList = new SelectTextArea(
+				"replacementPattern",
+				Translator.get("PlotterSettings.replacementPattern"),
+				settings.getUserGcode().getAllPattern(settings)
+		);
 		replacementPatternList.setLineWrap(false);
 		replacementPatternList.setEditable(false);
 		replacementPatternList.setDragEnabled(true);
-		add(replacementPatternList, java.awt.BorderLayout.WEST);
-
+		//
+		jSplitPane1.setLeftComponent(replacementPatternList);
+		jSplitPane1.setRightComponent(jTabbedPane1);
+		jSplitPane1.setDividerLocation(250);
+		//
+		add(jSplitPane1, java.awt.BorderLayout.CENTER);
+		//
 		JPanel bottom = new JPanel(new FlowLayout());
 		buttonSave = new JButton(Translator.get("Save"));
 		bottom.add(buttonSave);
 		buttonSave.addActionListener((e) -> {
 			save();
 		});
-
-		
-		
 		add(bottom, java.awt.BorderLayout.SOUTH);
+		//
+		setMinimumSize(new Dimension(640, 400));
+		setPreferredSize(new Dimension(640, 400));
 	}
 
 	private void save() {
-
 		PlotterSettings settings = myPlotter.getSettings();
 		settings.getUserGcode().setUserGeneralStartGcode(userGeneralStartGcode.getText());
 		settings.getUserGcode().setUserGeneralEndGcode(userGeneralEndGcode.getText());
 		settings.getUserGcode().setUserToolChangeStartGcode(userToolChangeStartGcode.getText());
 		settings.getUserGcode().setUserToolChangeEndGcode(userToolChangeEndGcode.getText());
-
 	}
 }
