3e9d08d7187a0c089c4429c80aa1446c32a788de
Dan Royer <dan@marginallyclever.com>
2022-03-11 14:13:41 -0800

    Update TurtleMove.java

--- src/main/java/com/marginallyclever/makelangelo/turtle/TurtleMove.java
+++ src/main/java/com/marginallyclever/makelangelo/turtle/TurtleMove.java
@@ -21,6 +21,10 @@ public class TurtleMove {
 		this(m.x,m.y,m.type);
 	}
 
+	public void setColor(ColorRGB c) {
+		this.x = c.toInt();
+	}
+
 	public ColorRGB getColor() {
 		return new ColorRGB((int)x);
 	}
@@ -28,7 +32,11 @@ public class TurtleMove {
 	public double getDiameter() {
 		return y;
 	}
-	
+
+	public void setDiameter(double d) {
+		y=d;
+	}
+
 	public String toString() {
 		switch(type) {
 		case TOOL_CHANGE:
