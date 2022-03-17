6bd71c63c8ec64126902c8983b107fe1f6bc329b
coliss86 <coliss86@yahoo.fr>
2022-03-17 23:20:46 +0100

    makeArt ?

--- src/main/java/com/marginallyclever/makelangelo/makeart/ScaleTurtle.java
+++
@@ -1,9 +0,0 @@
-package com.marginallyclever.makelangelo.makeart;
-
-import com.marginallyclever.makelangelo.turtle.Turtle;
-
-public class ScaleTurtle {
-	public static void run(Turtle turtle,double sx,double sy) {
-		turtle.scale(sx,sy);
-	}
-}
--- src/main/java/com/marginallyclever/makelangelo/makeart/imageconverter/voronoi/VoronoiCellEdge.java
+++
@@ -1,6 +0,0 @@
-package com.marginallyclever.makelangelo.makeart.imageConverter.voronoi;
-
-public class VoronoiCellEdge {
-	public double px, py;
-	public double nx, ny;
-}
