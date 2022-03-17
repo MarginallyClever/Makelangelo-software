0a37c74874b56be2260c688e5632af1efbd043ad
Dan Royer <dan@marginallyclever.com>
2022-03-16 23:50:34 -0700

    fix string label in multi-file gcode output (#614)

--- src/main/java/com/marginallyclever/makelangelo/plotter/plotterControls/SaveGCode.java
+++ src/main/java/com/marginallyclever/makelangelo/plotter/plotterControls/SaveGCode.java
@@ -47,7 +47,51 @@ public class SaveGCode {
 			String selectedFile = fc.getSelectedFile().getAbsolutePath();
 			String fileWithExtension = addExtension(selectedFile,((FileNameExtensionFilter)fc.getFileFilter()).getExtensions());
 			logger.debug("File selected by user: {}", fileWithExtension);
-			save(fileWithExtension,turtle,robot);
+
+			int count = countTurtleToolChanges(turtle);
+			if(count>1) {
+				maybeSaveSeparateFiles(count,fileWithExtension, turtle, plotter,parent);
+			} else {
+				saveOneFile(fileWithExtension, turtle, plotter);
+			}
+		}
+	}
+
+	/**
+	 * Offer to split the gcode file into one file per tool change, which is probably one per color.
+	 * @param count number of tool changes detected
+	 * @param fileWithExtension the user's desired filename.
+	 * @param turtle the source turtle containing many tool changes.
+	 * @param plotter the plotter that will translate the turtle to gcode.
+	 * @param parent the parent frame for the confirmation dialog.
+	 * @throws Exception if saving the file fails.
+	 */
+	private void maybeSaveSeparateFiles(int count,String fileWithExtension, Turtle turtle, Plotter plotter, JFrame parent) throws Exception {
+		String title = Translator.get("SaveGCode.splitGCodeTitle");
+		String query = Translator.get("SaveGCode.splitGCodeQuestion",new String[]{Integer.toString(count)});
+		int n = JOptionPane.showConfirmDialog(parent, query, title, JOptionPane.YES_NO_OPTION);
+		if(n==JOptionPane.NO_OPTION) {
+			saveOneFile(fileWithExtension, turtle, plotter);
+		} else if(n==JOptionPane.YES_OPTION) {
+			// split filename.ext.  New format will be filename-n.ext
+			int last = fileWithExtension.lastIndexOf(".");
+			String ext = fileWithExtension.substring(last);
+			String fileWithoutExtension = fileWithExtension.substring(0,last);
+			// now save each file
+			List<Turtle> list = turtle.splitByToolChange();
+			int i=0;
+			for( Turtle t : list ) {
+				++i;
+				String newFileName = fileWithoutExtension+"-"+Integer.toString(i)+ext;
+				saveOneFile(newFileName,turtle,plotter);
+			}
+		}
+	}
+
+	private int countTurtleToolChanges(Turtle turtle) {
+		int i=0;
+		for( TurtleMove m : turtle.history ) {
+			if(m.type == MovementType.TOOL_CHANGE) ++i;
 		}
 	}
 
