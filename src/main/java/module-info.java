83f2e233014befdf5adee0ad49d87752691b5871
Dan Royer <dan@marginallyclever.com>
2022-03-27 15:23:16 -0700

    removed 'uses' as it does not.  it only provides the service.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -15,6 +15,7 @@ module com.marginallyclever.makelangelo {
     requires jrpicam;
     requires org.jetbrains.annotations;
     requires com.formdev.flatlaf;
+    requires org.reflections;
 
     opens com.marginallyclever.convenience;
     opens com.marginallyclever.makelangelo.makeart.io.vector;
@@ -22,14 +23,14 @@ module com.marginallyclever.makelangelo {
     opens com.marginallyclever.makelangelo.turtle;
 
     exports com.marginallyclever.convenience.log to logback.core;
+
+    exports com.marginallyclever.makelangelo.donatelloimpl to com.marginallyclever.nodegraphcore;
     exports com.marginallyclever.makelangelo.donatelloimpl.nodes to com.marginallyclever.nodegraphcore;
     exports com.marginallyclever.makelangelo.donatelloimpl.nodes.shapes to com.marginallyclever.nodegraphcore;
 
-    uses com.marginallyclever.nodegraphcore.NodeRegistry;
     provides com.marginallyclever.nodegraphcore.NodeRegistry with
             com.marginallyclever.makelangelo.donatelloimpl.DonatelloRegistry;
 
-    uses com.marginallyclever.nodegraphcore.DAORegistry;
     provides com.marginallyclever.nodegraphcore.DAORegistry with
             com.marginallyclever.makelangelo.donatelloimpl.DonatelloRegistry;
 }
