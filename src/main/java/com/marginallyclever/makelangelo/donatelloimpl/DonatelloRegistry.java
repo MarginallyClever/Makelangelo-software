e62282c5d620af846a7351d894822b64c0aeb558
Dan Royer <dan@marginallyclever.com>
2022-03-27 15:23:35 -0700

    switch back to the old dumb way

--- src/main/java/com/marginallyclever/makelangelo/donatelloimpl/DonatelloRegistry.java
+++ src/main/java/com/marginallyclever/makelangelo/donatelloimpl/DonatelloRegistry.java
@@ -1,6 +1,10 @@
 package com.marginallyclever.makelangelo.donatelloimpl;
 
-import com.marginallyclever.makelangelo.donatelloimpl.nodes.TurtleDAO4JSON;
+import com.marginallyclever.makelangelo.donatelloimpl.nodes.*;
+import com.marginallyclever.makelangelo.donatelloimpl.nodes.shapes.Circle;
+import com.marginallyclever.makelangelo.donatelloimpl.nodes.shapes.Line;
+import com.marginallyclever.makelangelo.donatelloimpl.nodes.shapes.NGon;
+import com.marginallyclever.makelangelo.donatelloimpl.nodes.shapes.Rectangle;
 import com.marginallyclever.makelangelo.turtle.Turtle;
 import com.marginallyclever.nodegraphcore.DAO4JSONFactory;
 import com.marginallyclever.nodegraphcore.DAORegistry;
@@ -17,6 +21,10 @@ import org.slf4j.LoggerFactory;
 public class DonatelloRegistry implements DAORegistry, NodeRegistry {
     private static final Logger logger = LoggerFactory.getLogger(DonatelloRegistry.class);
 
+    public String getName() {
+        return "Makelangelo";
+    }
+
     @Override
     public void registerDAO() {
         logger.info("Registering makelangelo-software DAOs");
@@ -26,6 +34,21 @@ public class DonatelloRegistry implements DAORegistry, NodeRegistry {
     @Override
     public void registerNodes() {
         logger.info("Registering makelangelo-software nodes");
-        NodeFactory.registerAllNodesInPackage("com.marginallyclever.makelangelo.donatelloimpl.nodes");
+        //NodeFactory.registerAllNodesInPackage("com.marginallyclever.makelangelo.donatelloimpl");
+
+        NodeFactory.registerNode(Circle.class);
+        NodeFactory.registerNode(Line.class);
+        NodeFactory.registerNode(Rectangle.class);
+        NodeFactory.registerNode(AddTurtles.class);
+        NodeFactory.registerNode(ColorTurtle.class);
+        NodeFactory.registerNode(LoadTurtle.class);
+        NodeFactory.registerNode(PathImageMask.class);
+        NodeFactory.registerNode(PatternOnPath.class);
+        NodeFactory.registerNode(PointOnPath.class);
+        NodeFactory.registerNode(PrintTurtle.class);
+        NodeFactory.registerNode(SaveTurtle.class);
+        NodeFactory.registerNode(TransformTurtle.class);
+        NodeFactory.registerNode(TurtleToBufferedImage.class);
+        NodeFactory.registerNode(NGon.class);
     }
 }
