d5f6c5dcd4eddbd1b545308f6f1e7bdf023aa97e
Dan Royer <dan@marginallyclever.com>
2022-03-27 15:21:57 -0700

    switch to using logger

--- src/test/java/com/marginallyclever/donatelloimpl/TestNodeGraphMakelangelo.java
+++ src/test/java/com/marginallyclever/donatelloimpl/TestNodeGraphMakelangelo.java
@@ -9,6 +9,8 @@ import com.marginallyclever.nodegraphcore.NodeVariable;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,6 +20,8 @@ import static org.junit.jupiter.api.Assertions.*;
  * @since 2022-02-01
  */
 public class TestNodeGraphMakelangelo {
+    private static final Logger logger = LoggerFactory.getLogger(TestNodeGraphMakelangelo.class);
+
     private static NodeGraph model = new NodeGraph();
 
     @BeforeAll
@@ -26,9 +30,9 @@ public class TestNodeGraphMakelangelo {
         DAO4JSONFactory.loadRegistries();
 
         assertNotEquals(0,NodeFactory.getNames().length);
-        System.out.println("NodeFactory.getNames().length="+NodeFactory.getNames().length);
+        logger.info("NodeFactory.getNames().length="+NodeFactory.getNames().length);
         for(String s : NodeFactory.getNames()) {
-            System.out.println("NodeFactory.getNames()="+s);
+            logger.info("  found="+s);
         }
     }
 
