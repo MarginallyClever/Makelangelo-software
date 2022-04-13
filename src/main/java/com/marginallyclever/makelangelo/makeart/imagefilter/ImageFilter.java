5a04f9b9854200b79abba89b1a6c27f208675547
Dan Royer <dan@marginallyclever.com>
2022-04-13 10:14:05 -0700

    faster pixel decoding

--- src/main/java/com/marginallyclever/makelangelo/makeart/imagefilter/ImageFilter.java
+++ src/main/java/com/marginallyclever/makelangelo/makeart/imagefilter/ImageFilter.java
@@ -11,28 +11,27 @@ import java.awt.*;
  */
 public class ImageFilter {
 	public static int decode32bit(int pixel) {
-		double a = 255.0 - (double)((pixel>>24) & 0xff) / 255.0;
 		int r = ((pixel >> 16) & 0xff);
 		int g = ((pixel >> 8) & 0xff);
 		int b = ((pixel) & 0xff);
+		double a = (double) ((pixel >> 24) & 0xff) / 255.0;
 
-		int r2 = (int)( (255.0 - r) * a + r);
-		int g2 = (int)( (255.0 - g) * a + g);
-		int b2 = (int)( (255.0 - b) * a + b);
-		
-		return (r2 + g2 + b2) / 3;
+		return average(r, g, b, a);
 	}
 
 	public static int decodeColor(Color c) {
-		double r = c.getRed();
-		double g = c.getGreen();
-		double b = c.getBlue();
-		double a = 1.0 - (double)c.getAlpha() / 255.0;
+		int r = c.getRed();
+		int g = c.getGreen();
+		int b = c.getBlue();
+		double a = (double)c.getAlpha() / 255.0;
+		return average(r, g, b, a);
+	}
 
-		int r2 = (int)( (255.0 - r) * a + r);
-		int g2 = (int)( (255.0 - g) * a + g);
-		int b2 = (int)( (255.0 - b) * a + b);
-		
+	private static int average(int r, int g, int b, double a) {
+		int r2 = (int)(r * a);
+		int g2 = (int)(g * a);
+		int b2 = (int)(b * a);
+
 		return (r2 + g2 + b2) / 3;
 	}
 
