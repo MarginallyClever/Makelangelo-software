a803d81bceeecf0333c5d0aa5b58d4e48dc406e3
Dan Royer <dan@marginallyclever.com>
2023-02-21 11:59:34 -0800

    added CellularNoise

--- src/main/java/com/marginallyclever/convenience/noise/NoiseFactory.java
+++ src/main/java/com/marginallyclever/convenience/noise/NoiseFactory.java
@@ -2,13 +2,14 @@ package com.marginallyclever.convenience.noise;
 
 public class NoiseFactory {
     public static String [] getNames() {
-        return new String[] {"Perlin","Simplex"};
+        return new String[] {"Perlin","Simplex","Cellular"};
     }
 
     public static Noise getNoise(int i) {
         return switch (i) {
             case 0 -> new PerlinNoise();
             case 1 -> new SimplexNoise();
+            case 2 -> new CellularNoise();
             default -> null;
         };
     }
