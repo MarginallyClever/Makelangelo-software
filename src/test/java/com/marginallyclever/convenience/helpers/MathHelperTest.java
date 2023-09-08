8677388f44296ee13f522305853bedce2068e1e9
Dan Royer <dan@marginallyclever.com>
2023-09-08 15:31:58 -0700

    test false cases fail as they should

--- src/test/java/com/marginallyclever/convenience/helpers/MathHelperTest.java
+++ src/test/java/com/marginallyclever/convenience/helpers/MathHelperTest.java
@@ -1,24 +1,47 @@
 package com.marginallyclever.convenience.helpers;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import javax.vecmath.Point2d;
 
 public class MathHelperTest {
     @Test
-    void testBetween() {
-        Point2d a = new Point2d(0,0);
-        Point2d b = new Point2d(1,0);
-        Point2d c = new Point2d(0.5,0);
+    public void testBetween() {
+        Point2d a = new Point2d(0, 0);
+        Point2d b = new Point2d(1, 0);
+        Point2d c = new Point2d(0.5, 0);
         double epsilon = 1e-9;
 
-        assert(MathHelper.between(a,b,c,epsilon));
+        assert (MathHelper.between(a, b, c, epsilon));
+
+        for (int i = 0; i < 50; ++i) {
+            a.set(Math.random() * 500 - 250, Math.random() * 500 - 250);
+            b.set(Math.random() * 500 - 250, Math.random() * 500 - 250);
+            c = MathHelper.lerp(a, b, Math.random());
+            assert (MathHelper.between(a, b, c, epsilon));
+        }
+    }
+
+    @Test
+    public void testNotBetween() {
+        Point2d a = new Point2d(0, 0);
+        Point2d b = new Point2d(1, 0);
+        Point2d c;
+        double epsilon = 1e-9;
 
         for(int i=0;i<50;++i) {
             a.set(Math.random()*500-250, Math.random()*500-250);
             b.set(Math.random()*500-250, Math.random()*500-250);
-            c = MathHelper.lerp(a,b,Math.random());
-            assert(MathHelper.between(a,b,c,epsilon));
+            c = MathHelper.lerp(a,b,1.0001+Math.random());
+            Assertions.assertFalse(MathHelper.between(a, b, c, epsilon));
+        }
+
+        for(int i=0;i<50;++i) {
+            a.set(Math.random()*500-250, Math.random()*500-250);
+            b.set(Math.random()*500-250, Math.random()*500-250);
+            c = MathHelper.lerp(a,b,-0.0001-Math.random());
+            Assertions.assertFalse(MathHelper.between(a, b, c, epsilon));
         }
     }
 }
