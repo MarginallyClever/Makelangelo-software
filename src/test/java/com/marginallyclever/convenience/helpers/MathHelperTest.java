e19af7dd8a4cb1a9f49685f323a0b390c333338a
Dan Royer <dan@marginallyclever.com>
2023-09-08 15:34:54 -0700

    test points not on line return false

--- src/test/java/com/marginallyclever/convenience/helpers/MathHelperTest.java
+++ src/test/java/com/marginallyclever/convenience/helpers/MathHelperTest.java
@@ -8,12 +8,10 @@ import javax.vecmath.Point2d;
 public class MathHelperTest {
     @Test
     public void testBetween() {
-        Point2d a = new Point2d(0, 0);
-        Point2d b = new Point2d(1, 0);
-        Point2d c = new Point2d(0.5, 0);
+        Point2d a = new Point2d();
+        Point2d b = new Point2d();
+        Point2d c;
         double epsilon = 1e-9;
-
-        assert (MathHelper.between(a, b, c, epsilon));
 
         for (int i = 0; i < 50; ++i) {
             a.set(Math.random() * 500 - 250, Math.random() * 500 - 250);
@@ -25,8 +23,8 @@ public class MathHelperTest {
 
     @Test
     public void testNotBetween() {
-        Point2d a = new Point2d(0, 0);
-        Point2d b = new Point2d(1, 0);
+        Point2d a = new Point2d();
+        Point2d b = new Point2d();
         Point2d c;
         double epsilon = 1e-9;
 
@@ -41,6 +39,26 @@ public class MathHelperTest {
             a.set(Math.random()*500-250, Math.random()*500-250);
             b.set(Math.random()*500-250, Math.random()*500-250);
             c = MathHelper.lerp(a,b,-0.0001-Math.random());
+            Assertions.assertFalse(MathHelper.between(a, b, c, epsilon));
+        }
+    }
+
+    @Test
+    public void testOffLine() {
+        Point2d a = new Point2d();
+        Point2d b = new Point2d();
+        Point2d ortho = new Point2d();
+        Point2d c;
+        double epsilon = 1e-9;
+
+        for(int i=0;i<50;++i) {
+            a.set(Math.random()*500-250, Math.random()*500-250);
+            b.set(Math.random()*500-250, Math.random()*500-250);
+            c = MathHelper.lerp(a,b,1.0001+Math.random());
+            ortho.set(b);
+            ortho.sub(a);
+            c.x+=ortho.y;
+            c.y+=ortho.x;
             Assertions.assertFalse(MathHelper.between(a, b, c, epsilon));
         }
     }
