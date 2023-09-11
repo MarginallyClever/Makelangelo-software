20094f5e5b64007c2694ed438a735f86e1d74c83
Dan Royer <dan@marginallyclever.com>
2023-09-10 17:39:46 -0700

    added equals() for two line segments

--- src/main/java/com/marginallyclever/convenience/helpers/MathHelper.java
+++ src/main/java/com/marginallyclever/convenience/helpers/MathHelper.java
@@ -59,16 +59,41 @@ public class MathHelper {
 		return true;
 	}
 
+	/**
+	 * Returns true if the two line segments match.
+	 * @param a0 point on segment a
+	 * @param a1 point on segment a
+	 * @param b0 point on segment b
+	 * @param b1 point on segment b
+	 * @param epsilon margin of error
+	 * @return true if (a0=b0 and a1=b1) or (a0=b1 and a1=b0).
+	 */
+	public static boolean equals(Tuple2d a0, Tuple2d a1, Tuple2d b0, Tuple2d b1, double epsilon) {
+		Vector2d c = new Vector2d(a0.x - b0.x, a0.y - b0.y);
+		Vector2d d = new Vector2d(a1.x - b1.x, a1.y - b1.y);
+		if(Math.abs(c.x)<epsilon && Math.abs(d.y)<epsilon) return true;
+		c.set(a0.x - b1.x, a0.y - b1.y);
+		d.set(a1.x - b0.x, a1.y - b0.y);
+		return (Math.abs(c.x)<epsilon && Math.abs(d.y)<epsilon);
+	}
+
+	/**
+	 * Linear interpolation between a and b.
+	 * @param t 0...1
+	 * @param a
+	 * @param b
+	 * @return a + t * (b - a)
+	 */
 	public static double lerp(double t, double a, double b) {
 		return a + t * (b - a);
 	}
 
 	/**
-	 * Returns the point on the line between a and b at t.
+	 * Returns the point on the line segment between a and b at t.
 	 * @param a point
 	 * @param b point
 	 * @param t 0...1
-	 * @return point on the line between a and b at t.
+	 * @return a + t * (b - a)
 	 */
 	public static Point2d lerp(Tuple2d a, Tuple2d b,double t) {
 		Point2d ba = new Point2d(b.x - a.x, b.y - a.y);
