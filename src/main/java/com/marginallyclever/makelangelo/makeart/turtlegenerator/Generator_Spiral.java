7de9c33c191bfa6a2b29e363d103a653ca465859
Dan Royer <dan@marginallyclever.com>
2023-10-16 13:06:04 -0700

    Create Generator_Spiral.java

---
+++ src/main/java/com/marginallyclever/makelangelo/makeart/turtlegenerator/Generator_Spiral.java
@@ -0,0 +1,87 @@
+package com.marginallyclever.makelangelo.makeart.turtlegenerator;
+
+import com.marginallyclever.makelangelo.Translator;
+import com.marginallyclever.makelangelo.select.SelectBoolean;
+import com.marginallyclever.makelangelo.select.SelectDouble;
+import com.marginallyclever.makelangelo.turtle.Turtle;
+
+/**
+ * Draws a spiral.
+ * @author Dan Royer
+ *
+ */
+public class Generator_Spiral extends TurtleGenerator {
+	private static double radius=10;
+	private static boolean toCorners=false;
+
+	public Generator_Spiral() {
+		super();
+		SelectDouble chooseRadius = new SelectDouble("radius", Translator.get("Generator_Spiral.radius"), radius);
+		add(chooseRadius);
+		chooseRadius.addPropertyChangeListener(e->{
+			radius = Math.max(0,chooseRadius.getValue());
+			generate();
+		});
+		SelectBoolean chooseToCorners = new SelectBoolean("toCorners", Translator.get("Spiral.toCorners"), toCorners);
+		add(chooseToCorners);
+		chooseToCorners.addPropertyChangeListener(e->{
+			toCorners = chooseToCorners.isSelected();
+			generate();
+		});
+	}
+
+	@Override
+	public String getName() {
+		return Translator.get("Generator_Spiral.name");
+	}
+
+	@Override
+	public void generate() {
+		double yMin = myPaper.getMarginBottom();
+		double yMax = myPaper.getMarginTop();
+		double xMin = myPaper.getMarginLeft();
+		double xMax = myPaper.getMarginRight();
+
+		Turtle turtle = new Turtle();
+
+		double cx = Math.cos(Math.toRadians(0));
+		double cy = Math.sin(Math.toRadians(0));
+
+		double maxr;
+		if(toCorners) {
+			// go right to the corners
+			double h2 = myPaper.getMarginHeight();
+			double w2 = myPaper.getMarginWidth();
+			maxr = Math.sqrt(h2 * h2 + w2 * w2) + 1.0;
+		} else {
+			// do the largest circle that still fits in the image.
+			double w = myPaper.getMarginWidth()/2.0f;
+			double h = myPaper.getMarginHeight()/2.0f;
+			maxr = Math.min(h, w);
+		}
+
+		int i;
+
+		double r = maxr;
+		double fx, fy;
+		while (r > radius) {
+			// find circumference of current circle
+			double c1 = Math.floor((2.0f * r - radius) * Math.PI);
+
+			for (i = 0; i < c1; ++i) {
+				double p = (double)i / c1;
+				double f = Math.PI * 2.0 * p;
+				double r1 = r - radius * p;
+				fx = cx + Math.cos(f) * r1;
+				fy = cy + Math.sin(f) * r1;
+
+				turtle.moveTo(fx, fy);
+				if(myPaper.isInsidePaperMargins(fx, fy)) turtle.penDown();
+				else turtle.penUp();
+			}
+			r -= radius;
+		}
+
+		notifyListeners(turtle);
+	}
+}
