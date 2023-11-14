6ca69c3289b60534d292c1982b204b853d7ac286
Dan Royer <dan@marginallyclever.com>
2023-11-14 15:22:45 -0800

    fixed PaperSettingsPanelTest at last!

--- src/test/java/com/marginallyclever/makelangelo/paper/PaperSettingsPanelTest.java
+++ src/test/java/com/marginallyclever/makelangelo/paper/PaperSettingsPanelTest.java
@@ -2,34 +2,58 @@ package com.marginallyclever.makelangelo.paper;
 
 import com.marginallyclever.makelangelo.Translator;
 import com.marginallyclever.util.PreferencesHelper;
+import org.assertj.swing.core.BasicRobot;
+import org.assertj.swing.core.Robot;
+import org.assertj.swing.fixture.FrameFixture;
 import org.assertj.swing.fixture.JPanelFixture;
-import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
+
+import javax.swing.*;
 
 /**
  * {@code PaperSettings} tests
  * @author Dan Royer
  *
  */
-public class PaperSettingsPanelTest extends AssertJSwingJUnitTestCase {
-	private JPanelFixture panel;
 
-	@Override
-	protected void onSetUp() {
+@DisabledIfEnvironmentVariable(named = "CI", matches = "true", disabledReason = "headless environment")
+public class PaperSettingsPanelTest {
+	private FrameFixture window;
+
+	@BeforeEach
+	public void setUp() {
+		Robot robot = BasicRobot.robotWithNewAwtHierarchy();
+
 		PreferencesHelper.start();
 		Translator.start();
-		panel = new JPanelFixture(robot(),new PaperSettingsPanel(new Paper()));
+
+		PaperSettingsPanel panel = new PaperSettingsPanel(new Paper());
+		JFrame frame = new JFrame();
+		frame.setContentPane(panel);
+		frame.pack();
+		window = new FrameFixture(robot, frame);
+		window.show(); // shows the frame to test
+	}
+
+	@AfterEach
+	public void tearDown() {
+		if(window!=null) window.cleanUp();
 	}
 
 	@SuppressWarnings("unlikely-arg-type")
 	@Test
 	public void testLandscapeToPortrait() {
-		panel.comboBox("size").selectItem(1);
-		assert(panel.textBox("width.field").equals("1682"));
-		assert(panel.textBox("height.field").equals("2378"));
-		panel.comboBox("size").selectItem(1);
-		panel.checkBox("landscape").click();
-		assert(panel.textBox("width.field").equals("2378"));
-		assert(panel.textBox("height.field").equals("1682"));
+		JPanelFixture panel = window.panel(PaperSettingsPanel.class.getSimpleName());
+		panel.requireVisible();
+		panel.panel("size").comboBox().selectItem(1);
+		assert(Double.parseDouble(panel.textBox("width.field").text()) == 1682);
+		assert(Double.parseDouble(panel.textBox("height.field").text()) == 2378.0);
+		panel.panel("size").comboBox().selectItem(1);
+		panel.panel("landscape").checkBox().click();
+		assert(Double.parseDouble(panel.textBox("width.field").text()) == 2378);
+		assert(Double.parseDouble(panel.textBox("height.field").text()) == 1682);
 	}
 }
