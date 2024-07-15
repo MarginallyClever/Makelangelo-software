506970ac9b5a26cf09206dc977b96f28e3425f8f
Dan Royer <dan@marginallyclever.com>
2024-07-15 11:35:50 -0700

    Create PenPanel.java

---
+++ src/main/java/com/marginallyclever/makelangelo/pen/PenPanel.java
@@ -0,0 +1,49 @@
+package com.marginallyclever.makelangelo.pen;
+
+import com.marginallyclever.makelangelo.select.SelectColor;
+import com.marginallyclever.makelangelo.select.SelectDouble;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Describes a pen that can be used to draw on a surface.
+ */
+public class PenPanel extends JPanel {
+    private final Pen myPen;
+
+    public PenPanel() {
+        this(new Pen("Black"));
+    }
+
+    public PenPanel(final Pen pen) {
+        super(new FlowLayout(FlowLayout.LEADING));
+        this.myPen = pen;
+
+        setName(PenPanel.class.getSimpleName());
+
+        SelectColor colorChoice = new SelectColor("PenColor","", pen.color, this);
+        colorChoice.addPropertyChangeListener(e -> pen.color = colorChoice.getColor());
+
+        SelectDouble radiusChoice = new SelectDouble("PenRadius","",pen.radius);
+        radiusChoice.addPropertyChangeListener(e -> pen.radius = Math.abs(radiusChoice.getValue()));
+
+        add(colorChoice);
+        add(radiusChoice);
+        add(new JLabel("⌀ mm"));
+
+        setPreferredSize(new Dimension(250,25));
+    }
+
+    public void setSelected(boolean selected) {
+        if (selected) {
+            setBackground(Color.LIGHT_GRAY);
+        } else {
+            setBackground(UIManager.getColor("Panel.background"));
+        }
+    }
+
+    public Pen getPen() {
+        return myPen;
+    }
+}
