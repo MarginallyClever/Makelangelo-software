30c67736414e6754ed0bc6bb38c7a4d5badf7ca8
Dan Royer <dan@marginallyclever.com>
2025-01-03 10:39:10 -0800

    removed open/export no longer used by Donatello

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -20,23 +20,4 @@ module com.marginallyclever.makelangelo {
     requires org.locationtech.jts;
     requires org.apache.httpcomponents.httpclient;
     requires org.apache.httpcomponents.httpcore;
-
-    opens com.marginallyclever.convenience;
-    opens com.marginallyclever.convenience.voronoi;
-    opens com.marginallyclever.makelangelo.makeart.io;
-    opens com.marginallyclever.makelangelo.plotter.plottercontrols;
-    opens com.marginallyclever.makelangelo.turtle;
-
-    exports com.marginallyclever.communications;
-    exports com.marginallyclever.convenience.log to ch.qos.logback.core;
-    exports com.marginallyclever.makelangelo;
-    exports com.marginallyclever.makelangelo.makeart;
-    exports com.marginallyclever.makelangelo.makeart.imagefilter;
-    exports com.marginallyclever.makelangelo.makeart.turtletool;
-    exports com.marginallyclever.makelangelo.paper;
-    exports com.marginallyclever.makelangelo.plotter.plottersettings;
-    exports com.marginallyclever.makelangelo.turtle;
-
-    opens com.marginallyclever.convenience.noise;
-    opens com.marginallyclever.convenience.helpers;
 }
