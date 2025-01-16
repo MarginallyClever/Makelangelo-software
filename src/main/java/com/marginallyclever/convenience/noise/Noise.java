26702769846a5f07a7033b54a4634a79e5a249bb
Dan Royer <dan@marginallyclever.com>
2025-01-16 08:53:31 -0800

    added comments

--- src/main/java/com/marginallyclever/convenience/noise/Noise.java
+++ src/main/java/com/marginallyclever/convenience/noise/Noise.java
@@ -1,14 +1,27 @@
 package com.marginallyclever.convenience.noise;
 
 public interface Noise {
-    // 1D noise
+    /**
+     * 1D noise
+     * @param xin a double
+     * @return a double [0...1]
+     */
     double noise(double xin);
 
-    // 2D noise
+    /**
+     * 2D noise
+     * @param xin a double
+     * @param yin a double
+     * @return a double [0...1]
+     */
     double noise(double xin, double yin);
 
-    // 3D noise
+    /**
+     * 3D noise
+     * @param xin a double
+     * @param yin a double
+     * @param zin a double
+     * @return a double [0...1]
+     */
     double noise(double xin, double yin,double zin);
-
-    //double noise(double xin, double yin,double zin,double win);
 }
