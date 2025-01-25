ae6137dcaab299656f5b80ff22f74dab628eb676
Dan Royer <dan@marginallyclever.com>
2025-01-25 12:47:16 -0800

    Spiral working the way I want

--- src/main/java/com/marginallyclever/makelangelo/donatelloimpl/nodes/Spiral.java
+++ src/main/java/com/marginallyclever/makelangelo/donatelloimpl/nodes/Spiral.java
@@ -11,14 +11,16 @@ import com.marginallyclever.nodegraphcore.port.Output;
  */
 public class Spiral extends Node {
     private final Input<Turtle> source = new Input<>("turtle", Turtle.class, new Turtle());
-    private final Input<Number> twist = new Input<>("twist", Number.class, 1);
+    private final Input<Number> r0 = new Input<>("r0", Number.class, 1);
+    private final Input<Number> dr = new Input<>("dr", Number.class, 1);
     private final Input<Number> stepSize = new Input<>("stepSize", Number.class, 1);
     private final Output<Turtle> output = new Output<>("output", Turtle.class, new Turtle());
 
     public Spiral() {
         super("Spiral");
         addVariable(source);
-        addVariable(twist);
+        addVariable(r0);
+        addVariable(dr);
         addVariable(stepSize);
         addVariable(output);
     }
@@ -26,15 +28,15 @@ public class Spiral extends Node {
     @Override
     public void update() {
         try {
-            var twistValue = twist.getValue().doubleValue();
+            var r0Value = r0.getValue().doubleValue();
+            var drValue = dr.getValue().doubleValue();
             var turtle = source.getValue();
             TurtlePathWalker pathWalker = new TurtlePathWalker(turtle);
             double dist = turtle.getBounds().width;
             var pace = Math.max(0.0001, stepSize.getValue().doubleValue());
 
-            double curve = Math.toRadians(twistValue);
             double curveSum = 0;
-            double px=0,py=0;
+            double px,py;
 
             var result = new Turtle();
             double t = 0.0;  // the distance along the curve
@@ -43,18 +45,23 @@ public class Spiral extends Node {
                 // get the next point
                 var p2 = pathWalker.walk(pace);
                 // the x of the original path is the distance forward
-                var dx = p2.x-t;
+                var dt = p2.x-t;
                 t = p2.x;
+                // radius at this point
+                double rN = r0Value + drValue * (curveSum/(2*Math.PI));
                 // forward direction
                 double nx = Math.cos(curveSum);
                 double ny = Math.sin(curveSum);
-                curveSum += dx * curve;  // as px/py move forward, the curve increases proportionally.
-                curve += Math.toRadians(dx * 0.00001);
-                px += dx * nx;
-                py += dx * ny;
+                // position
+                px = rN * nx;
+                py = rN * ny;
+                // dt is the arc length of the curve between the last point and this point
+                // the angle represented by dt and rN is calculated as follows:
+                double dtAngle = dt / rN;
+                curveSum += dtAngle;
                 // the y of the original path is the distance sideways
-                double tx = p2.y * ny;
-                double ty = p2.y * -nx;
+                double tx = p2.y * nx;
+                double ty = p2.y * ny;
 
                 result.moveTo(px+tx,py+ty);
                 result.penDown();
