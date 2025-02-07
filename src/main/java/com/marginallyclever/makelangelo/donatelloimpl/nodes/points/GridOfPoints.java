da9866d23a3888e8220e863c65b76d00c864f96b
Dan Royer <dan@marginallyclever.com>
2025-02-06 20:35:40 -0800

    x and y spacing now use InputNumber, added style.

--- src/main/java/com/marginallyclever/makelangelo/donatelloimpl/nodes/points/GridOfPoints.java
+++ src/main/java/com/marginallyclever/makelangelo/donatelloimpl/nodes/points/GridOfPoints.java
@@ -1,7 +1,8 @@
 package com.marginallyclever.makelangelo.donatelloimpl.nodes.points;
 
-import com.marginallyclever.donatello.ports.InputDouble;
 import com.marginallyclever.donatello.ports.InputInt;
+import com.marginallyclever.donatello.ports.InputNumber;
+import com.marginallyclever.donatello.ports.InputOneOfMany;
 import com.marginallyclever.nodegraphcore.Node;
 
 import javax.vecmath.Point2d;
@@ -12,9 +13,10 @@ import java.util.stream.IntStream;
  */
 public class GridOfPoints extends Node {
     private final InputInt numberAcross = new InputInt("x count",10);
-    private final InputDouble spaceAcross = new InputDouble("x spacing",10d);
+    private final InputNumber spaceAcross = new InputNumber("x spacing",10d);
     private final InputInt numberDown = new InputInt("y count",10);
-    private final InputDouble spaceDown = new InputDouble("y spacing",10d);
+    private final InputNumber spaceDown = new InputNumber("y spacing",10d);
+    private final InputOneOfMany style = new InputOneOfMany("style");
     private final OutputPoints output = new OutputPoints("output");
 
     public GridOfPoints() {
@@ -23,23 +25,34 @@ public class GridOfPoints extends Node {
         addVariable(spaceAcross);
         addVariable(numberDown);
         addVariable(spaceDown);
+        addVariable(style);
         addVariable(output);
+
+        style.setOptions(new String[]{"spacing","total"});
     }
 
     @Override
     public void update() {
         var nx = Math.max(1,numberAcross.getValue());
         var ny = Math.max(1,numberDown.getValue());
-        var dx = spaceAcross.getValue();
-        var dy = spaceDown.getValue();
+        double dx, dy;
 
         var list = new ListOfPoints();
-        IntStream.range(0,ny).forEach(y->{
-            IntStream.range(0,nx).forEach(x->{
-                list.add(new Point2d(x*dx,y*dy));
+        if(style.getValue()==1) {
+            // total distance divided by number of points.
+            dx = spaceAcross.getValue().doubleValue() / nx;
+            dy = spaceDown.getValue().doubleValue() / ny;
+        } else {
+            // space between points
+            dx = spaceAcross.getValue().doubleValue();
+            dy = spaceDown.getValue().doubleValue();
+        }
+
+        IntStream.range(0, ny).forEach(y -> {
+            IntStream.range(0, nx).forEach(x -> {
+                list.add(new Point2d(x * dx, y * dy));
             });
         });
-
         output.send(list);
     }
 }
