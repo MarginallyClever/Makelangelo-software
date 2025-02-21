1a53d2081864eb6fa5ec9e1c2350e8972d19079c
Dan Royer <dan@marginallyclever.com>
2025-02-21 09:50:29 -0800

    explicitly set extension of file on save, which will trigger turtle factory to save correctly.

--- src/main/java/com/marginallyclever/makelangelo/donatelloimpl/nodes/turtle/SaveTurtle.java
+++ src/main/java/com/marginallyclever/makelangelo/donatelloimpl/nodes/turtle/SaveTurtle.java
@@ -7,8 +7,11 @@ import com.marginallyclever.makelangelo.plotter.plottersettings.PlotterSettings;
 import com.marginallyclever.makelangelo.plotter.plottersettings.PlotterSettingsManager;
 import com.marginallyclever.makelangelo.turtle.Turtle;
 import com.marginallyclever.nodegraphcore.Node;
+import org.apache.commons.io.FilenameUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
  * Save a {@link Turtle} to a file.
@@ -35,10 +38,21 @@ public class SaveTurtle extends Node {
         if(filenameValue==null || filenameValue.isEmpty())  return;
 
         try {
+            var fc = TurtleFactory.getSaveFileChooser();
+           String withExtension = addExtension(filenameValue,((FileNameExtensionFilter)fc.getFileFilter()).getExtensions());
+            logger.debug("File selected by user: {}", withExtension);
             PlotterSettings settings = PlotterSettingsManager.buildMakelangelo5();
-            TurtleFactory.save(turtle.getValue(),filenameValue,settings);
+            TurtleFactory.save(turtle.getValue(),withExtension,settings);
         } catch (Exception e) {
             logger.warn("Failed to save", e);
         }
+    }
+
+    private String addExtension(String name, String [] extensions) {
+        for( String e : extensions ) {
+            if(FilenameUtils.getExtension(name).equalsIgnoreCase(e)) return name;
+        }
+
+        return name + "." + extensions[0];
     }
 }
