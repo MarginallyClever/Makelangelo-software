cec26d61ff18841bb5e153544ab83c37b2782fb4
Dan Royer <dan@marginallyclever.com>
2025-03-16 19:09:12 -0700

    display output in a non-blocking manner.
    use a SwingWorker to run the process so the JTextArea can be updated.

--- src/main/java/com/marginallyclever/makelangelo/firmwareuploader/FirmwareUploaderPanel.java
+++ src/main/java/com/marginallyclever/makelangelo/firmwareuploader/FirmwareUploaderPanel.java
@@ -2,10 +2,10 @@ package com.marginallyclever.makelangelo.firmwareuploader;
 
 import com.marginallyclever.communications.serial.SerialTransportLayer;
 import com.marginallyclever.convenience.CommandLineOptions;
-import com.marginallyclever.makelangelo.Translator;
 import com.marginallyclever.donatello.select.SelectButton;
 import com.marginallyclever.donatello.select.SelectOneOfMany;
 import com.marginallyclever.donatello.select.SelectReadOnlyText;
+import com.marginallyclever.makelangelo.Translator;
 import com.marginallyclever.util.PreferencesHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +13,8 @@ import org.slf4j.LoggerFactory;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
 /**
  * A panel for uploading firmware to the robot.
@@ -27,6 +29,7 @@ public class FirmwareUploaderPanel extends JPanel {
 	private final SelectOneOfMany port = new SelectOneOfMany("port",Translator.get("Port"));
     private final SelectButton startM5 = new SelectButton("startM5",Translator.get("FirmwareUploaderPanel.startM5"));
 	private final SelectButton startHuge = new SelectButton("startHuge",Translator.get("FirmwareUploaderPanel.startHuge"));
+	private final JTextArea console = new JTextArea();
 
 
     public FirmwareUploaderPanel() {
@@ -34,12 +37,14 @@ public class FirmwareUploaderPanel extends JPanel {
 		this.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 
 		updateCOMPortList();
+		console.setEditable(false);
 
 		GridBagConstraints c = new GridBagConstraints();
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridx=0;
 		c.gridy=0;
 		c.weightx=1;
+		c.gridwidth=1;
 		c.weighty=0;
 
         SelectButton refreshButton = new SelectButton("refresh", "⟳");
@@ -47,13 +52,38 @@ public class FirmwareUploaderPanel extends JPanel {
 
         SelectReadOnlyText help = new SelectReadOnlyText("help", Translator.get("FirmwareUploader.help"));
         help.attach(this,c);  c.gridy++;
-		port.attach(this,c);  c.gridy++;
-		refreshButton.attach(this,c);  c.gridy++;
-		startM5.attach(this,c);  c.gridy++;
-		startHuge.attach(this,c);  c.gridy++;
+		c.gridwidth=1;
+
+		JPanel panel = new JPanel(new BorderLayout(2,0));
+		panel.add(port.getLabel(),BorderLayout.LINE_START);
+		panel.add(port.getField(),BorderLayout.CENTER);
+		this.add(panel,c);
+		c.gridx++;
+		c.weightx=0;
+		this.add(refreshButton.getButton(),c);
+		c.gridy++;
+
+		c.gridx=0;
+		startM5.attach(this,c);
+		c.gridy++;
+
+		startHuge.attach(this,c);
+		c.gridy++;
+
+		c.fill = GridBagConstraints.BOTH;
+		c.weighty=1;
+		c.weightx=1;
+		c.gridx=0;
+		c.gridwidth=3;
+
+		var scroll = new JScrollPane(console);
+		scroll.setPreferredSize(new Dimension(300, 100));
+		scroll.setMinimumSize(new Dimension(300, 100));
+		this.add(scroll,c);
 
 		startM5.addActionListener(e -> run(e,"firmware-m5.hex"));
 		startHuge.addActionListener(e -> run(e,"firmware-huge.hex"));
+
 	}
 
 	private void updateCOMPortList() {
@@ -107,28 +137,60 @@ public class FirmwareUploaderPanel extends JPanel {
 		startHuge.setEnabled(false);
 		setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
 
-		String status = Translator.get("FirmwareUploaderPanel.finished");
-		int messageType = JOptionPane.PLAIN_MESSAGE;
-		int result = 1;
+		var doc = console.getDocument();
+		// clear text area
 		try {
-			result = firmwareUploader.performUpdate(port.getSelectedItem());
-		} catch (Exception e1) {
-			logger.error("failed to run avrdude: ",e1);
-			status = e1.getMessage();
-			messageType = JOptionPane.ERROR_MESSAGE;
+			doc.remove(0, doc.getLength());
+		} catch (Exception e) {
+			logger.error("failed to clear console: ",e);
 		}
 
-		setCursor(Cursor.getDefaultCursor());
-		startM5.setEnabled(true);
-		startHuge.setEnabled(true);
+		// save the current output stream
+		var out = System.out;
+		// print results to the text area
+		System.setOut(new PrintStream(new OutputStream() {
+			@Override
+			public void write(int b) {
+				try {
+					doc.insertString(doc.getLength(), String.valueOf((char) b), null);
+				} catch (Exception e) {
+					logger.error("failed to write to console: ",e);
+				}
+			}
+		}));
 
-		if(result!=0) {
-			logger.error("upload failed.");
-			status = Translator.get("FirmwareUploaderPanel.failed");
-			messageType = JOptionPane.ERROR_MESSAGE;
-		}
+		SwingWorker<Void, Void> worker = new SwingWorker<>() {
+			@Override
+			protected Void doInBackground() throws Exception {
+				String status = Translator.get("FirmwareUploaderPanel.finished");
+				int messageType = JOptionPane.PLAIN_MESSAGE;
+				int result = 1;
+				try {
+					result = firmwareUploader.performUpdate(port.getSelectedItem());
+				} catch (Exception e1) {
+					logger.error("failed to run avrdude: ", e1);
+					status = e1.getMessage();
+					messageType = JOptionPane.ERROR_MESSAGE;
+				}
+				if (result != 0) {
+					logger.error("upload failed.");
+					status = Translator.get("FirmwareUploaderPanel.failed");
+					messageType = JOptionPane.ERROR_MESSAGE;
+				}
+				JOptionPane.showMessageDialog(FirmwareUploaderPanel.this, status, title, messageType);
+				return null;
+			}
 
-		JOptionPane.showMessageDialog(this,status,title,messageType);
+			@Override
+			protected void done() {
+				setCursor(Cursor.getDefaultCursor());
+				startM5.setEnabled(true);
+				startHuge.setEnabled(true);
+				System.setOut(out);
+			}
+		};
+
+		worker.execute();
 	}
 	
 	public static void main(String[] args) throws Exception {
