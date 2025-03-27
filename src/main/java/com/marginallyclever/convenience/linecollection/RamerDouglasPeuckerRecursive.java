01d38345adf9fb33d73946d9facff094adee52f9
Dan Royer <dan@marginallyclever.com>
2025-03-27 11:08:29 -0700

    closed loop condition

--- src/main/java/com/marginallyclever/convenience/linecollection/RamerDouglasPeuckerRecursive.java
+++ src/main/java/com/marginallyclever/convenience/linecollection/RamerDouglasPeuckerRecursive.java
@@ -4,56 +4,99 @@ import com.marginallyclever.convenience.LineSegment2D;
 import org.jetbrains.annotations.NotNull;
 
 import javax.vecmath.Point2d;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * {@link RamerDouglasPeuckerRecursive} simplifies a {@link LineCollection} using the <a
+ * <p>{@link RamerDouglasPeuckerRecursive} simplifies a {@link LineCollection} using the <a
  * href="https://en.wikipedia.org/wiki/Ramer%E2%80%93Douglas%E2%80%93Peucker_algorithm">Ramer-Douglas-Peucker
- * algorithm</a> in a recursive fashion.
+ * algorithm</a> in a recursive fashion.</p>
+ * <p>Requires all points are contiguous and of the same color.</p>
  */
 public class RamerDouglasPeuckerRecursive implements LineSimplifier {
-    private final LineCollection original;
-    private final boolean[] keep;
+    private final double CONTIGUOUS_TOLERANCE = 1e-4;
 
-    public RamerDouglasPeuckerRecursive(@NotNull LineCollection lineCollection) {
-        original = lineCollection;
-        keep = new boolean[original.size()];
+    private final List<Point2d> points = new ArrayList<>();
+    private final boolean[] keep;
+    private final Color c;
+    private double distanceToleranceSq;
+
+    /**
+     * Construct a new {@link RamerDouglasPeuckerRecursive} object.
+     * @param lineCollection the line collection to simplify
+     * @throws IllegalArgumentException if the segments are not contiguous
+     */
+    public RamerDouglasPeuckerRecursive(@NotNull LineCollection lineCollection) throws IllegalArgumentException {
+        testSegmentsAreContiguousAndSameColor(lineCollection);
+
+        c = lineCollection.getFirst().color;
+        points.add(lineCollection.getFirst().start);
+        for (var line : lineCollection) {
+            points.add(line.end);
+        }
+        keep = new boolean[points.size()];
     }
 
     @Override
     public @NotNull LineCollection simplify(double distanceTolerance) {
-        testSegmentsAreContiguous();
-
-        var len = original.size();
+        var len = points.size();
         keep[0] = true;
         keep[len-1] = true;
 
+        distanceToleranceSq = distanceTolerance * distanceTolerance;
+
         try {
-            simplifySection(0, len, distanceTolerance * distanceTolerance, keep);
-        } catch(Exception e) {
+            if(lineIsClosedLoop()) {
+                // if this is a closed loop the ptLineDistSq test will fail, so we need the first point
+                // and the point as far from first point as possible, then test in two halves.
+                int furthestIndex = getFurthestPointFromStart();
+                // if the furthest point is the first or last point, we can't simplify.
+                if(furthestIndex!=0 && furthestIndex!=len-1) {
+                    simplifySection(0,furthestIndex);
+                    simplifySection(furthestIndex,len-1);
+                }
+            } else {
+                simplifySection(0, len - 1);
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         return assembleResult();
     }
 
-    private LineCollection assembleResult() {
-        LineCollection result = new LineCollection();
-        Point2d head = original.getFirst().start;
+    private int getFurthestPointFromStart() {
+        var a = points.getFirst();
+        double maxDistanceSq = 0;
+        int maxIndex = 0;
+        for (int k = 1; k < points.size()-1; k++) {
+            double distSq = a.distanceSquared(points.get(k));
+            if (distSq > maxDistanceSq) {
+                maxDistanceSq = distSq;
+                maxIndex = k;
+            }
+        }
+        return maxIndex;
+    }
 
-        var len = original.size();
-        int count = 0;
+    private boolean lineIsClosedLoop() {
+        return points.getFirst().distanceSquared(points.getLast()) < CONTIGUOUS_TOLERANCE;
+    }
+
+    private @NotNull LineCollection assembleResult() {
+        LineCollection result = new LineCollection();
+        var head = points.getFirst();
+
+        var len = points.size();
         for (int i = 0; i < len; i++) {
             if (keep[i]) {
-                count++;
-                Point2d next = original.get(i).end;
-                result.add(new LineSegment2D(head, next, original.get(i).color));
+                var next = points.get(i);
+                result.add(new LineSegment2D(head, next, c));
                 head = next;
             }
         }
 
-        if(len!=count) {
-            System.out.println("simplified from " + len + " to " + count);
-        }
         return result;
     }
 
@@ -61,13 +104,22 @@ public class RamerDouglasPeuckerRecursive implements LineSimplifier {
      * Test that the segments are contiguous.
      * @throws IllegalArgumentException if the segments are not contiguous
      */
-    private void testSegmentsAreContiguous() throws IllegalArgumentException {
-        var len = original.size();
+    private void testSegmentsAreContiguousAndSameColor(LineCollection list) throws IllegalArgumentException {
+        var len = list.size();
+        if(len<2) return;
+
+        var first = list.getFirst();
+        Color firstColor = first.color;
         // test that the end of each original line is the start of the next
         for (int i = 0; i < len-1; i++) {
-            if (original.get(i).end.distanceSquared(original.get(i+1).start)>1e-4) {
+            var next = list.get(i+1);
+            if( first.end.distanceSquared(next.start) > CONTIGUOUS_TOLERANCE ) {
                 throw new IllegalArgumentException("Line segments must be contiguous.");
             }
+            if( !firstColor.equals(next.color) ) {
+                throw new IllegalArgumentException("Line segments must be the same color.");
+            }
+            first = next;
         }
     }
 
@@ -77,29 +129,19 @@ public class RamerDouglasPeuckerRecursive implements LineSimplifier {
      * then try to simplify the two halves.</p>
      * @param start the start index
      * @param end the end index
-     * @param distanceToleranceSq the distance tolerance squared
-     * @param usePt the array of booleans that indicates whether a point should be retained after simplification
      */
-    private void simplifySection(int start, int end,double distanceToleranceSq,boolean[] usePt) {
-        if ((start + 1) >= end) return; // adjacent points, nothing to simplify
-        System.out.println("testing "+start+" to "+end);
+    private void simplifySection(int start, int end) {
+        if(start + 1 >= end) return; // adjacent points, nothing to simplify
 
-        LineSegment2D seg=null;
-        try {
-            var a = original.get(start).start;
-            var b = original.get(end-1).end;
-            var c = original.get(start).color;
-
-            seg = new LineSegment2D(a,b,c);
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
+        var a = points.get(start);
+        var b = points.get(end);
 
         // find the point that is farthest from the line between the start and end points.
         double maxDistanceSq = 0;
         int maxIndex = -1;
-        for (int k = start+1; k < end-1; k++) {
-            double distSq = seg.ptLineDistSq(original.get(k).end);
+        for (int k = start+1; k < end; k++) {
+            var p = points.get(k);
+            double distSq = java.awt.geom.Line2D.ptLineDistSq(a.x,a.y, b.x,b.y, p.x,p.y);
             if (distSq > maxDistanceSq) {
                 maxDistanceSq = distSq;
                 maxIndex = k;
@@ -109,9 +151,9 @@ public class RamerDouglasPeuckerRecursive implements LineSimplifier {
         if (maxDistanceSq > distanceToleranceSq && maxIndex>-1) {
             // Split the work at the point of greatest inflection.
             // Keep the point and simplify the two halves.
-            usePt[maxIndex] = true;
-            simplifySection(start, maxIndex,distanceToleranceSq,usePt);
-            simplifySection(maxIndex, end,distanceToleranceSq,usePt);
+            keep[maxIndex] = true;
+            simplifySection(start, maxIndex);
+            simplifySection(maxIndex, end);
         } // else all points between start and end are within tolerance, they are already marked as false (discard).
     }
 }
