dbd8d789cc7a7fc53fcff057e54307fb23bcf9aa
Dan Royer <dan@marginallyclever.com>
2026-08-04 14:56:24 -0700

    improved scale to fit and scale to fill

--- src/main/java/com/marginallyclever/makelangelo/makeart/turtlegenerator/TurtleGeneratorHelper.java
+++ src/main/java/com/marginallyclever/makelangelo/makeart/turtlegenerator/TurtleGeneratorHelper.java
@@ -12,26 +12,21 @@ public class TurtleGeneratorHelper {
      * mode 1 = fit paper
      * @param sourceImage the image to scale
      * @param paper the paper reference by which to scale the image.
-     * @param mode the mode to scale the image
+     * @param mode the mode to scale the image.  mode 0: fill the paper.  mode 1: fit the image inside the margins.
      */
     public static void scaleImage(TransformedImage sourceImage, Paper paper, int mode) {
         Rectangle2D.Double rect = paper.getMarginRectangle();
         double width  = rect.getWidth();
         double height = rect.getHeight();
 
-        boolean test;
-        if (mode == 0) {
-            test = width < height;  // fill paper
-        } else {
-            test = width > height;  // fit paper
-        }
+        double imageWidth = sourceImage.getSourceImage().getWidth();
+        double imageHeight = sourceImage.getSourceImage().getHeight();
+        double widthRatio = width / imageWidth;
+        double heightRatio = height / imageHeight;
+        double f = (mode == 1)
+                ? Math.max(widthRatio, heightRatio)
+                : Math.min(widthRatio, heightRatio);
 
-        float f;
-        if( test ) {
-            f = (float)( width / (double)sourceImage.getSourceImage().getWidth() );
-        } else {
-            f = (float)( height / (double)sourceImage.getSourceImage().getHeight() );
-        }
-        sourceImage.setScale(f,-f);
+        sourceImage.setScale((float)f, -(float)f);
     }
 }
